cd4c7619602d71e1bb487e67b87eccc8eda66eff
Usman Suleiman <usmansbk@gmail.com>
2018-11-17 01:14:06 +0100

    Setting up

---
+++ android/app/src/main/java/com/schdlr/MainApplication.java
@@ -0,0 +1,74 @@
+package com.schdlr;
+
+import android.app.Application;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.fabric.crashlytics.RNFirebaseCrashlyticsPackage;
+import io.invertase.firebase.perf.RNFirebasePerformancePackage;
+import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage;
+
+import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.calendarevents.CalendarEventsPackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+ 
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+ 
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new RNFirebasePackage(),
+            new RNFirebasePackage(),
+            new FBSDKPackage(mCallbackManager),
+            new CalendarEventsPackage(),
+            new RNFetchBlobPackage(),
+            new ReactNativeConfigPackage(),
+            new RNFirebaseCrashlyticsPackage(),
+            new RNFirebasePerformancePackage(),
+            new RNFirebaseAnalyticsPackage()
+      );
+    }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    AppEventsLogger.activateApp(this);
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
