fb4d3911d919452ba12ea16afd52c6f0f4661fef
Usman Suleiman <usmansbk@gmail.com>
2018-11-18 09:20:37 +0100

    Refactor

---
+++ src/config/env.js
@@ -0,0 +1,24 @@
+const dev = {
+  ONE_SIGNAL_ID: 'c45c9feb-9c17-4f6b-a397-abc7b49e5e40',
+  APP_URL: 'https://schdlr-ng.herokuapp.com',
+  IOS_URL: '//:schdlr',
+  PROTOCOL: 'http://',
+  DOMAIN: '192.168.43.190:4000',
+  PORT: '4000',
+  EMAIL: 'usmansbk@gmail.com',
+  BUILD_VERSION: 'beta',
+  BUILD: 'dev'
+};
+
+export const prod = {
+  ONE_SIGNAL_ID: 'c45c9feb-9c17-4f6b-a397-abc7b49e5e40',
+  APP_URL: 'https://schdlr-ng.herokuapp.com',
+  IOS_URL: '//:schdlr',
+  PROTOCOL: 'https://',
+  DOMAIN: 'schdlr-ng.herokuapp.com',
+  EMAIL: 'usmansbk@gmail.com',
+  BUILD_VERSION: 'beta',
+  BUILD: 'prod'
+};
+
+export default dev_env;
