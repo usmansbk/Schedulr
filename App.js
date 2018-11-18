cf409c567b23b8b4e2729d4926c952f8cc85b537
Usman Suleiman <usmansbk@gmail.com>
2018-11-18 02:13:11 +0100

    Refactor

--- App.js
+++ App.js
@@ -1,63 +1,10 @@
-/**
- * Schdlr App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React from 'react';
-import { YellowBox } from 'react-native';
-import { ApolloProvider } from 'react-apollo';
-import { MenuProvider } from 'react-native-popup-menu';
-import { Root } from 'native-base';
-import Config from 'react-native-config';
-import RNLanguages from 'react-native-languages';
-import Firebase from 'react-native-firebase';
-import AppContainer from './src/components/Navigator';
-import client from './src/config/apolloClient';
-import i18n from './src/config/i18n';
+import { Provider as PaperProvider } from 'react-native-paper';
+import App from './src/App';
+import theme from './src/config/theme';
 
-const prefix = Config.APP_URL;
-
-const defaultHandler = (ErrorUtils.getGlobalHandler &&
-  ErrorUtils.getGlobalHandler()) || ErrorUtils._globalHandler;
-
-ErrorUtils.setGlobalHandler((error, isFatal) => {
-  Firebase.crashlytics().log(error.stack);
-  if (isFatal) {
-    //Firebase.crashlytics().crash();
-  } else {
-    Firebase.crashlytics().recordError(0, 'non-fatal');
-  }
-  defaultHandler.apply(this.arguments);
-})
-
-YellowBox.ignoreWarnings([
-  "Warning: isMounted(...) is deprecated",
-  "Module RCTImageLoader"
-]);
-
-export default class App extends React.Component {
-  componentWillMount() {
-    RNLanguages.addEventListener('change', this._onLanguagesChange);
-  }
-
-  componentWillUnmount() {
-    RNLanguages.removeEventListener('change', this._onLanguagesChange);
-  }
-
-  _onLanguagesChange = ({ language }) => {
-    i18n.locale = language;
-  };
-
-  render() {
-    return (
-      <Root>
-        <MenuProvider backHandler>
-          <ApolloProvider client={client}>
-            <AppContainer uriPrefix={prefix} />
-          </ApolloProvider>
-        </MenuProvider>
-      </Root>
-    )
-  }
-}
+export default () => (
+  <PaperProvider theme={theme}>
+    <App />
+  </PaperProvider>
+);
---
+++ src/config/theme.js
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'react-native-paper';
+
+export default {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#3f51b5',
+    accent: '#757de8',
+    error: '#cc3300',
+    text: '#404040',
+  }
+};
