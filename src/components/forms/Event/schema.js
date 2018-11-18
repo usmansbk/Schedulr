c07d76df6363010ceb188f3b206a1e7adc4f80b6
Usman Suleiman <usmansbk@gmail.com>
2018-11-19 00:24:38 +0100

    Add validation schema

---
+++ src/components/forms/Event/schema.js
@@ -0,0 +1,14 @@
+import * as Yup from 'yup';
+
+export default Yup.object().shape({
+  name: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Name is required'),
+  description: Yup.string()
+    .max(160, 'Too Long!'),
+  location: Yup.string()
+    .max(160, 'Too Long!'),
+  groupId: Yup.number()
+    .required('Event group is required')
+});
