0fc304d019393ed0255736a43b023c33b97841ac
Usman Suleiman <usmansbk@gmail.com>
2018-11-19 12:10:57 +0100

    Refactor

---
+++ src/components/screens/Login/Container.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { AsyncStorage } from 'react-native';
+import Login from './Login';
+
+export default class LoginScreen extends React.Component {
+
+  _signInAsync = async () => {
+    await AsyncStorage.setItem('userToken', 'abc');
+    this.props.navigation.navigate('App');
+  };
+
+  render() {
+    return (<Login handleLogin={this._signInAsync} />);
+  }
+}
