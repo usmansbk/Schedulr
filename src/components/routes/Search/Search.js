ffdc1cb41ba15cbd6b98638bdd0e2394be6b2556
Usman Suleiman <usmansbk@gmail.com>
2018-11-19 19:36:47 +0100

    Refactor button

--- src/components/routes/Search/Search.js
+++ src/components/routes/Search/Search.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Searchbar } from 'react-native-paper';
 import { View, StyleSheet } from 'react-native';
 
-export default () => (
+export default (props) => (
   <View style={styles.container}>
     <Searchbar
+      icon="arrow-back"
       placeholder="Search "
       value=""
       style={styles.searchbar}
+      onIconPress={() => props.navigation.goBack()}
     />
   </View>
 );
