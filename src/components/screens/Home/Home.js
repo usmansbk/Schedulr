63fd7e7fed7a7cd046dc51f94264b4bf8e2524b6
Usman Suleiman <usmansbk@gmail.com>
2018-11-19 10:44:34 +0100

    Refactor

--- src/components/screens/Home/Home.js
+++ src/components/screens/Home/Home.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Appbar, BottomNavigation } from 'react-native-paper';
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 import { createAppContainer } from 'react-navigation';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import moment from 'moment';
-import EventsRoute from '../../routes/Events';
-import GroupsRoute from '../../routes/Groups';
 import AccountRoute from '../../routes/Account';
-import StarredRoute from '../../routes/Starred';
 import SearchRoute from '../../routes/Search';
 import HomeRoute from '../../routes/Home';
 import NotificationRoute from '../../routes/Notifications';
@@ -42,45 +36,3 @@ const Home = createMaterialBottomTabNavigator({
 });
 
 export default createAppContainer(Home);
-
-// export default class Home extends React.Component {
-//   state = {
-//     index: 0,
-//     routes: [
-//       { key: 'events', title: 'Events', icon: 'date-range'},
-//       { key: 'groups', title: 'Groups', icon: 'group-work' },
-//       { key: 'starred', title: 'Starred', icon: 'star-border'},
-//       { key: 'account', title: 'Account', icon: 'menu' }
-//     ],
-//   };
-
-//   _getTitle = () => moment().format('dddd, Do');
-
-//   _handleIndexChange = index => this.setState({ index });
-
-//   _renderScene = BottomNavigation.SceneMap({
-//     events: EventsRoute,
-//     groups: GroupsRoute,
-//     starred: StarredRoute,
-//     account: AccountRoute
-//   });
-
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Appbar.Header>
-//           <Appbar.Content title={this._getTitle()}/>
-//           <Appbar.Action icon="notifications-none" />
-//           <Appbar.Action icon="search" />
-//         </Appbar.Header>
-//         <BottomNavigation
-//           activeColor={activeColor}
-//           barStyle={styles.barStyle}
-//           navigationState={this.state}
-//           onIndexChange={this._handleIndexChange}
-//           renderScene={this._renderScene}
-//         />
-//       </View>
-//     )
-//   }
-// }
