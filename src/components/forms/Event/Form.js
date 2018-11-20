4e92b16ef9609b0344f6ec5ac1401d040072f32e
Usman Suleiman <usmansbk@gmail.com>
2018-11-20 22:39:13 +0100

    Refactor

--- src/components/forms/Event/Form.js
+++ src/components/forms/Event/Form.js
@@ -32,183 +32,180 @@ const defaultValues = {
   groupId: '',
 };
 
-const Form = (props) => {
-  const {
-    loading,
-    groups=[],
-    initialValues,
-    handleSubmit,
-    handleCancel,
-    isEdit
-  } = props;
-  return (
-    <Formik
-      initialValues={initialValues || defaultValues}
-      validationSchema={formSchema}
-      onSubmit={(values, { setSubmitting }) => {
-        if (isEventValid(values)) {
-          handleSubmit && handleSubmit(values);
-        }
-        setSubmitting(false);
-      }}
-    >
-      {({
-        values,
-        errors,
-        touched,
-        isSubmitting=loading,
-        handleSubmit,
-        handleChange,
-        handleBlur,
-        setFieldValue
-      }) => (
-        <React.Fragment>
-        <Appbar.Header style={styles.header}>
-          <Button
+const Form = ({
+  loading,
+  groups=[],
+  initialValues,
+  handleSubmit,
+  handleCancel,
+  isEdit
+}) => (
+  <Formik
+    initialValues={initialValues || defaultValues}
+    validationSchema={formSchema}
+    onSubmit={(values, { setSubmitting }) => {
+      if (isEventValid(values)) {
+        handleSubmit && handleSubmit(values);
+      }
+      setSubmitting(false);
+    }}
+  >
+    {({
+      values,
+      errors,
+      touched,
+      isSubmitting=loading,
+      handleSubmit,
+      handleChange,
+      handleBlur,
+      setFieldValue
+    }) => (
+      <React.Fragment>
+      <Appbar.Header style={styles.header}>
+        <Button
+          mode="outlined"
+          onPress={handleCancel}
+          color={navButtonColor}
+        >Cancel</Button>
+        <Button
+          loading={isSubmitting}
+          mode="outlined"
+          color={navButtonColor}
+          onPress={handleSubmit}
+        >{ isEdit ? 'Save' : 'Create'}</Button>
+      </Appbar.Header>
+      <ScrollView>
+        <View style={styles.form}>
+          <TextInput
+            placeholder="Title"
+            value={values.title}
+            onChangeText={handleChange('title')}
+            onBlur={handleBlur('title')}
             mode="outlined"
-            onPress={handleCancel}
-            color={navButtonColor}
-          >Cancel</Button>
-          <Button
-            loading={isSubmitting}
+          />
+          <HelperText
+            type="error"
+            visible={errors.title && touched.title}
+          >
+          {errors.title}
+          </HelperText>
+          <TextInput
+            placeholder="Description"
+            value={values.description}
+            onChangeText={handleChange('description')}
+            onBlur={handleBlur('description')}
             mode="outlined"
-            color={navButtonColor}
-            onPress={handleSubmit}
-          >{ isEdit ? 'Save' : 'Create'}</Button>
-        </Appbar.Header>
-        <ScrollView>
-          <View style={styles.form}>
-            <TextInput
-              placeholder="Title"
-              value={values.title}
-              onChangeText={handleChange('title')}
-              onBlur={handleBlur('title')}
-              mode="outlined"
+          />
+          <HelperText
+            type="error"
+            visible={errors.description && touched.description}
+          >
+          {errors.description}
+          </HelperText>
+          <TextInput
+            placeholder="Location"
+            value={values.location}
+            onChangeText={handleChange('location')}
+            onBlur={handleBlur('location')}
+            mode="outlined"
+          />
+          <HelperText
+            type="error"
+            visible={errors.location && touched.location}
+          >
+          {errors.location}
+          </HelperText>
+          <DateTimeInput
+            label="From"
+            value={values.start}
+            onChangeDate={(date) => setFieldValue('start', date)}
+          />
+          <DateTimeInput
+            label="To"
+            value={values.end}
+            disabled={values.allDay}
+            onChangeDate={(date) => setFieldValue('end', date)}
+          />
+          <View style={styles.radio}>
+            <Text style={styles.radioText}>All Day</Text>
+            <RadioButton
+              value='allDay'
+              onPress={() => {
+                setFieldValue('allDay', !values.allDay)
+              }}
+              status={values.allDay ? 'checked' : 'unchecked'}
             />
-            <HelperText
-              type="error"
-              visible={errors.title && touched.title}
-            >
-            {errors.title}
-            </HelperText>
-            <TextInput
-              placeholder="Description"
-              value={values.description}
-              onChangeText={handleChange('description')}
-              onBlur={handleBlur('description')}
-              mode="outlined"
-            />
-            <HelperText
-              type="error"
-              visible={errors.description && touched.description}
-            >
-            {errors.description}
-            </HelperText>
-            <TextInput
-              placeholder="Location"
-              value={values.location}
-              onChangeText={handleChange('location')}
-              onBlur={handleBlur('location')}
-              mode="outlined"
-            />
-            <HelperText
-              type="error"
-              visible={errors.location && touched.location}
-            >
-            {errors.location}
-            </HelperText>
-            <DateTimeInput
-              label="From"
-              value={values.start}
-              onChangeDate={(date) => setFieldValue('start', date)}
-            />
-            <DateTimeInput
-              label="To"
-              value={values.end}
-              disabled={values.allDay}
-              onChangeDate={(date) => setFieldValue('end', date)}
-            />
-            <View style={styles.radio}>
-              <Text style={styles.radioText}>All Day</Text>
-              <RadioButton
-                value='allDay'
-                onPress={() => {
-                  setFieldValue('allDay', !values.allDay)
-                }}
-                status={values.allDay ? 'checked' : 'unchecked'}
-              />
-            </View>
-            <View style={styles.pickerSpacing}>
-              <Text style={styles.radioText}>Repetition</Text>
-              <Picker
-                prompt="Repeat"
-                selectedValue={values.repeat}
-                style={styles.picker}
-                itemStyle={styles.pickerItem}
-                onValueChange={itemValue => setFieldValue('repeat', itemValue)}
-              >
-                {
-                  frequency.map(freq => (
-                    <Picker.Item key={freq.id} label={freq.name} value={freq.id} />
-                  ))
-                }
-              </Picker>
-              {
-                (!canRepeat(values)) && (
-                  <HelperText
-                    type="error"
-                    visible={true}
-                  >
-                    {CANT_REPEAT}
-                  </HelperText>
-                )
-              }
-            </View>
-            <View style={styles.pickerSpacing}>
-              <Text style={styles.radioText}>Type</Text>
-              <Picker
-                prompt="Type"
-                selectedValue={values.type}
-                style={styles.picker}
-                itemStyle={styles.pickerItem}
-                onValueChange={itemValue => setFieldValue('type', itemValue)}
-              >
-                {
-                  eventTypes.map(type => (
-                    <Picker.Item key={type.id} label={type.name} value={type.id} />
-                  ))
-                }
-              </Picker>
-            </View>
-            <View style={styles.pickerSpacing}>
-              <Text style={styles.radioText}>Group</Text>
-              <Picker
-                prompt="Select a group"
-                selectedValue={values.groupId}
-                style={styles.picker}
-                itemStyle={styles.pickerItem}
-                onValueChange={itemValue => setFieldValue('groupId', itemValue)}
-              >
-                <Picker.Item label={(groups.length === 0) ? "No group" : "Select a group"} value="" />
-                {
-                  groups.map(group => (
-                    <Picker.Item key={group.id} label={group.name} value={group.id} />
-                  ))
-                }
-              </Picker>
-              <HelperText
-                type="error"
-                visible={errors.groupId && touched.groupId}
-              >
-                {errors.groupId}
-              </HelperText>
-            </View>
           </View>
-        </ScrollView>
-        </React.Fragment>
-      )}
-    </Formik>
-  );
-};
+          <View style={styles.pickerSpacing}>
+            <Text style={styles.radioText}>Repetition</Text>
+            <Picker
+              prompt="Repeat"
+              selectedValue={values.repeat}
+              style={styles.picker}
+              itemStyle={styles.pickerItem}
+              onValueChange={itemValue => setFieldValue('repeat', itemValue)}
+            >
+              {
+                frequency.map(freq => (
+                  <Picker.Item key={freq.id} label={freq.name} value={freq.id} />
+                ))
+              }
+            </Picker>
+            {
+              (!canRepeat(values)) && (
+                <HelperText
+                  type="error"
+                  visible={true}
+                >
+                  {CANT_REPEAT}
+                </HelperText>
+              )
+            }
+          </View>
+          <View style={styles.pickerSpacing}>
+            <Text style={styles.radioText}>Type</Text>
+            <Picker
+              prompt="Type"
+              selectedValue={values.type}
+              style={styles.picker}
+              itemStyle={styles.pickerItem}
+              onValueChange={itemValue => setFieldValue('type', itemValue)}
+            >
+              {
+                eventTypes.map(type => (
+                  <Picker.Item key={type.id} label={type.name} value={type.id} />
+                ))
+              }
+            </Picker>
+          </View>
+          <View style={styles.pickerSpacing}>
+            <Text style={styles.radioText}>Group</Text>
+            <Picker
+              prompt="Select a group"
+              selectedValue={values.groupId}
+              style={styles.picker}
+              itemStyle={styles.pickerItem}
+              onValueChange={itemValue => setFieldValue('groupId', itemValue)}
+            >
+              <Picker.Item label={(groups.length === 0) ? "No group" : "Select a group"} value="" />
+              {
+                groups.map(group => (
+                  <Picker.Item key={group.id} label={group.name} value={group.id} />
+                ))
+              }
+            </Picker>
+            <HelperText
+              type="error"
+              visible={errors.groupId && touched.groupId}
+            >
+              {errors.groupId}
+            </HelperText>
+          </View>
+        </View>
+      </ScrollView>
+      </React.Fragment>
+    )}
+  </Formik>
+);
 
 export default Form;
--- src/components/routes/EventDetails/Details.js
+++ src/components/routes/EventDetails/Details.js
@@ -23,60 +23,58 @@ export default ({
   navigateToComments,
   handleShare,
   handleMaps,
-}) => {
-  return (
-    <React.Fragment>
-    <ScrollView>
-      <View style={styles.container}>
-        <View style={styles.content}>
-          <View style={styles.head}>
-            { isCancelled && <Text style={styles.date}>Cancelled</Text>}
-            <Headline style={styles.title}>{title}</Headline>
-            <Text style={styles.date}>{date}</Text>
+}) => (
+  <React.Fragment>
+  <ScrollView>
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <View style={styles.head}>
+          { isCancelled && <Text style={styles.date}>Cancelled</Text>}
+          <Headline style={styles.title}>{title}</Headline>
+          <Text style={styles.date}>{date}</Text>
+        </View>
+        <Divider />
+        <View style={styles.body}>
+          <View style={styles.item}>
+            <Text style={styles.label}>TYPE</Text>
+            <Text style={styles.value}>{type}</Text>
           </View>
-          <Divider />
-          <View style={styles.body}>
-            <View style={styles.item}>
-              <Text style={styles.label}>TYPE</Text>
-              <Text style={styles.value}>{type}</Text>
-            </View>
-            <View style={styles.item}>
-              <Text style={styles.label}>LOCATION</Text>
-              <Text style={styles.value}>{location || 'No location set'}</Text>
-            </View>
-            <View style={styles.item}>
-              <Text style={styles.label}>GROUP</Text>
-              <Text onPress={() => navigateToGroup(groupId)} style={styles.value}>{groupName}</Text>
-            </View>
-            <View style={styles.item}>
-              <Text style={styles.label}>REPEAT</Text>
-              <Text style={styles.value}>{repeat}</Text>
-            </View>
-            <View style={styles.item}>
-              <Text style={styles.label}>CREATED</Text>
-              <Text style={styles.value}>{createdAt}</Text>
-            </View>
-            <View style={styles.item}>
-              <Text style={styles.label}>DESCRIPTION</Text>
-              <Text style={styles.value}>{description || 'No description'}</Text>
-            </View>
+          <View style={styles.item}>
+            <Text style={styles.label}>LOCATION</Text>
+            <Text style={styles.value}>{location || 'No location set'}</Text>
+          </View>
+          <View style={styles.item}>
+            <Text style={styles.label}>GROUP</Text>
+            <Text onPress={() => navigateToGroup(groupId)} style={styles.value}>{groupName}</Text>
+          </View>
+          <View style={styles.item}>
+            <Text style={styles.label}>REPEAT</Text>
+            <Text style={styles.value}>{repeat}</Text>
+          </View>
+          <View style={styles.item}>
+            <Text style={styles.label}>CREATED</Text>
+            <Text style={styles.value}>{createdAt}</Text>
+          </View>
+          <View style={styles.item}>
+            <Text style={styles.label}>DESCRIPTION</Text>
+            <Text style={styles.value}>{description || 'No description'}</Text>
           </View>
         </View>
       </View>
-    </ScrollView>
-    <Divider />
-    <Actions
-      id={id}
-      title={title}
-      location={location}
-      starred={starred}
-      starsCount={starsCount}
-      commentsCount={commentsCount}
-      date={date}
-      handleShare={handleShare}
-      handleMaps={handleMaps}
-      navigateToComments={navigateToComments}
-    />
-    </React.Fragment>
-  );
-}
+    </View>
+  </ScrollView>
+  <Divider />
+  <Actions
+    id={id}
+    title={title}
+    location={location}
+    starred={starred}
+    starsCount={starsCount}
+    commentsCount={commentsCount}
+    date={date}
+    handleShare={handleShare}
+    handleMaps={handleMaps}
+    navigateToComments={navigateToComments}
+  />
+  </React.Fragment>
+);
