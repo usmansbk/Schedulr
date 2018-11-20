bc9c77f7e58ddb8e37c3b593a307f00018ae7d81
Usman Suleiman <usmansbk@gmail.com>
2018-11-20 11:49:17 +0100

    Refactor

--- src/components/forms/Event/frequency.js
+++ src/components/forms/Event/frequency.js
@@ -1,5 +1,5 @@
 export default [
-  { name: 'One-time', id: 'ONCE' },
+  { name: 'Never', id: 'ONCE' },
   { name: 'Daily', id: 'DAILY' },
   { name: 'Weekly', id: 'WEEKLY' },
   { name: 'Monthly', id: 'MONTHLY' },
