e83ea7fde58702ffb313bb0b2458154b6da92ff3
Usman Suleiman <usmansbk@gmail.com>
2018-11-22 01:01:57 +0100

    Refactor

--- src/components/common/Fab/Button.js
+++ src/components/common/Fab/Button.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { FAB } from 'react-native-paper';
+import colors from '../../../config/colors';
 
 const Fab = ({ onPress, style, icon, small }) => (
   <FAB
     onPress={onPress}
+    theme={{
+      colors: {
+        accent: colors.primary
+      }
+    }}
     style={style}
     color="#fff"
     icon={icon}
