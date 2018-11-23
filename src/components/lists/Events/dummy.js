7185787b381ce7bc60531e591fc4d20914c0ab31
Usman Suleiman <usmansbk@gmail.com>
2018-11-23 02:28:04 +0100

    Refactor

--- src/components/lists/Events/dummy.js
+++ src/components/lists/Events/dummy.js
@@ -1,5 +1,5 @@
 export default [{
-  title: '11/23/2018',
+  title: '11/24/2018',
   data: [{
     cursor: 1,
     node: {
@@ -36,6 +36,60 @@ export default [{
     }
   },{
     cursor: 3,
+    node: {
+      id: 3,
+      title: 'Dev Mode 3',
+      description: 'Lorem Ipsum Dolor Amet',
+      location: null,
+      start: Date.now(),
+      end: new Date('11/20/2018'),
+      allDay: false,
+      isCancelled: false,
+      starred: false,
+      starsCount: 1234567,
+      commentsCount: 0,
+      repeat: 'WEEKLY',
+      type: 'HOBBY',
+    }
+  },]
+},{
+  title: '11/23/2018',
+  data: [{
+    cursor: 5,
+    node: {
+      id: 1,
+      title: 'Dev Mode',
+      description: 'Lorem Ipsum Dolor Amet',
+      location: 'No location set',
+      start: Date.now(),
+      end: new Date('11/20/2018'),
+      allDay: true,
+      isCancelled: false,
+      starred: false,
+      starsCount: 0,
+      commentsCount: 222203,
+      repeat: 'WEEKLY',
+      type: 'LECTURE',
+    }
+  },{
+    cursor: 6,
+    node: {
+      id: 2,
+      title: 'Dev Mode 2 Dragon Fever Slayer Authum King 2',
+      description: 'Lorem Ipsum Dolor Amet',
+      location: 'No location set',
+      start: Date.now(),
+      end: new Date('11/20/2018'),
+      allDay: false,
+      isCancelled: true,
+      starred: true,
+      starsCount: 2,
+      commentsCount: 0,
+      repeat: 'DAILY',
+      type: 'REMINDER',
+    }
+  },{
+    cursor: 7,
     node: {
       id: 3,
       title: 'Dev Mode 3',
---
+++ src/components/lists/Groups/List.js
@@ -0,0 +1,3 @@
+import React from 'react';
+import { View } from 'react-native';
+import { } from 'react-native-paper';
