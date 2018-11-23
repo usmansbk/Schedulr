4597e2c1114dcdfee2f535d4262c8c1335dd63b4
Usman Suleiman <usmansbk@gmail.com>
2018-11-23 15:42:52 +0100

    Refactor

--- src/components/lists/Events/Empty.js
+++ src/components/lists/Events/Empty.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { Headline, Paragraph } from 'react-native-paper';
+import { Headline } from 'react-native-paper';
 import styles from './styles';
 
 export default () => (
   <View style={styles.empty}>
     <Icon style={styles.emptyIcon} name="event-busy" />
     <Headline style={styles.emptyTitle}>No upcoming event</Headline>
-    <Paragraph style={styles.emptyMessage}>
-    Create an event by pressing the bottom-right button.
-    </Paragraph>
   </View>
 );
