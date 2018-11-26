ee7812bfd1a2274f73a468415c0fffeecf4fff1b
Usman Suleiman <usmansbk@gmail.com>
2018-11-26 20:50:59 +0100

    Refactor

--- src/components/screens/Help/Screen.js
+++ src/components/screens/Help/Screen.js
@@ -3,10 +3,12 @@ import Help from '../../routes/Help';
 
 export default class Screen extends React.Component {
   _goBack = () => this.props.navigation.goBack();
+  _onPressItem = (id) => alert(id);
   render() {
     return (
       <Help
         goBack={this._goBack}
+        onPressItem={this._onPressItem}
       />
     );
   }
