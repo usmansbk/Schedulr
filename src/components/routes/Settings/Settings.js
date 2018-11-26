a658816f144373641049b37c75c5e2a691dc0f6d
Usman Suleiman <usmansbk@gmail.com>
2018-11-26 23:15:13 +0100

    Refactor

--- src/components/routes/Settings/Settings.js
+++ src/components/routes/Settings/Settings.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import { View } from 'react-native';
-import { Appbar, List, Switch } from 'react-native-paper';
+import { ScrollView } from 'react-native';
+import {
+  Appbar,
+  List,
+  Switch,
+  Divider
+} from 'react-native-paper';
 import styles from '../../../config/styles';
 import colors from '../../../config/colors';
 
@@ -14,13 +19,52 @@ export default (props) => (
         titleStyle={styles.headerColor}
       />
     </Appbar.Header>
-    <View>
+    <ScrollView>
       <List.Section title="Reminders">
         <List.Item
           title="Mute"
           right={() => <Switch />}
         />
+        <Divider />
+        <List.Item
+          title="Event ended"
+          description="Get notified when an event end's"
+          right={() => <Switch />}
+        />
+        <Divider />
+        <List.Item
+          title="Remind me before"
+          right={() => <List.Icon icon="chevron-right" />}
+        />
+        <Divider />
       </List.Section>
-    </View>
+      <List.Section title="Push Notifications">
+        <List.Item
+          title="Disable"
+          right={() => <Switch />}
+        />
+      </List.Section>
+      <List.Section title="Comments">
+        <List.Item
+          title="From group admin"
+          right={() => <Switch />}
+        />
+        <Divider />
+        <List.Item
+          title="From group members"
+          right={() => <Switch />}
+        />
+        <Divider />
+        <List.Item
+          title="From visitors"
+          right={() => <Switch />}
+        />
+        <Divider />
+        <List.Item
+          title="Replies to your comments"
+          right={() => <Switch />}
+        />
+      </List.Section>
+    </ScrollView>
   </React.Fragment>
 );
