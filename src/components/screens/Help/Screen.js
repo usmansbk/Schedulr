0c5e525d2d621370899f9adebdadcae559285a6c
Usman Suleiman <usmansbk@gmail.com>
2018-11-27 07:27:50 +0100

    Refactor

--- src/components/screens/Help/Screen.js
+++ src/components/screens/Help/Screen.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Linking } from 'react-native';
 import Help from '../../routes/Help';
+import env from '../../../config/env';
 
 export default class Screen extends React.Component {
   _goBack = () => this.props.navigation.goBack();
-  _onPressItem = (id) => alert(id);
+  _onPressItem = (id) => {
+    let url;
+    switch(id) {
+      case 'faq':
+        url = env.FAQ_URL;
+        break;
+      case 'contact':
+        url = env.CONTACT_URL;
+        break;
+      case 'copyright':
+      case 'terms':
+        url = env.LEGALITY_URL;
+        break;
+      default:
+        url = env.APP_URL;
+        break;
+    }
+    Linking.openURL(url)
+      .catch(err => console.error('An error occured', err));
+  };
+
   render() {
     return (
       <Help
--- src/config/env.js
+++ src/config/env.js
@@ -1,15 +1,19 @@
 import moment from 'moment';
 const date = moment().format('YYYY-MM-DD');
+const url = 'https://schdlr-ng.herokuapp.com';
 const dev = {
   ONE_SIGNAL_ID: 'c45c9feb-9c17-4f6b-a397-abc7b49e5e40',
-  APP_URL: 'https://schdlr-ng.herokuapp.com',
+  APP_URL: url,
   IOS_URL: '//:schdlr',
   PROTOCOL: 'http://',
   DOMAIN: '192.168.43.190:4000',
   PORT: '4000',
   EMAIL: 'usmansbk@gmail.com',
   APP_VERSION: `${date}-beta-dev`,
-  DOWNLOAD_URL: 'https://schdlr-ng.herokuapp.com/download'
+  DOWNLOAD_URL: `${url}/download`,
+  FAQ_URL: `${url}/faq`,
+  CONTACT_URL: `${url}/contact`,
+  LEGALITY_URL: `${url}/legal`
 };
 
 const prod = {
@@ -20,7 +24,10 @@ const prod = {
   DOMAIN: 'schdlr-ng.herokuapp.com',
   EMAIL: 'usmansbk@gmail.com',
   APP_VERSION: `1.0.0-release`,
-  DOWNLOAD_URL: 'https://schdlr-ng.herokuapp.com/download'
+  DOWNLOAD_URL: 'https://schdlr-ng.herokuapp.com/download',
+  FAQ_URL: `${url}/faq`,
+  CONTACT_URL: `${url}/contact`,
+  LEGALITY_URL: `${url}/legal`
 };
 
 export default dev;
