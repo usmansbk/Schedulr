9ae653e0d50dac0ca9c8854e8cf595676c937163
Usman Suleiman <usmansbk@gmail.com>
2018-11-30 01:13:55 +0100

    Fix section header

--- src/components/lists/Events/SectionHeader.js
+++ src/components/lists/Events/SectionHeader.js
@@ -5,21 +5,24 @@ import { Text } from 'react-native-paper';
 import capitalizr from '../../../lib/capitalizr';
 import styles from './styles';
 
+const DATE_FORMAT = 'MMMM DD, YYYY';
+const DAY_FORMAT = 'dddd';
+
 const getFormattedDate = (date) => {
   const parsedDate = Date.parse(date);
   let heading = moment(parsedDate).calendar(null, {
-    sameElse: function () { return 'MMMM DD, YYYY' },
+    sameElse: function () { return DATE_FORMAT },
   });
   let index = heading.indexOf(' at ');
-  let SUBHEADING_FORMAT = 'dddd';
+  let SUBHEADING_FORMAT = DAY_FORMAT;
   if (index !== -1) {
     heading = heading.substring(0, index);
-    SUBHEADING_FORMAT = 'MMMM DD, YYYY';
+    SUBHEADING_FORMAT = DATE_FORMAT;
   }
   const subheading = moment(parsedDate).format(SUBHEADING_FORMAT);
-  const today = moment().format(SUBHEADING_FORMAT);
+  const today = moment().format(DATE_FORMAT);
   let timeAgo = '';
-  const isToday = subheading === today;
+  const isToday = moment(subheading).format(DATE_FORMAT) === today;
   if (!isToday) timeAgo = moment(parsedDate).startOf('d').fromNow();
   return {
     heading,
