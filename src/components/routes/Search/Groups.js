68c4d91d18fca3be7d3fbd53b02592d10bca195a
Usman Suleiman <usmansbk@gmail.com>
2018-12-01 23:11:02 +0100

    Refactor

--- src/components/routes/Search/Groups.js
+++ src/components/routes/Search/Groups.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
 import List from '../../lists/Groups';
 
 export default class Groups extends React.PureComponent {
   render() {
     return (
-      <View style={styles.container}>
-        <List />
-      </View>
+      <List />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-})
