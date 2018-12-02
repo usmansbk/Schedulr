0f6600fc639ecbe6475bf53a130db99eb52d36f5
Usman Suleiman <usmansbk@gmail.com>
2018-12-02 06:38:19 +0100

    Refactor

--- src/components/lists/EventSearch/styles.js
+++ src/components/lists/EventSearch/styles.js
@@ -38,7 +38,7 @@ export default StyleSheet.create({
   },
   itemContent: {
     flex: 1,
-    paddingHorizontal: 8,
+    paddingHorizontal: 16,
     justifyContent: 'center'
   },
   itemBody: {
--- src/components/lists/Groups/Footer.js
+++ src/components/lists/Groups/Footer.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Text } from 'react-native-paper';
 import styles from './styles';
 
 export default ({ visible }) => visible ? (
-  <View style={styles.footer} />
+  <View style={styles.footer}>
+    <Text style={styles.footerText}>No more groups</Text>
+  </View>
 ) : null;
