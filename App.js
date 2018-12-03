682b1eaa6cf7db787427225677bc7e441e5fbd7f
Usman Suleiman <usmansbk@gmail.com>
2018-12-03 02:09:44 +0100

    Setup amplify

--- App.js
+++ App.js
@@ -2,8 +2,12 @@ import React from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { MenuProvider } from 'react-native-popup-menu';
 import SplashScreen from 'react-native-splash-screen';
+import Amplify from 'aws-amplify';
 import AppContainer from './src/App';
+import config from './src/aws-exports';
 import theme from './src/config/theme';
+
+Amplify.configure(config);
 
 export default class App extends React.Component {
   componentDidMount = () => {
