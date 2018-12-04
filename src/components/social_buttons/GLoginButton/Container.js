ca9edd8847d649257fc78d8276516934802793f1
Usman Suleiman <usmansbk@gmail.com>
2018-12-04 23:14:09 +0100

    Refactor container

--- src/components/social_buttons/GLoginButton/Container.js
+++ src/components/social_buttons/GLoginButton/Container.js
@@ -3,6 +3,7 @@ import {
   GoogleSignin,
   statusCodes
 } from 'react-native-google-signin';
+import firebase from 'react-native-firebase';
 import Button from './Button';
 import env from '../../../config/env';
 
@@ -17,21 +18,23 @@ export default class Container extends React.Component {
       await GoogleSignin.hasPlayServices();
       const {
         idToken,
-        user: {
-          email,
-          name,
-        }
+        user
       } = await GoogleSignin.signIn();
-      alert(`${email} ${name} ${idToken}`);
+      await this.props.onLogin({
+        name: user.name,
+        email: user.email,
+        identity: 'google',
+        token: idToken,
+        expires_at: null
+      });
     } catch (error) {
       if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-
-      } else if (error.code === statusCodes.IN_PROGRSS) {
-
+        alert('Login cancelled');
       } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-
+        alert("Google Play services not available");
       } else {
-        alert(error.message)
+        alert(error.message);
+        firebase.crashlytics().log(error.message);
       }
     }
   }
