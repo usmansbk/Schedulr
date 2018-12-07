65d742bf681393dc711a2cb4e73bda4b9b0d6424
Usman Suleiman <usmansbk@gmail.com>
2018-12-07 04:42:19 +0100

    Remove duplicate

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { MenuProvider } from 'react-native-popup-menu';
+import { ApolloProvider } from 'react-apollo';
 import SplashScreen from 'react-native-splash-screen';
 import Amplify from 'aws-amplify';
 import AppContainer from './src/App';
 import config from './src/aws-exports';
+import client from './src/config/client';
 import theme from './src/config/theme';
 
 Amplify.configure(config);
@@ -17,9 +19,11 @@ export default class App extends React.Component {
   render() {
     return (
       <MenuProvider backHandler={true}>
-        <PaperProvider theme={theme}>
-          <AppContainer />
-        </PaperProvider>
+        <ApolloProvider client={client}>
+          <PaperProvider theme={theme}>
+            <AppContainer />
+          </PaperProvider>
+        </ApolloProvider>
       </MenuProvider>
     );
   }
