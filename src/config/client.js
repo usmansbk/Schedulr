7c7e0e33f2462a5ee6e0fc52315b27cd5ec5a0fa
Usman Suleiman <usmansbk@gmail.com>
2018-12-09 14:13:19 +0100

    Refactor

--- src/config/client.js
+++ src/config/client.js
@@ -1,11 +1,11 @@
-import AWSAppSyncClient, { AUTH_TYPE } from 'aws-appsync';
+import AWSAppSyncClient from 'aws-appsync';
 import aws_config from '../aws-exports';
 
 export default client = new AWSAppSyncClient({
   url: aws_config.aws_appsync_graphqlEndpoint,
   region: aws_config.aws_appsync_region,
   auth: {
-    type: AUTH_TYPE.API_KEY,
+    type: aws_config.aws_appsync_authenticationType,
     apiKey: aws_config.aws_appsync_apiKey,
   }
 });
