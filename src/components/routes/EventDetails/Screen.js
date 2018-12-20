b3b1b197d6bf0e7f1423e72e95429a83771afbf4
Usman Suleiman <usmansbk@gmail.com>
2018-12-20 23:10:34 +0100

    Refactor

--- src/components/routes/EventDetails/Screen.js
+++ src/components/routes/EventDetails/Screen.js
@@ -1,92 +1,136 @@
 import React from 'react';
+import moment from 'moment';
 import { Appbar } from 'react-native-paper';
 import Details from './Details';
 import styles from '../../../config/styles';
 import colors from '../../../config/colors';
+import { formatDate, getNextDate } from '../../../lib/time';
+import {decapitalize} from '../../../lib/capitalizr';
 
-export default ({
-  id,
-  title,
-  date,
-  nextDate,
-  eventType,
-  location,
-  groupName,
-  groupId,
-  repeat,
-  createdAt,
-  updatedAt,
-  description,
-  starred,
-  starsCount,
-  commentsCount,
-  isAuthor,
-  isValid,
-  isCancelled,
-  handleBack,
-  handleDelete,
-  handleRepeat,
-  handleEdit,
-  handleCancel,
-  navigateToGroup,
-  navigateToComments,
-}) => (
-  <React.Fragment>
-    <Appbar.Header style={styles.header}  collapsable>
-      <Appbar.BackAction color={colors.gray} onPress={handleBack} />
-      <Appbar.Content titleStyle={styles.headerColor} title="Details" />
-      {
-        isAuthor && (
-          <React.Fragment>
-            <Appbar.Action
-              icon="delete"
-              color={colors.gray}
-              onPress={handleDelete}
-            />
-            <Appbar.Action
-              icon="repeat"
-              color={colors.gray}
-              onPress={handleRepeat}
-            />
-            {
-              isValid && (
-                <React.Fragment>
-                  <Appbar.Action
-                    icon="mode-edit"
-                    color={colors.gray}
-                    onPress={handleEdit}
-                  />
-                  <Appbar.Action
-                    icon="close"
-                    color={color.gray}
-                    onPress={handleCancel}
-                  />
-                </React.Fragment>
-              )
-            }
-          </React.Fragment>
-        )
-      }
-    </Appbar.Header>
-    <Details
-      id={id}
-      title={title}
-      date={date}
-      nextDate={nextDate}
-      eventType={eventType}
-      location={location}
-      groupName={groupName}
-      groupId={groupId}
-      repeat={repeat}
-      createdAt={createdAt}
-      updatedAt={updatedAt}
-      description={description}
-      starred={starred}
-      starsCount={starsCount}
-      commentsCount={commentsCount}
-      isCancelled={isCancelled}
-      navigateToGroup={navigateToGroup}
-      navigateToComments={navigateToComments}
-    />
-  </React.Fragment>
-)
+const CREATED_DATE_FORMAT = "ddd DD, MMM YYYY, hh:mm a";
+
+export default class EventDetails extends React.Component {
+  static defaultProps = {
+    id: 1,
+    title: '(No title)',
+    startAt: moment().toISOString(),
+    endAt: moment().add(1, 'h').toISOString(),
+    eventType: 'WORK',
+    location: null,
+    group: {
+      name: 'Dev Mode Group',
+      id: 2,
+    },
+    allDay: false,
+    repeat: 'WEEKLY',
+    createdAt: moment().toISOString(),
+    updatedAt: moment().toISOString(),
+    description: 'No description',
+    isCancelled: false,
+    starred: false,
+    starsCount: 1000,
+    commentsCount: 240,
+    isAuthor: true,
+  };
+  
+  _getRepeatDate = () => {
+    const {
+      startAt,
+      repeat,
+      endAt
+    } = this.props;
+    if (repeat === 'NEVER') return '';
+    return moment(getNextDate(startAt, repeat, startAt, endAt)).format(CREATED_DATE_FORMAT)
+  }
+  _isValid = (isCancelled, endAt) => (!isCancelled) && (Date.now() < Date.parse(endAt));
+
+  render() {
+    const {
+      id,
+      title,
+      eventType,
+      location,
+      group,
+      repeat,
+      createdAt,
+      updatedAt,
+      description,
+      starred,
+      starsCount,
+      commentsCount,
+      isAuthor,
+      isValid,
+      isCancelled,
+      handleBack,
+      handleDelete,
+      handleRepeat,
+      handleEdit,
+      handleCancel,
+      navigateToGroup,
+      navigateToComments,
+    } = this.props;
+
+    return (
+      <React.Fragment>
+        <Appbar.Header style={styles.header}  collapsable>
+          <Appbar.BackAction color={colors.gray} onPress={handleBack} />
+          <Appbar.Content titleStyle={styles.headerColor} title="Details" />
+          {
+            isAuthor && (
+              <React.Fragment>
+                <Appbar.Action
+                  icon="delete"
+                  color={colors.gray}
+                  onPress={handleDelete}
+                />
+                <Appbar.Action
+                  icon="repeat"
+                  color={colors.gray}
+                  onPress={handleRepeat}
+                />
+                {
+                  isValid && (
+                    <React.Fragment>
+                      <Appbar.Action
+                        icon="mode-edit"
+                        color={colors.gray}
+                        onPress={handleEdit}
+                      />
+                      <Appbar.Action
+                        icon="close"
+                        color={color.gray}
+                        onPress={handleCancel}
+                      />
+                    </React.Fragment>
+                  )
+                }
+              </React.Fragment>
+            )
+          }
+        </Appbar.Header>
+        <Details
+          id={id}
+          title={title}
+          date={formatDate(startAt, endAt, allDay)}
+          nextDate={this._getRepeatDate()}
+          eventType={decapitalize(eventType)}
+          location={location && location.address}
+          groupName={group.name}
+          groupId={group.id}
+          repeat={decapitalize(repeat)}
+          createdAt={moment(createdAt).format(CREATED_DATE_FORMAT)}
+          updatedAt={moment(updatedAt).format(CREATED_DATE_FORMAT)}
+          description={description}
+          starred={starred}
+          starsCount={starsCount}
+          commentsCount={commentsCount}
+          isAuthor={isAuthor}
+          isValid={this._isValid(isCancelled, endAt)}
+          isCancelled={isCancelled}
+          navigateToGroup={navigateToGroup}
+          navigateToComments={navigateToComments}
+        />
+      </React.Fragment>
+    )
+  }
+}
--- src/components/screens/EventDetails/Details.js
+++ src/components/screens/EventDetails/Details.js
@@ -1,48 +1,12 @@
 import React from 'react';
-import moment from 'moment';
 import Details from '../../routes/EventDetails';
 import DeleteDialog from '../../dialogs/DeleteEvent';
 import CancelDialog from '../../dialogs/CancelEvent';
 import EditDialog from '../../dialogs/EditEvent';
-import { formatDate, getNextDate } from '../../../lib/time';
-import {decapitalize} from '../../../lib/capitalizr';
-
-const CREATED_DATE_FORMAT = "ddd DD, MMM YYYY, hh:mm a";
 
 export default class DetailsScreen extends React.Component {
-  static defaultProps = {
-    id: 1,
-    title: '(No title)',
-    startAt: moment().toISOString(),
-    endAt: moment().add(1, 'h').toISOString(),
-    eventType: 'WORK',
-    location: null,
-    group: {
-      name: 'Dev Mode Group',
-      id: 2,
-    },
-    allDay: false,
-    repeat: 'WEEKLY',
-    createdAt: moment().toISOString(),
-    updatedAt: moment().toISOString(),
-    description: 'No description',
-    isCancelled: false,
-    starred: false,
-    starsCount: 1000,
-    commentsCount: 240,
-    isAuthor: true,
-  };
   state = {
     visibleDialog: null,
-  }
-  _getRepeatDate = () => {
-    const {
-      startAt,
-      repeat,
-      endAt
-    } = this.props;
-    if (repeat === 'NEVER') return '';
-    return moment(getNextDate(startAt, repeat, startAt, endAt)).format(CREATED_DATE_FORMAT)
   }
   _goBack = () => this.props.navigation.goBack();
   _openDeleteDialog = () => this.setState({ visibleDialog: 'delete' });
@@ -53,76 +17,39 @@ export default class DetailsScreen extends React.Component {
   _handleCancel = ({ id, option }) => alert(`${id} - ${option}`);
   _navigateToGroup = (id) => this.props.navigation.navigate('GroupEvents', { id });
   _navigateToComments = (id) => this.props.navigation.navigate('Comments', { id });
-  _isValid = (isCancelled, endAt) => (!isCancelled) && (Date.now() < Date.parse(endAt));
   _hideDialog = () => this.setState({ visibleDialog: false });
   
   render() {
     const { visibleDialog } = this.state;
-    const {
-      id,
-      title,
-      startAt,
-      endAt,
-      eventType,
-      location,
-      group,
-      repeat,
-      allDay,
-      createdAt,
-      updatedAt,
-      description,
-      isCancelled,
-      starred,
-      starsCount,
-      commentsCount,
-      isAuthor,
-    } = this.props;
+    const { id } = this.props;
     return (
       <React.Fragment>
-      <Details
-        id={id}
-        title={title}
-        date={formatDate(startAt, endAt, allDay)}
-        nextDate={this._getRepeatDate()}
-        eventType={decapitalize(eventType)}
-        location={location && location.address}
-        groupName={group.name}
-        groupId={group.id}
-        repeat={decapitalize(repeat)}
-        createdAt={moment(createdAt).format(CREATED_DATE_FORMAT)}
-        updatedAt={moment(updatedAt).format(CREATED_DATE_FORMAT)}
-        description={description}
-        starred={starred}
-        starsCount={starsCount}
-        commentsCount={commentsCount}
-        isAuthor={isAuthor}
-        isValid={this._isValid(isCancelled, endAt)}
-        isCancelled={isCancelled}
-        handleBack={this._goBack}
-        handleDelete={this._openDeleteDialog}
-        handleCancel={this._openCancelDialog}
-        handleEdit={this._openEditDialog}
-        handleRepeat={this._handleRepeat}
-        navigateToGroup={this._navigateToGroup}
-        navigateToComments={this._navigateToComments}
-      />
-      <DeleteDialog
-        id={id}
-        visible={visibleDialog === 'delete'}
-        handleDismiss={this._hideDialog}
-      />
-      <CancelDialog
-        id={id}
-        visible={visibleDialog === 'cancel'}
-        handleDismiss={this._hideDialog}
-        onConfirm={this._handleCancel}
-      />
-      <EditDialog
-        id={id}
-        visible={visibleDialog === 'edit'}
-        handleDismiss={this._hideDialog}
-        onConfirm={this._handleEdit}
-      />
+        <Details
+          handleBack={this._goBack}
+          handleDelete={this._openDeleteDialog}
+          handleCancel={this._openCancelDialog}
+          handleEdit={this._openEditDialog}
+          handleRepeat={this._handleRepeat}
+          navigateToGroup={this._navigateToGroup}
+          navigateToComments={this._navigateToComments}
+        />
+        <DeleteDialog
+          id={id}
+          visible={visibleDialog === 'delete'}
+          handleDismiss={this._hideDialog}
+        />
+        <CancelDialog
+          id={id}
+          visible={visibleDialog === 'cancel'}
+          handleDismiss={this._hideDialog}
+          onConfirm={this._handleCancel}
+        />
+        <EditDialog
+          id={id}
+          visible={visibleDialog === 'edit'}
+          handleDismiss={this._hideDialog}
+          onConfirm={this._handleEdit}
+        />
       </React.Fragment>
     )
   }
