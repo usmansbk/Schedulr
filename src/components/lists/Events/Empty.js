03f76e0cbcb4b10d914930c7a3cc6817fae8cc75
Usman Suleiman <usmansbk@gmail.com>
2018-12-22 01:15:11 +0100

    Refactor

--- src/components/lists/Events/Empty.js
+++ src/components/lists/Events/Empty.js
@@ -6,11 +6,7 @@ import styles from './styles';
 export default (props) => (
   <View style={styles.empty}>
     <Headline style={styles.emptyTitle}>
-    {
-      props.starred ? 'Nothing to see here - yet' : (
-        'When you join a group, their events and agendas show up here'
-      )
-    }
+    {props.message}
     </Headline>
   </View>
 );
