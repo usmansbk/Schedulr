5d1a942ecb2e3d46f155a3fd334712629b04c9d7
Usman Suleiman <usmansbk@gmail.com>
2018-12-29 11:03:19 +0100

    Add analytics

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -5,7 +5,9 @@ const awsmobile =  {
     "aws_cognito_identity_pool_id": "us-east-1:a1a2b017-d352-47fb-8ef3-a3b9e5185e67",
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_NlWZD8V4X",
-    "aws_user_pools_web_client_id": "78q5rb9g7ag39ljb9uk6prfadl"
+    "aws_user_pools_web_client_id": "78q5rb9g7ag39ljb9uk6prfadl",
+    "aws_mobile_analytics_app_id": "af044914af834552a3bdb9530f4cbed7",
+    "aws_mobile_analytics_app_region": "us-east-1"
 };
 
 
