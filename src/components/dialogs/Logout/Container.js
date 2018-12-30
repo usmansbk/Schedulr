2ea32e60e48db7da63f0ffa5346cae5bb394c408
Usman Suleiman <usmansbk@gmail.com>
2018-12-30 23:21:16 +0100

    Reset cache on logout

--- src/components/dialogs/Logout/Container.js
+++ src/components/dialogs/Logout/Container.js
@@ -17,10 +17,11 @@ class Container extends React.Component {
       await this._awsSignOut();
       await this._fbLogout();
       await this._googleSignout();
-      await client.resetStore();
+      //await client.resetStore();
     } catch {
       // offline
     }
+    await client.cache.reset();
     this.setState({ loading: false });
     this._handleDismiss();
     this.props.navigation.navigate('Auth');
