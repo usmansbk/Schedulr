6f1b8f3c3b8b4092fba7f95be5eb4cbb660ae021
Usman Suleiman <usmansbk@gmail.com>
2018-12-31 06:52:27 +0100

    Fix grammer

--- src/components/lists/Events/List.js
+++ src/components/lists/Events/List.js
@@ -42,7 +42,7 @@ class List extends React.Component {
     } else if (listType === 'starred') {
       message = 'No important event - yet'
     } else {
-      message = 'When you follow a board, their schedules show up here';
+      message = 'When you follow a board, their schedule show up here';
     }
     return <Empty message={message} />
   };
