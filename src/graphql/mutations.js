35dea7b32017743e0aa5de1af8c9eb1bd845b4db
Usman Suleiman <usmansbk@gmail.com>
2019-01-03 10:38:55 +0100

    Update mutations

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -26,34 +26,6 @@ export const createBoard = `mutation CreateBoard($input: CreateBoardInput!) {
       pictureUrl
     }
     followersCount
-    followers {
-      nextToken
-      items {
-        id
-        name
-        email
-        pictureUrl
-      }
-    }
-    events {
-      nextToken
-      items {
-        id
-        title
-        description
-        startAt
-        endAt
-        allDay
-        isCancelled
-        cancelledDates
-        starsCount
-        isStarred
-        isAuthor
-        commentsCount
-        createdAt
-        updatedAt
-      }
-    }
     createdAt
     updatedAt
   }
@@ -66,44 +38,6 @@ export const updateBoard = `mutation UpdateBoard($input: UpdateBoardInput!) {
     description
     status
     isPublic
-    isFollowing
-    isAuthor
-    author {
-      id
-      name
-      email
-      pictureUrl
-    }
-    followersCount
-    followers {
-      nextToken
-      items {
-        id
-        name
-        email
-        pictureUrl
-      }
-    }
-    events {
-      nextToken
-      items {
-        id
-        title
-        description
-        startAt
-        endAt
-        allDay
-        isCancelled
-        cancelledDates
-        starsCount
-        isStarred
-        isAuthor
-        commentsCount
-        createdAt
-        updatedAt
-      }
-    }
-    createdAt
     updatedAt
   }
 }
@@ -111,49 +45,6 @@ export const updateBoard = `mutation UpdateBoard($input: UpdateBoardInput!) {
 export const deleteBoard = `mutation DeleteBoard($input: DeleteBoardInput!) {
   deleteBoard(input: $input) {
     id
-    name
-    description
-    status
-    isPublic
-    isFollowing
-    isAuthor
-    author {
-      id
-      name
-      email
-      pictureUrl
-    }
-    followersCount
-    followers {
-      nextToken
-      items {
-        id
-        name
-        email
-        pictureUrl
-      }
-    }
-    events {
-      nextToken
-      items {
-        id
-        title
-        description
-        startAt
-        endAt
-        allDay
-        isCancelled
-        cancelledDates
-        starsCount
-        isStarred
-        isAuthor
-        commentsCount
-        createdAt
-        updatedAt
-      }
-    }
-    createdAt
-    updatedAt
   }
 }
 `;
@@ -173,34 +64,6 @@ export const followBoard = `mutation FollowBoard($input: FollowBoardInput!) {
       pictureUrl
     }
     followersCount
-    followers {
-      nextToken
-      items {
-        id
-        name
-        email
-        pictureUrl
-      }
-    }
-    events {
-      nextToken
-      items {
-        id
-        title
-        description
-        startAt
-        endAt
-        allDay
-        isCancelled
-        cancelledDates
-        starsCount
-        isStarred
-        isAuthor
-        commentsCount
-        createdAt
-        updatedAt
-      }
-    }
     createdAt
     updatedAt
   }
@@ -209,97 +72,15 @@ export const followBoard = `mutation FollowBoard($input: FollowBoardInput!) {
 export const unfollowBoard = `mutation UnfollowBoard($input: UnfollowBoardInput!) {
   unfollowBoard(input: $input) {
     id
-    name
-    description
-    status
-    isPublic
     isFollowing
-    isAuthor
-    author {
-      id
-      name
-      email
-      pictureUrl
-    }
     followersCount
-    followers {
-      nextToken
-      items {
-        id
-        name
-        email
-        pictureUrl
-      }
-    }
-    events {
-      nextToken
-      items {
-        id
-        title
-        description
-        startAt
-        endAt
-        allDay
-        isCancelled
-        cancelledDates
-        starsCount
-        isStarred
-        isAuthor
-        commentsCount
-        createdAt
-        updatedAt
-      }
-    }
-    createdAt
-    updatedAt
   }
 }
 `;
 export const closeBoard = `mutation CloseBoard($input: CloseBoardInput!) {
   closeBoard(input: $input) {
     id
-    name
-    description
     status
-    isPublic
-    isFollowing
-    isAuthor
-    author {
-      id
-      name
-      email
-      pictureUrl
-    }
-    followersCount
-    followers {
-      nextToken
-      items {
-        id
-        name
-        email
-        pictureUrl
-      }
-    }
-    events {
-      nextToken
-      items {
-        id
-        title
-        description
-        startAt
-        endAt
-        allDay
-        isCancelled
-        cancelledDates
-        starsCount
-        isStarred
-        isAuthor
-        commentsCount
-        createdAt
-        updatedAt
-      }
-    }
-    createdAt
     updatedAt
   }
 }
@@ -307,48 +88,7 @@ export const closeBoard = `mutation CloseBoard($input: CloseBoardInput!) {
 export const openBoard = `mutation OpenBoard($input: OpenBoardInput!) {
   openBoard(input: $input) {
     id
-    name
-    description
     status
-    isPublic
-    isFollowing
-    isAuthor
-    author {
-      id
-      name
-      email
-      pictureUrl
-    }
-    followersCount
-    followers {
-      nextToken
-      items {
-        id
-        name
-        email
-        pictureUrl
-      }
-    }
-    events {
-      nextToken
-      items {
-        id
-        title
-        description
-        startAt
-        endAt
-        allDay
-        isCancelled
-        cancelledDates
-        starsCount
-        isStarred
-        isAuthor
-        commentsCount
-        createdAt
-        updatedAt
-      }
-    }
-    createdAt
     updatedAt
   }
 }
@@ -372,13 +112,6 @@ export const createEvent = `mutation CreateEvent($input: CreateEventInput!) {
     board {
       id
       name
-      description
-      isPublic
-      isFollowing
-      isAuthor
-      followersCount
-      createdAt
-      updatedAt
     }
     cancelledDates
     starsCount
@@ -391,16 +124,6 @@ export const createEvent = `mutation CreateEvent($input: CreateEventInput!) {
       pictureUrl
     }
     commentsCount
-    comments {
-      nextToken
-      items {
-        id
-        content
-        isAuthor
-        createdAt
-        updatedAt
-      }
-    }
     createdAt
     updatedAt
   }
@@ -422,38 +145,6 @@ export const updateEvent = `mutation UpdateEvent($input: UpdateEventInput!) {
     isCancelled
     repeat
     eventType
-    board {
-      id
-      name
-      description
-      isPublic
-      isFollowing
-      isAuthor
-      followersCount
-      createdAt
-      updatedAt
-    }
-    cancelledDates
-    starsCount
-    isStarred
-    isAuthor
-    author {
-      id
-      name
-      email
-      pictureUrl
-    }
-    commentsCount
-    comments {
-      nextToken
-      items {
-        id
-        content
-        isAuthor
-        createdAt
-        updatedAt
-      }
-    }
     createdAt
     updatedAt
   }
@@ -462,105 +153,14 @@ export const updateEvent = `mutation UpdateEvent($input: UpdateEventInput!) {
 export const deleteEvent = `mutation DeleteEvent($input: DeleteEventInput!) {
   deleteEvent(input: $input) {
     id
-    title
-    description
-    location {
-      address
-      latitude
-      longitude
-    }
-    startAt
-    endAt
-    allDay
-    isCancelled
-    repeat
-    eventType
-    board {
-      id
-      name
-      description
-      isPublic
-      isFollowing
-      isAuthor
-      followersCount
-      createdAt
-      updatedAt
-    }
-    cancelledDates
-    starsCount
-    isStarred
-    isAuthor
-    author {
-      id
-      name
-      email
-      pictureUrl
-    }
-    commentsCount
-    comments {
-      nextToken
-      items {
-        id
-        content
-        isAuthor
-        createdAt
-        updatedAt
-      }
-    }
-    createdAt
-    updatedAt
   }
 }
 `;
 export const cancelEvent = `mutation CancelEvent($input: CancelEventInput!) {
   cancelEvent(input: $input) {
     id
-    title
-    description
-    location {
-      address
-      latitude
-      longitude
-    }
-    startAt
-    endAt
-    allDay
     isCancelled
-    repeat
-    eventType
-    board {
-      id
-      name
-      description
-      isPublic
-      isFollowing
-      isAuthor
-      followersCount
-      createdAt
-      updatedAt
-    }
     cancelledDates
-    starsCount
-    isStarred
-    isAuthor
-    author {
-      id
-      name
-      email
-      pictureUrl
-    }
-    commentsCount
-    comments {
-      nextToken
-      items {
-        id
-        content
-        isAuthor
-        createdAt
-        updatedAt
-      }
-    }
-    createdAt
     updatedAt
   }
 }
@@ -568,106 +168,16 @@ export const cancelEvent = `mutation CancelEvent($input: CancelEventInput!) {
 export const starEvent = `mutation StarEvent($input: StarEventInput!) {
   starEvent(input: $input) {
     id
-    title
-    description
-    location {
-      address
-      latitude
-      longitude
-    }
-    startAt
-    endAt
-    allDay
-    isCancelled
-    repeat
-    eventType
-    board {
-      id
-      name
-      description
-      isPublic
-      isFollowing
-      isAuthor
-      followersCount
-      createdAt
-      updatedAt
-    }
-    cancelledDates
     starsCount
     isStarred
-    isAuthor
-    author {
-      id
-      name
-      email
-      pictureUrl
-    }
-    commentsCount
-    comments {
-      nextToken
-      items {
-        id
-        content
-        isAuthor
-        createdAt
-        updatedAt
-      }
-    }
-    createdAt
-    updatedAt
   }
 }
 `;
 export const unstarEvent = `mutation UnstarEvent($input: UnstarEventInput!) {
   unstarEvent(input: $input) {
     id
-    title
-    description
-    location {
-      address
-      latitude
-      longitude
-    }
-    startAt
-    endAt
-    allDay
-    isCancelled
-    repeat
-    eventType
-    board {
-      id
-      name
-      description
-      isPublic
-      isFollowing
-      isAuthor
-      followersCount
-      createdAt
-      updatedAt
-    }
-    cancelledDates
     starsCount
     isStarred
-    isAuthor
-    author {
-      id
-      name
-      email
-      pictureUrl
-    }
-    commentsCount
-    comments {
-      nextToken
-      items {
-        id
-        content
-        isAuthor
-        createdAt
-        updatedAt
-      }
-    }
-    createdAt
-    updatedAt
   }
 }
 `;
@@ -683,22 +193,6 @@ export const createComment = `mutation CreateComment($input: CreateCommentInput!
       updatedAt
     }
     isAuthor
-    event {
-      id
-      title
-      description
-      startAt
-      endAt
-      allDay
-      isCancelled
-      cancelledDates
-      starsCount
-      isStarred
-      isAuthor
-      commentsCount
-      createdAt
-      updatedAt
-    }
     author {
       id
       name
@@ -713,39 +207,6 @@ export const createComment = `mutation CreateComment($input: CreateCommentInput!
 export const deleteComment = `mutation DeleteComment($input: DeleteCommentInput!) {
   deleteComment(input: $input) {
     id
-    content
-    toComment {
-      id
-      content
-      isAuthor
-      createdAt
-      updatedAt
-    }
-    isAuthor
-    event {
-      id
-      title
-      description
-      startAt
-      endAt
-      allDay
-      isCancelled
-      cancelledDates
-      starsCount
-      isStarred
-      isAuthor
-      commentsCount
-      createdAt
-      updatedAt
-    }
-    author {
-      id
-      name
-      email
-      pictureUrl
-    }
-    createdAt
-    updatedAt
   }
 }
 `;
