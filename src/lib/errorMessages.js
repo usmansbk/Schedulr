b673763b84aa15d22970f9254950d672844ad540
Usman Suleiman <usmansbk@gmail.com>
2019-01-03 15:38:55 +0100

    Fix error message

--- src/lib/errorMessages.js
+++ src/lib/errorMessages.js
@@ -1,2 +1,3 @@
 export const CANT_REPEAT ="Event's duration should be shorter than repeat frequency";
 export const INVALID_START = 'End date should be greater than start date';
+export const WRONG_TIME = "Event must end in a future date";
--- src/lib/formValidator.js
+++ src/lib/formValidator.js
@@ -1,19 +1,26 @@
 import { Alert } from 'react-native';
 import { repeatLength } from './time';
-import { CANT_REPEAT, INVALID_START } from './errorMessages';
+import { CANT_REPEAT, INVALID_START, WRONG_TIME } from './errorMessages';
 
 export const canRepeat = ({ repeat, endAt, startAt }) => {
   if (repeat === 'NEVER') return true;
-  const duration = Math.abs(Date.parse(endAt) - Date.parse(startAt));
+  const duration = Math.abs(endAt - startAt);
   return duration < repeatLength(repeat);
 };
-export function isEventValid({startAt, endAt, repeat}) {
+export function isEventValid(event) {
+  const startAt = Date.parse(event.startAt);
+  const endAt = Date.parse(event.endAt);
+  const repeat = event.repeat;
+
   let validity = true
   if (!canRepeat({ startAt, endAt, repeat })) {
     Alert.alert("Repeat", CANT_REPEAT);
     validity = false;
   } else if (startAt > endAt) {
     Alert.alert('Duration', INVALID_START);
+    validity = false;
+  } else if (endAt < Date.now() ) {
+    Alert.alert('Wrong time', WRONG_TIME);
     validity = false;
   }
   return validity;
