d9642dabf94349def8b436281468aa5c8572e4a2
Usman Suleiman <usmansbk@gmail.com>
2019-01-03 10:12:32 +0100

    Refactor

--- src/lib/parseItem.js
+++ src/lib/parseItem.js
@@ -11,10 +11,10 @@ const REMINDER = 'REMINDER'
  * @param { Event } param0
  */
 export const parseDetails = ({ eventType, repeat, endAt, allDay, startAt }) => {
-  const recurrence = repeat === 'NEVER' ? '' : decapitalize(repeat);
+  const recurrence = repeat === 'NEVER' ? '' : (decapitalize(repeat));
   if ((eventType === REMINDER) || allDay) return `${recurrence ? (recurrence + ' ' + decapitalize(eventType, true)) : eventType}`;
   const duration = moment(Date.parse(endAt)).from(Date.parse(startAt), true);
-  return capitalizr(`${duration} ${eventType.toLowerCase()}, ${recurrence.toLowerCase()}`);
+  return capitalizr(`${duration} ${eventType.toLowerCase()} ${recurrence.toLowerCase()}`);
 };
 
 /**
