3724769032690940c2023cf4cbd7aa46c1ca4cbd
Usman Suleiman <usmansbk@gmail.com>
2019-01-06 19:42:52 +0100

    Refactor

--- src/components/tabs/Boards/Boards.js
+++ src/components/tabs/Boards/Boards.js
@@ -3,20 +3,29 @@ import Toast from 'react-native-simple-toast';
 import List from '../../lists/Boards';
 import FAB from '../../common/Fab';
 
-export default (props) => {
-  if (props.error) {
-    Toast.show(props.error.message, Toast.SHORT);
+export default class Boards extends React.Component {
+  shouldComponentUpdate = (nextProps) => {
+    return ((nextProps.loading) !== this.props.loading) || (
+      nextProps.boards !== this.props.boards
+    );
   }
-  return (
-  <React.Fragment>
-    <List
-      loading={props.loading}
-      boards={props.boards}
-      onRefresh={props.onRefresh}
-    />
-    <FAB
-      icon="add"
-      onPress={() => props.navigation.navigate('NewBoard')}
-    />
-  </React.Fragment>
-)};
+  
+  render() {
+    if (this.props.error) {
+      Toast.show(this.props.error.message, Toast.SHORT);
+    }
+    return (
+      <React.Fragment>
+        <List
+          loading={this.props.loading}
+          boards={this.props.boards}
+          onRefresh={this.props.onRefresh}
+        />
+        <FAB
+          icon="add"
+          onPress={() => this.props.navigation.navigate('NewBoard')}
+        />
+      </React.Fragment>
+    )
+  }
+}
--- src/components/tabs/Events/Events.js
+++ src/components/tabs/Events/Events.js
@@ -3,23 +3,31 @@ import Toast from 'react-native-simple-toast';
 import List from '../../lists/Events';
 import FAB from '../../common/Fab';
 
-export default (props) => {
-  if (props.error) {
-    Toast.show(props.error.name, Toast.LONG);
+export default class Events extends React.Component {
+  shouldComponentUpdate = (nextProps) => {
+    return ((nextProps.loading) !== this.props.loading) || (
+      nextProps.events !== this.props.events
+    );
   }
-  return (
-    <React.Fragment>
-      <List
-        loading={props.loading}
-        events={props.events}
-        hasPreviousEvents={Boolean(props.nextToken)}
-        onRefresh={props.onRefresh}
-      />
-      <FAB
-        icon="edit"
-        onPress={() => props.navigation.navigate('NewEvent')}
-      />
-    </React.Fragment>
-  )
-};
+  
+  render() {
+    if (this.props.error) {
+      Toast.show(this.props.error.name, Toast.LONG);
+    }
+    return (
+      <React.Fragment>
+        <List
+          loading={this.props.loading}
+          events={this.props.events}
+          hasPreviousEvents={Boolean(this.props.nextToken)}
+          onRefresh={this.props.onRefresh}
+        />
+        <FAB
+          icon="edit"
+          onPress={() => this.props.navigation.navigate('NewEvent')}
+        />
+      </React.Fragment>
+    )
+  }
+}
 
