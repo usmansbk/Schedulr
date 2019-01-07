9ddb048b658c741861d007b72cd640f84d42673d
Usman Suleiman <usmansbk@gmail.com>
2019-01-08 00:57:44 +0100

    Fix: navigation

--- src/components/lists/Events/List.js
+++ src/components/lists/Events/List.js
@@ -45,7 +45,11 @@ class List extends React.Component {
   _renderSectionHeader = ({section}) => <SectionHeader section={section} />;
   _onPressItem = (id) => this.props.navigation.navigate('EventDetails', { id });
   _onPressCommentItem = (id) => this.props.navigation.navigate('Comments', { id });
-  _navigateToBoardEvents = (id) => this.props.navigation.navigate('BoardEvents', { id });
+  _navigateToBoardEvents = (id) => {
+    let screen = 'BoardEvents';
+    if (this.props.listType === 'board') screen = 'BoardInfo';
+    this.props.navigation.navigate(screen, { id })
+  };
 
   _renderItem = ({ item: {
     id,
