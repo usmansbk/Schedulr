581c32ad6eda3dab8620a71bc6ed8e8a13dbba1d
Usman Suleiman <usmansbk@gmail.com>
2019-01-08 01:12:39 +0100

    Feat: setup local notifications

--- src/components/tabs/Events/Events.js
+++ src/components/tabs/Events/Events.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { PushNotificationIOS } from 'react-native';
 import Toast from 'react-native-simple-toast';
+import PushNotification from 'react-native-push-notification';
 import List from '../../lists/Events';
 import FAB from '../../common/Fab';
 
@@ -8,6 +10,15 @@ export default class Events extends React.Component {
     return ((nextProps.loading) !== this.props.loading) || (
       nextProps.events !== this.props.events
     );
+  }
+
+  componentDidMount = () => {
+    PushNotification.configure({
+      onNotification: notification => {
+        notification.finish(PushNotificationIOS.FetchResult.NoData);
+      },
+      popInitialNotification: false
+    });
   }
   
   render() {
