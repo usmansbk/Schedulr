35321802f98e571e5fd314f9ba6234f5a30f9773
Usman Suleiman <usmansbk@gmail.com>
2019-01-08 01:47:00 +0100

    Feat: add top level navigator

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { ApolloProvider } from 'react-apollo';
 import SplashScreen from 'react-native-splash-screen';
 import Amplify from 'aws-amplify';
 import AppContainer from './src/App';
+import NavigationService from './src/config/navigation';
 import config from './src/aws-exports';
 import client from './src/config/client';
 import theme from './src/config/theme';
@@ -23,7 +24,11 @@ export default class App extends React.Component {
       <MenuProvider backHandler={true}>
         <PaperProvider theme={theme}>
           <ApolloProvider client={client}>
-            <AppContainer />
+            <AppContainer
+              ref={navigatorRef => {
+                NavigationService.setTopLevelNavigator(navigatorRef);
+              }}
+            />
           </ApolloProvider>
         </PaperProvider>
       </MenuProvider>
