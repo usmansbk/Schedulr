75257e7338d4cfdada823e61503607adeafdbfb7
Usman Suleiman <usmansbk@gmail.com>
2019-01-20 23:23:13 +0100

    Refactor

--- App.js
+++ App.js
@@ -1,26 +1,35 @@
 import React from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { MenuProvider } from 'react-native-popup-menu';
+import { GoogleSignin } from 'react-native-google-signin';
 import { ApolloProvider } from 'react-apollo';
 import { Rehydrated } from 'aws-appsync-react';
 import SplashScreen from 'react-native-splash-screen';
-import Amplify from 'aws-amplify';
+import Amplify, { Auth } from 'aws-amplify';
 import AppContainer from './src/App';
 import Loading from './src/components/common/Loading';
 import NavigationService from './src/config/navigation';
 import config from './src/aws-exports';
 import client from './src/config/client';
 import theme from './src/config/theme';
-import { initRefreshHandlers } from './src/config/auth';
+import env from './src/config/env';
+import { refreshGoogleToken } from './src/config/auth';
 
 // console.disableYellowBox = true;
 
+GoogleSignin.configure({
+  webClientId: env.WEB_CLIENT_ID
+});
 Amplify.configure(config);
+Auth.configure({
+  refreshHandlers: {
+    'google': refreshGoogleToken
+  } 
+});
 
 export default class App extends React.Component {
   componentDidMount = () => {
     SplashScreen.hide();
-    initRefreshHandlers();
   }
   
   render() {
