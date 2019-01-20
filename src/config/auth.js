1d19dc654cd5c044baff074565aff3b8edaaa458
Usman Suleiman <usmansbk@gmail.com>
2019-01-20 23:24:01 +0100

    Refactor

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,30 +1,25 @@
 import { Auth } from 'aws-amplify';
 import { GoogleSignin, statusCodes } from 'react-native-google-signin';
 import Toast from 'react-native-simple-toast';
-import env from '../config/env';
 
-GoogleSignin.configure({
-  offlineAccess: false,
-  webClientId: env.WEB_CLIENT_ID
-});
-
-async function refreshGoogleToken() {
+export async function refreshGoogleToken() {
   let token = null;
   let expires_at = null;
+  console.log('refreshing');
   try {
     const {
       idToken,
       accessTokenExpirationDate
     } = await GoogleSignin.signInSilently();
-
     token = idToken;
     expires_at = accessTokenExpirationDate;
+    console.log(token);
   } catch (error) {
     if (error.code === statusCodes.SIGN_IN_REQUIRED) {
       Toast.show('Signin required', Toast.SHORT);
     }
   }
-
+  console.log('refreshed');
   return new Promise(res, rej => {
     const data = {
       token,
@@ -33,13 +28,5 @@ async function refreshGoogleToken() {
     res(data);
   });
 }
-
-export const initRefreshHandlers = () => {
-  Auth.configure({
-    refreshHandlers: {
-      'google': refreshGoogleToken
-    }
-  });
-};
 
 export default Auth;
