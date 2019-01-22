54021528f0136259923d62800a6cc5c5466170da
Usman Suleiman <usmansbk@gmail.com>
2019-01-22 10:07:42 +0100

    Update comments count

--- src/components/dialogs/DeleteComment/Hoc.js
+++ src/components/dialogs/DeleteComment/Hoc.js
@@ -1,7 +1,7 @@
 import { graphql, compose } from 'react-apollo';
 import gql from 'graphql-tag';
 import Dialog from './Dialog';
-import { listEventComments } from '../../../graphql/queries';
+import { listEventComments, getEvent } from '../../../graphql/queries';
 import { deleteComment } from '../../../graphql/mutations';
 
 export default graphql(gql(deleteComment), {
@@ -14,9 +14,16 @@ export default graphql(gql(deleteComment), {
     update: (cache, { data: { deleteComment } }) => {
       if (deleteComment) {
         const query = gql(listEventComments);
+        const eventQuery = gql(getEvent);
+
         const data = cache.readQuery({ query, variables: { id: props.eventId } });
+        const prevEventData = cache.readQuery({ query: eventQuery, variables: { id: props.eventId } });
+
         data.listComments.items = data.listComments.items.filter(item => item.id !== deleteComment.id);
+        prevEventData.getEvent.commentsCount = prevEventData.getEvent.commentsCount - 1;
+
         cache.writeQuery({ query, data });
+        cache.writeData({ query: eventQuery, data: prevEventData });
       }
     },
     optimisticResponse: () => ({
