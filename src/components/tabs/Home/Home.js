59fdf09102dd441cc288f87cf2c40407e4ef7f3d
Usman Suleiman <usmansbk@gmail.com>
2019-01-24 14:59:19 +0100

    Refactor

--- src/components/tabs/Home/Home.js
+++ src/components/tabs/Home/Home.js
@@ -12,7 +12,7 @@ const Home = createMaterialTopTabNavigator({
   Boards: { screen: BoardsRoute },
   Starred: { screen: StarredRoute },
 }, {
-  lazy: false,
+  lazy: true,
   initialRouteName: 'Events',
   initialLayout: { height: 0, width: Dimensions.get('window').width },
   tabBarOptions: {
