dbf0158f2b8183a09680d89915894e66e8c7730e
Usman Suleiman <usmansbk@gmail.com>
2019-01-25 03:15:19 +0100

    Use navigation tabs

--- src/components/tabs/Search/Tabs.js
+++ src/components/tabs/Search/Tabs.js
@@ -1,79 +1,34 @@
-import React from 'react';
-import { Dimensions, StyleSheet } from 'react-native';
 import {
-  TabView,
-  TabBar,
-} from 'react-native-tab-view';
+  createMaterialTopTabNavigator,
+  createAppContainer
+} from 'react-navigation';
+import { StyleSheet } from 'react-native';
 import Events from './Events';
 import Boards from './Boards';
 import colors from '../../../config/colors';
 
 const styles = StyleSheet.create({
-  container: {flex: 1},
   barStyle: {
-    backgroundColor: 'white',
-    elevation: 4
+    elevation: 4,
+    backgroundColor: '#fff',
   },
   indicatorStyle: {
     backgroundColor: colors.primary_light
-  },
-  label: {
-    color: colors.black
   }
 });
 
-const initialLayout = {
-  height: 0,
-  width: Dimensions.get('window').width,
-};
-
-export default class Tab extends React.Component {
-  static title = "Search bar";
-  static backgroundColor = colors.primary_light;
-  static appbarElevation = 2;
-
-  state = {
-    index: 0,
-    routes: [
-      { key: 'boards', title: 'Boards' },
-      { key: 'events', title: 'Events' },
-    ],
-  };
-
-  _handleIndexChange = index => this.setState({ index });
-  _getLabelText = ({ route }) => (route.title);
-  _renderTabBar = props => (
-    <TabBar
-      {...props}
-      indicatorStyle={styles.indicatorStyle}
-      style={styles.barStyle}
-      labelStyle={styles.label}
-      getLabelText={this._getLabelText}
-    />
-  );
-  _renderScene = ({ route }) => {
-    const { query } = this.props;
-    switch(route.key) {
-      case 'boards':
-        return <Boards query={query} />;
-      case 'events':
-        return <Events query={query} />;
-      default:
-        return null;
-    }
-  };
-
-  render() {
-    return (
-      <TabView
-        lazy
-        style={[styles.container]}
-        navigationState={this.state}
-        renderScene={this._renderScene}
-        renderTabBar={this._renderTabBar}
-        onIndexChange={this._handleIndexChange}
-        initialLayout={initialLayout}
-      />
-    )
+export default createAppContainer(createMaterialTopTabNavigator(
+  {
+    Events,
+    Boards
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: colors.primary,
+      inactiveTintColor: colors.gray,
+      upperCaseLabel: false,
+      indicatorStyle: styles.indicatorStyle,
+      style: styles.barStyle
+    },
   }
-}
+));
