1243847d65391cf427ebfabcbe94975e7dd283d4
Usman Suleiman <usmansbk@gmail.com>
2019-01-25 03:39:56 +0100

    Refactor font size

--- src/components/tabs/Home/Home.js
+++ src/components/tabs/Home/Home.js
@@ -5,7 +5,7 @@ import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import EventsRoute from '../Events';
 import BoardsRoute from '../Boards';
 import StarredRoute from '../Starred';
-import styles, { activeColor, inactiveTintColor, FONT_SIZE, BLUR_SIZE } from './styles';
+import styles, { activeColor, inactiveTintColor, FONT_SIZE } from './styles';
 
 const Home = createMaterialTopTabNavigator({
   Events: { screen: EventsRoute },
@@ -35,7 +35,7 @@ const Home = createMaterialTopTabNavigator({
       } else if (routeName === 'Starred') {
         iconName = `star`;
       }
-      return <Icon name={iconName} size={focused ? FONT_SIZE : BLUR_SIZE} color={tintColor} />
+      return <Icon name={iconName} size={FONT_SIZE} color={tintColor} />
     }
   })
 });
