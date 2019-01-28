065bbe2f6f782c10a5033328b7326cf1b9fbe546
Usman Suleiman <usmansbk@gmail.com>
2019-01-28 14:25:50 +0100

    Chore

--- src/components/lists/Boards/Empty.js
+++ src/components/lists/Boards/Empty.js
@@ -3,19 +3,16 @@ import { View } from 'react-native';
 import { Headline, Paragraph } from 'react-native-paper';
 import styles from './styles';
 
-export default ({ search, profile }) => profile ? null : (
-  <View style={styles.empty}>
-    <Headline style={styles.emptyTitle}>
-    {
-      search ? "What interests you?" : "Follow a board or create a new one"
-    }
-    </Headline>
-    {
-      search && (
-        <Paragraph style={styles.paragraph}>
-          Find a board, follow, and get notified on event updates.
-        </Paragraph>
-      )
-    }
-  </View>
-);
+export default ({ search, profile }) => {
+  let title = "Follow a board or create a new one";
+  let paragraph = "Find a board, follow, and get notified on event updates."
+  if (search) title = "What interests you?";
+  else if (profile) title = "No boards";
+
+  return (
+    <View style={styles.empty}>
+      <Headline style={styles.emptyTitle}>{title}</Headline>
+      { search && <Paragraph style={styles.paragraph}>{paragraph}</Paragraph> }
+    </View>
+  );
+};
