de7bb2ae987acbfd45a74512a124a4b365494097
Usman Suleiman <usmansbk@gmail.com>
2019-01-29 14:53:54 +0100

    Refactor

--- src/components/lists/Events/Item.js
+++ src/components/lists/Events/Item.js
@@ -62,7 +62,7 @@ export default class Item extends React.PureComponent {
           <View style={styles.right}>
             <View style={styles.itemBody}>
               <View style={styles.body}>
-                <Text style={isPending ? styles.offlineTitle : styles.itemHeadline} numberOfLines={2} ellipsizeMode="tail">{title}</Text>
+                <Text style={isPending ? styles.offlineTitle : styles.itemHeadline} numberOfLines={1} ellipsizeMode="tail">{title}</Text>
                 <Text style={styles.time}>{time}</Text>
                 <Text style={styles.duration}>{duration}{eventType}</Text>
                 <Text style={styles.status}>{repeatEvent}<Tag status={status} /></Text>
