796094621f3a08f5880c38c1ddff203844e3cb7c
Usman Suleiman <usmansbk@gmail.com>
2019-01-29 03:08:33 +0100

    Add fab

--- src/components/tabs/Search/Events.js
+++ src/components/tabs/Search/Events.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import List from '../../lists/EventSearch';
+import Fab from '../../common/Fab';
 
 export default class Events extends React.PureComponent {
   render() {
     // alert(this.props.screenProps.query);
     return (
-      <List />
+      <React.Fragment>
+        <List />
+        <Fab
+          small
+          icon="filter-list"
+          onPress={() => null}
+        />
+      </React.Fragment>
     );
   }
 }
