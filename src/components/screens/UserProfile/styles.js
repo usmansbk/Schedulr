345fee462db35aa20572159652ae7078afda2690
Usman Suleiman <usmansbk@gmail.com>
2019-02-03 18:47:40 +0100

    Chore

--- src/components/screens/UserProfile/styles.js
+++ src/components/screens/UserProfile/styles.js
@@ -17,7 +17,6 @@ export default StyleSheet.create({
   content: {
     justifyContent: 'center',
     alignItems: 'center',
-    paddingTop: 32,
     // backgroundColor: 'red'
   },
   headline: {
@@ -51,7 +50,7 @@ export default StyleSheet.create({
   count: {
     color: colors.light_gray_3,
     fontWeight: 'bold',
-    fontSize: 22,
+    fontSize: 25,
     marginTop: 8,
     marginBottom: 4
   },
