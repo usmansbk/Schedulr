943969b6817f559eee230ccc83e6f52def6bf8c1
Usman Suleiman <usmansbk@gmail.com>
2019-02-03 20:33:37 +0100

    Chore

---
+++ src/components/screens/Search/Events.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { graphql, compose } from 'react-apollo';
+import gql from 'graphql-tag';
+import List from '../../lists/EventSearch';
+import Fab from '../../common/Fab';
+import { listAllEvents } from '../../../graphql/queries';
+
+class Events extends React.PureComponent {
+  render() {
+    const {
+      screenProps: { isConnected },
+      loading,
+      events
+    } = this.props;
+    return (
+      <React.Fragment>
+        <List
+          isConnected={isConnected}
+          loading={loading}
+          events={events}
+        />
+        {
+          isConnected && (
+            <Fab
+              small
+              icon="filter-list"
+              onPress={() => null}
+            />
+          )
+        }
+      </React.Fragment>
+    );
+  }
+}
+
+export default compose(
+  graphql(gql(listAllEvents), {
+    alias: 'withSearchEventsOffline',
+    skip: props => false && props.screenProps.isConnected,
+    options: {
+      fetchPolicy: 'cache-only'
+    },
+    props: ({ data, ownProps }) => ({
+      loading: data.loading,
+      events: data && data.listAllEvents && data.listAllEvents.items.filter(
+        item => item.title.toLowerCase().includes(ownProps.screenProps.query.toLowerCase())
+      ),
+      ...ownProps
+    })
+  })
+)(Events);
---
+++ src/components/screens/Search/Tabs_Nav.js
@@ -0,0 +1,39 @@
+import {
+  createMaterialTopTabNavigator,
+  createAppContainer
+} from 'react-navigation';
+import { StyleSheet } from 'react-native';
+import Events from './Events';
+import Boards from './Boards';
+import colors from '../../../config/colors';
+
+const styles = StyleSheet.create({
+  barStyle: {
+    elevation: 4,
+    backgroundColor: '#fff',
+  },
+  indicatorStyle: {
+    backgroundColor: colors.primary_light
+  },
+  searchbar: {
+    elevation: 0
+  }
+});
+
+const Tabs = createAppContainer(createMaterialTopTabNavigator(
+  {
+    Boards,
+    Events
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: colors.primary,
+      inactiveTintColor: colors.gray,
+      upperCaseLabel: false,
+      indicatorStyle: styles.indicatorStyle,
+      style: styles.barStyle
+    },
+  }
+));
+
+export default createAppContainer(Tabs);
