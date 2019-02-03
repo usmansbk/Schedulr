e768fbe2c428c41c8fc5b2c7bf1ca160a80622ad
Usman Suleiman <usmansbk@gmail.com>
2019-02-03 19:55:08 +0100

    Fix bug

--- src/components/lists/Boards/Empty.js
+++ src/components/lists/Boards/Empty.js
@@ -5,11 +5,9 @@ import styles from './styles';
 
 export default ({ profile, error }) => {
   let title = "Follow a board or create a new one";
-  let paragraph = null;
   if (profile) title = "No boards";
   if (error) {
     title = "Network error";
-    paragraph = "Check your internet connectio. Pull to refresh".
   }
 
   return (
