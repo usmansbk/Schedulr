a4204e27957c4d3d4c269fe002bf8e2778095e27
Usman Suleiman <usmansbk@gmail.com>
2019-02-04 16:09:36 +0100

    Refactor

--- src/components/tabs/Events/Events.js
+++ src/components/tabs/Events/Events.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Linking, Platform } from 'react-native';
 import List from '../../lists/Events';
 import FAB from '../../common/Fab';
+import NavigationService from '../../../config/navigation';
 import schdlAll from '../../../helpers/setReminders';
 import SimpleToast from 'react-native-simple-toast';
 
@@ -9,6 +11,34 @@ export default class Events extends React.Component {
   
   componentDidUpdate = () => {
     schdlAll(this.props.events);
+  }
+ 
+  componentDidMount = () => {
+    Linking.addEventListener('url', this.handleOpenURL);
+  }
+
+  componentWillUnmount = () => {
+    Linking.removeEventListener('url', this.handleOpenURL);
+  }
+
+  handleOpenURL = (event) => {
+    this.navigate(event.url);
+  }
+
+  navigate = (url) => {
+    const route = url.replace(/.*?:\/\//g, '');
+    const id = route.match(/\/([^\/]+)\/?$/)[1];
+    const routeName = route.split('/')[1];
+    switch(routeName) {
+      case 'event':
+        NavigationService.navigate('EventDetails', { id });
+        break;
+      case 'board':
+        NavigationService.navigate('BoardInfo', { id });
+        break;
+      default:
+        break;
+    }
   }
 
   render() {
