30cc1d4961b7e5b1772b484b87158dd8f400b050
Usman Suleiman <usmansbk@gmail.com>
2019-02-06 05:38:07 +0100

    Refactor: add collapsible header

--- src/components/tabs/Search/Search.js
+++ src/components/tabs/Search/Search.js
@@ -1,53 +1,79 @@
-import React from 'react';
-import {
-  createMaterialTopTabNavigator,
-  createAppContainer,
-  createStackNavigator
-} from 'react-navigation';
-import { StyleSheet } from 'react-native';
-import Events from './Events';
-import Boards from './Boards';
-import Searchbar from '../../common/Searchbar';
+import React, {Component} from 'react';
+import { TouchableRipple, Searchbar, Text } from 'react-native-paper';
+import { FlatList } from 'react-navigation';
+import { Animated } from 'react-native';
 import colors from '../../../config/colors';
+import styles from './styles';
 
-const styles = StyleSheet.create({
-  barStyle: {
-    elevation: 4,
-    backgroundColor: '#fff',
-  },
-  indicatorStyle: {
-    backgroundColor: colors.primary_light
-  },
-  searchbar: {
-    elevation: 0
+import { withCollapsible } from 'react-navigation-collapsible';
+
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
+
+class SearchTab extends Component{
+  constructor(props){
+    super(props);
+
+    const data = [];
+    for(let i = 0 ; i < 60 ; i++){
+      data.push(i.toString());
+    }
+
+    this.state = {
+      data: data
+    }
   }
-});
 
-const Tabs = createAppContainer(createMaterialTopTabNavigator(
-  {
-    Boards,
-    Events
-  },
-  {
-    tabBarOptions: {
-      activeTintColor: colors.primary,
-      inactiveTintColor: colors.gray,
-      upperCaseLabel: false,
-      indicatorStyle: styles.indicatorStyle,
-      style: styles.barStyle
-    },
+  renderItem = ({item}) => (
+    <TouchableRipple 
+      onPress={() => {
+        this.props.navigation.navigate('DetailScreen');
+      }}
+      style={styles.item}>
+      <Text style={{fontSize: 22}}>{item}</Text>
+    </TouchableRipple>
+  )
+
+  render(){
+    const { paddingHeight, animatedY, onScroll } = this.props.collapsible;
+
+    return (
+      <AnimatedFlatList 
+        style={styles.list}
+        data={this.state.data}
+        renderItem={this.renderItem}
+        keyExtractor={(item, index) => String(index)}
+
+        contentContainerStyle={{paddingTop: paddingHeight}}
+        scrollIndicatorInsets={{top: paddingHeight}}        
+        onScroll={onScroll} 
+        _mustAddThis={animatedY}
+        />
+    )
   }
-));
+}
 
-const SearchStack = createStackNavigator({
-  Tabs
-}, {
-  defaultNavigationOptions: ({ navigation }) => ({
-    header: <Searchbar 
-      placeholder="Search for..."
+const SearchBar = ({navigation}) => {
+  return (
+    <TouchableRipple
       onPress={() => navigation.navigate('SearchScreen')}
-    />
-  })
-})
+    >
+      <Searchbar
+        icon="search"
+        editable={false}
+        placeholder="Search"
+      />
+    </TouchableRipple>
+  );
+}
 
-export default createAppContainer(SearchStack);
+const collapsibleParams = {
+  collapsibleComponent: SearchBar,
+  collapsibleBackgroundStyle: {
+    height: 49, 
+    backgroundColor: colors.light_gray_2,
+    // elevation: 4
+    // disableFadeoutInnerComponent: true,
+  }
+}
+
+export default withCollapsible(SearchTab, collapsibleParams);
---
+++ src/components/tabs/Search/styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  list: {
+    flex: 1
+  },
+  input: {
+    backgroundColor: 'white',
+    flex: 1,
+    borderRadius: 10,
+    margin: 10,
+    justifyContent: 'center',
+  },
+  searchbar: {
+    width: '100%',
+    height: '100%', 
+    paddingHorizontal: 15,
+    justifyContent: 'center',
+  },
+  item: {
+    width: '100%',
+    height: 50,
+    borderBottomColor: '#0002',
+    borderBottomWidth: 0.5,
+    paddingHorizontal: 20,
+    justifyContent: 'center'
+  },
+  collapsible: {
+    
+  }
+})
