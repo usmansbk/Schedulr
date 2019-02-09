6500ad5d3ed185d8bcc818a80dc656311320d413
Usman Suleiman <usmansbk@gmail.com>
2019-02-09 18:50:47 +0100

    Refactor

--- src/components/tabs/Notifications/Notifications.js
+++ src/components/tabs/Notifications/Notifications.js
@@ -5,17 +5,4 @@ import List from '../../lists/Notifications';
 import styles from '../../../config/styles';
 import colors from '../../../config/colors';
 
-export default (props) => (
-  <View style={styles.container}>
-    <Appbar.Header style={styles.header} collapsable>
-      <Appbar.Content
-        titleStyle={styles.headerColor}/>
-      <Appbar.Action
-        icon="clear-all"
-        color={colors.gray}
-        onPress={() => console.log('Clear Notifications')}
-      />
-    </Appbar.Header>
-    <List />
-  </View>
-);
+export default (props) => <List />;
