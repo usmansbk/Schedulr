4610163406623ded569e9a64187cd4799b901bd3
Usman Suleiman <usmansbk@gmail.com>
2019-02-10 04:11:48 +0100

    Refactor

--- src/components/common/FollowButton/Button.js
+++ src/components/common/FollowButton/Button.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Fab from '../Fab';
 
-export default ({
-  style,
-  isFollowing,
-}) => (
-  <Fab
-    icon={isFollowing ? "check" : "add"}
-    label={isFollowing ? "Following" : "Follow"}
-  />
-);
+export default class Button extends React.Component {
+  _onPress = () => {
+    const {
+      isFollowing,
+      onFollowBoard,
+      onUnfollowBoard
+    } = this.props;
+    if (isFollowing) onUnfollowBoard();
+    else onFollowBoard();
+  };
+
+  render() {
+    const {
+      isFollowing
+    } = this.props;
+    return (
+      <Fab
+        icon={isFollowing ? "check" : "add"}
+        label={isFollowing ? "Following" : "Follow"}
+        onPress={this._onPress}
+      />
+    );
+  }
+}
--- src/components/common/FollowButton/Hoc.js
+++ src/components/common/FollowButton/Hoc.js
@@ -8,10 +8,51 @@ import { listAllBoards } from '../../../graphql/queries';
 
 export default compose(
   graphql(gql(followBoard), {
-    alias: 'withFollowBoard'
+    alias: 'withFollowBoard',
+    options: props => ({
+      variables: {
+        input: {
+          id: props.id
+        }
+      },
+      update: (cache, { data: { followBoard } }) => {
+        if (followBoard) {
+          const query = gql(listAllBoards);
+          const data = cache.readQuery({ query });
+          data.listAllBoards.items = [
+            ...data.listAllBoards.items.filter(item => item.id !== followBoard.id),
+            followBoard
+          ];
+          cache.writeQuery({ query, data });
+        }
+      }
+    }),
+    props: ({ mutate, ownProps }) => ({
+      onFollowBoard: async () => await mutate(),
+      ...ownProps
+    })
   }),
   graphql(gql(unfollowBoard), {
-    alias: 'withUnfollowBoard'
+    alias: 'withUnfollowBoard',
+    options: props => ({
+      variables: {
+        input: {
+          id: props.id
+        },
+      },
+      update: (cache, { data: { unfollowBoard } }) => {
+        if (unfollowBoard) {
+          const query = gql(listAllBoards);
+          const data = cache.readQuery({ query });
+          data.listAllBoards.items = data.listAllBoards.items.filter(item => item.id !== unfollowBoard.id);
+          cache.writeQuery({ query, data });
+        }
+      }
+    }),
+    props: ({ mutate, ownProps }) => ({
+      onUnfollowBoard: async () => await mutate(),
+      ...ownProps
+    })
   }),
   withNavigation
 )(Button);
