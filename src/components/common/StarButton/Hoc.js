dfed14215195c5a342f514ddd40d49ae6bd79af6
Usman Suleiman <usmansbk@gmail.com>
2019-02-11 06:30:09 +0100

    Add event on star

--- src/components/common/StarButton/Hoc.js
+++ src/components/common/StarButton/Hoc.js
@@ -3,6 +3,7 @@ import gql from 'graphql-tag';
 import Button from './Button';
 import { starEvent, unstarEvent } from '../../../graphql/mutations';
 import { toggleStarButton } from '../../../helpers/optimisticResponse';
+import { listAllEvents, getEvent } from '../../../graphql/queries';
 
 export default compose(
   graphql(gql(starEvent), {
@@ -13,7 +14,17 @@ export default compose(
         variables: {
           input
         },
-        optimisticResponse: () => toggleStarButton(input, prev, 'starEvent')
+        optimisticResponse: () => toggleStarButton(input, prev, 'starEvent'),
+        update: (cache, { data: { starEvent } }) => {
+          const data = cache.readQuery(gql(listAllEvents));
+          const eventNode = cache.readQuery(gql(getEvent), { id: ownProps.id});
+          const event = Object.assign({}, eventNode.getEvent, starEvent);
+          data.listAllEvents.items = [
+            ...data.listAllEvents.items.filter(item => item.id !== starEvent.id),
+            event
+          ];
+          cache.writeQuery({ query, data });
+        }
       }),
       ...ownProps
     }),
