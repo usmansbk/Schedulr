64c4f44b304b68beefc5f74f221a9af05ee457b0
Usman Suleiman <usmansbk@gmail.com>
2019-02-12 21:18:03 +0100

    Refactor

--- src/components/screens/UserBoards/Following.js
+++ src/components/screens/UserBoards/Following.js
@@ -3,6 +3,7 @@ import gql from 'graphql-tag';
 import SimpleToast from 'react-native-simple-toast';
 import List from '../../lists/Boards';
 import { createdBoards as createdBoardsQuery } from '../../../graphql/queries';
+
 class Boards extends Component{
 
   render(){
