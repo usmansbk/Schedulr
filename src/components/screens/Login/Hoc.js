e51fff929ea8458c8581844e2a268c494526da03
Usman Suleiman <usmansbk@gmail.com>
2019-02-13 11:47:31 +0100

    Fix login write state

--- src/components/screens/Login/Hoc.js
+++ src/components/screens/Login/Hoc.js
@@ -2,7 +2,6 @@ import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 import Container from './Container';
 import { LoginUser } from '../../../graphql/mutations';
-import { Me, Settings, RemindMeBefore } from '../../../graphql/queries';
 import defaults from '../../../graphql/localstate/defaults';
 
 export default graphql(gql(LoginUser), {
@@ -14,16 +13,13 @@ export default graphql(gql(LoginUser), {
       },
       update: (cache, { data: { loginUser } }) => {
         const me = { me: loginUser };
-        cache.writeQuery({
-          query: gql(Me),
+        cache.writeData({
           data: me
         });
-        cache.writeQuery({
-          query: gql(Settings),
+        cache.writeData({
           data: { settings: defaults.settings }
         });
-        cache.writeQuery({
-          query: gql(RemindMeBefore),
+        cache.writeData({
           data: { remindMeBefore: defaults.remindMeBefore }
         });
       }
