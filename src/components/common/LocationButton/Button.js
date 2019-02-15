a734ec0773bfc673b6d3b05e63e71f27613ce460
Usman Suleiman <usmansbk@gmail.com>
2019-02-15 07:28:17 +0100

    Remove redundant params

--- src/components/common/LocationButton/Button.js
+++ src/components/common/LocationButton/Button.js
@@ -1,35 +1,19 @@
 import React from 'react';
-import { Alert } from 'react-native';
 import { IconButton } from 'react-native-paper';
-import openMap from 'react-native-open-maps';
-
-const handleAddress = ({ address, latitude, longitude }) => {
-  if (latitude && longitude) {
-    openMap({
-      latitude,
-      longitude,
-    })
-  } else if (address) {
-    Alert.alert('Address', address);
-  }
-};
+import { createOpenLink } from 'react-native-open-maps';
 
 export default ({
   size,
   color,
   address,
-  longitude,
-  latitude
 }) => (
   <IconButton
     disabled={!address}
     icon={address ? 'location-on' : 'location-off'}
     color={color}
     size={size}
-    onPress={() => handleAddress({
-      address,
-      latitude,
-      longitude
+    onPress={createOpenLink({
+      query: address,
     })}
   />
 );
