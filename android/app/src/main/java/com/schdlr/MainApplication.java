7f840f084a0edd38a4d94b13077554262ea61e64
Usman Suleiman <usmansbk@gmail.com>
2019-02-15 07:37:53 +0100

    Add google geolocation service

--- android/app/src/main/java/com/schdlr/MainApplication.java
+++ android/app/src/main/java/com/schdlr/MainApplication.java
@@ -6,6 +6,7 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookSdk;
 
 import com.facebook.react.ReactApplication;
+import com.agontuk.RNFusedLocation.RNFusedLocationPackage;
 import com.amazonaws.RNAWSCognitoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -43,6 +44,7 @@ public class MainApplication extends Application implements ShareApplication, Re
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFusedLocationPackage(),
             new RNAWSCognitoPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage(),
