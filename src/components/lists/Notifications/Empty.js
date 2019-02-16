8c28b736880f881f9012056c45ab93b75fce474f
Usman Suleiman <usmansbk@gmail.com>
2019-02-16 11:46:32 +0100

    Chore

--- src/components/lists/Notifications/Empty.js
+++ src/components/lists/Notifications/Empty.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
 import { Headline, Caption } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import styles from './styles';
+import colors from '../../../config/colors';
 
 export default () => (
   <View style={styles.empty}>
+    <Icon name="notifications-active" size={48} color={colors.gray} />
     <Headline style={styles.emptyTitle}>No new notifications</Headline>
     <Caption style={styles.paragraph}>All caught up!</Caption>
   </View>
