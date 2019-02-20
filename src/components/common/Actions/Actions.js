7c7f6993759a0f37145b519bb999e8c0d3f596b2
Usman Suleiman <usmansbk@gmail.com>
2019-02-20 14:01:09 +0100

    Chore

--- src/components/common/Actions/Actions.js
+++ src/components/common/Actions/Actions.js
@@ -80,6 +80,6 @@ const styles = StyleSheet.create({
   actions: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    backgroundColor: colors.bg
+    backgroundColor: colors.white
   }
 });
