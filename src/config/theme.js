d5e9a83595ea1f65d180f6ac0c18ec5ea2732972
Usman Suleiman <usmansbk@gmail.com>
2019-02-21 20:37:11 +0100

    Chore

--- src/config/theme.js
+++ src/config/theme.js
@@ -12,7 +12,6 @@ export default {
     error: colors.light_red,
     text: colors.black,
     placeholder: colors.placeholder,
-    blackground: colors.light_gray,
     disabled: colors.disabled || DefaultTheme.colors.disabled
   }
 };
