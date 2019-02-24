10c947f4c571ed3b80b0c806c2419b80ad9cd344
Usman Suleiman <usmansbk@gmail.com>
2019-02-24 16:01:42 +0100

    Refactor

--- src/components/screens/Settings/Screen.js
+++ src/components/screens/Settings/Screen.js
@@ -3,47 +3,22 @@ import Settings from './Settings';
 import RemindMeDialog from '../../dialogs/RemindMe';
 
 export default class Screen extends React.Component {
-  static defaultProps = {
-    settings: {
-      playSound: true,
-      vibrate: true,
-      headsUp: false,
-      starredAlarm: true,
-      muteReminder: false,
-    }
-  }
   state = {
     visible: false,
   };
   _showDialog = () => this.setState({ visible: true });
   _hideDialog = () => this.setState({ visible: false });
   _goBack = () => this.props.navigation.goBack();
-  _handleValueChange = async (key) => {
-    await this.props.toggleSettings(key);
-  };
   render() {
-    const {
-      settings: {
-        playSound,
-        vibrate,
-        headsUp,
-        starredAlarm,
-        muteReminder
-      }
-    } = this.props;
     return (
       <React.Fragment>
         <Settings
-          vibrate={vibrate}
-          playSound={playSound}
-          headsUp={headsUp}
-          starredAlarm={starredAlarm}
-          muteReminder={muteReminder}
+          state={this.props.stores.settingsStore}
           goBack={this._goBack}
-          handleValueChange={this._handleValueChange}
           openRemindMeDialog={this._showDialog}
         />
         <RemindMeDialog
+          state={this.props.stores.remindMeStore}
           visible={this.state.visible}
           hideDialog={this._hideDialog}
           handleValueChange={this._handleValueChange}
--- src/components/screens/Settings/Settings.js
+++ src/components/screens/Settings/Settings.js
@@ -9,101 +9,113 @@ import {
 import styles from '../../../config/styles';
 import colors from '../../../config/colors';
 
-export default ({
-  goBack,
-  handleValueChange,
-  openRemindMeDialog,
-  muteReminder,
-  starredAlarm,
-  playSound,
-  vibrate,
-  headsUp,
-  disablePush
-}) => (
-  <React.Fragment>
-    <Appbar.Header style={styles.header} collapsable>
-      <Appbar.BackAction color={colors.gray} onPress={goBack} />
-      <Appbar.Content
-        title="Settings"
-        titleStyle={styles.headerColor}
-      />
-    </Appbar.Header>
-    <ScrollView style={styles.bg}>
-      <List.Section title="General">
-        <List.Item
-          title="Sound"
-          right={() => (
-            <Switch
-              value={playSound}
-              onValueChange={() => handleValueChange('playSound')}
+export default class Settings extends React.Component {
+  static defaultProps = {
+    state: {}
+  }
+  render() {
+    const {
+      goBack,
+      handleValueChange,
+      openRemindMeDialog,
+      state,
+    } = this.props;
+    const {
+      sound,
+      vibrate,
+      disableReminders,
+      headsUp,
+      starredEventsOnly,
+      disablePushNotifications,
+    } = state;
+
+    return (
+      <React.Fragment>
+        <Appbar.Header style={styles.header} collapsable>
+          <Appbar.BackAction color={colors.gray} onPress={goBack} />
+          <Appbar.Content
+            title="Settings"
+            titleStyle={styles.headerColor}
+          />
+        </Appbar.Header>
+        <ScrollView style={styles.bg}>
+          <List.Section title="General">
+            <List.Item
+              title="Sound"
+              right={() => (
+                <Switch
+                  value={sound}
+                  onValueChange={() => handleValueChange('sound')}
+                />
+              )}
             />
-          )}
-        />
-        <Divider />
-        <List.Item
-          title="Vibrate"
-          right={() => (
-            <Switch
-              value={vibrate}
-              onValueChange={() => handleValueChange('vibrate')}
+            <Divider />
+            <List.Item
+              title="Vibrate"
+              right={() => (
+                <Switch
+                  value={vibrate}
+                  onValueChange={() => handleValueChange('vibrate')}
+                />
+              )}
             />
-          )}
-        />
-        <Divider />
-      </List.Section>
-      <List.Section title="Reminders">
-        <List.Item
-          title="Disable"
-          right={() => (
-            <Switch
-              value={muteReminder}
-              onValueChange={() => handleValueChange('muteReminder')}
+            <Divider />
+          </List.Section>
+          <List.Section title="Reminders">
+            <List.Item
+              title="Disable"
+              right={() => (
+                <Switch
+                  value={disableReminders}
+                  onValueChange={() => handleValueChange('disableReminders')}
+                />
+              )}
             />
-          )}
-        />
-        <Divider />
-        <List.Item
-          title="Heads-up"
-          right={() => (
-            <Switch
-              disabled={true}
-              value={headsUp}
-              onValueChange={() => handleValueChange('headsUp')}
+            <Divider />
+            <List.Item
+              title="Heads-up"
+              right={() => (
+                <Switch
+                  disabled={true}
+                  value={headsUp}
+                  onValueChange={() => handleValueChange('headsUp')}
+                />
+              )}
             />
-          )}
-        />
-        <Divider />
-        <List.Item
-          title="Starred events only"
-          right={() => (
-            <Switch
+            <Divider />
+            <List.Item
+              title="Starred events only"
+              right={() => (
+                <Switch
+                  disabled={starredEventsOnly}
+                  value={starredEventsOnly}
+                  onValueChange={() => handleValueChange('starredEventsAlarm')}
+                />
+              )}
+            />
+            <Divider />
+            <List.Item
+              title="Remind me"
               disabled={muteReminder}
-              value={starredAlarm}
-              onValueChange={() => handleValueChange('starredAlarm')}
+              right={() => <List.Icon icon="chevron-right" />}
+              onPress={openRemindMeDialog}
             />
-          )}
-        />
-        <Divider />
-        <List.Item
-          title="Remind me"
-          disabled={muteReminder}
-          right={() => <List.Icon icon="chevron-right" />}
-          onPress={openRemindMeDialog}
-        />
-        <Divider />
-      </List.Section>
-      <List.Section title="Push notifications">
-        <List.Item
-          title="Disable"
-          right={() => (
-            <Switch
-              disabled={true}
-              value={disablePush}
-              onValueChange={() => handleValueChange('disablePush')}
+            <Divider />
+          </List.Section>
+          <List.Section title="Push notifications">
+            <List.Item
+              title="Disable"
+              right={() => (
+                <Switch
+                  disabled={true}
+                  value={disablePushNotifications}
+                  onValueChange={() => handleValueChange('disablePushNotifications')}
+                />
+              )}
             />
-          )}
-        />
-      </List.Section>
-    </ScrollView>
-  </React.Fragment>
-);
+          </List.Section>
+        </ScrollView>
+      </React.Fragment>
+    );
+  }
+}
