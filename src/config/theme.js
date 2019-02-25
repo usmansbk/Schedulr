ce330b91d62b3336b17256985ada03f9c890bd71
Usman Suleiman <usmansbk@gmail.com>
2019-02-25 18:27:40 +0100

    Undo

--- src/config/theme.js
+++ src/config/theme.js
@@ -1,10 +1,8 @@
 import { DefaultTheme } from 'react-native-paper';
 import colors from './colors';
-import { isDayTime } from '../lib/time';
 
 export default {
   ...DefaultTheme,
-  dark: !isDayTime(),
   colors: {
     ...DefaultTheme.colors,
     primary: colors.primary,
