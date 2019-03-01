d52d0cf0c64c956a5f3a92be3af6f7add4b337ca
Usman Suleiman <usmansbk@gmail.com>
2019-03-01 01:44:43 +0100

    Fix events status

--- src/lib/calendr.js
+++ src/lib/calendr.js
@@ -24,12 +24,28 @@ function getNextDayEvents(initialEvents, nextDate) {
   return initialEvents.reduce((accumulator, currentEvent) => {
     const eventDate = moment(currentEvent.startAt);
     const repeat = getRepeat(currentEvent.repeat);
-    if (repeat) {
+    if (repeat && !currentEvent.isCancelled) {
       const recurrence = eventDate.recur().every(1, repeat);
       const hasNext = recurrence.matches(refDate.format('l'));
       if (hasNext) {
-        const start = moment(refDate.toISOString());
-        accumulator.data.push(currentEvent);
+        const start = moment(currentEvent.startAt);
+        const startSec = start.seconds();
+        const startMins = start.minutes();
+        const startHours = start.hours();
+
+        const end = moment(currentEvent.endAt);
+
+        const duration = Math.abs(moment.duration(start.diff(end)));
+
+        const startAt = refDate.seconds(startSec).minutes(startMins).hours(startHours).toISOString();
+
+        const endAt = moment(startAt).add(duration).toISOString();
+
+        accumulator.data.push(Object.assign({}, currentEvent, {
+          startAt,
+          endAt
+        }));
+        accumulator.data = sortBy(accumulator.data, 'startAt');
       }
     } else if (eventDate.isSame(refDate, 'day')) {
       accumulator.data.push(currentEvent);
