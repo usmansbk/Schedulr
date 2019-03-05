26aed05826cb2dc32d1d70a08b32a967893583b9
Usman Suleiman <usmansbk@gmail.com>
2019-03-05 15:40:21 +0100

    Chore

--- src/components/screens/EventDetails/Container.js
+++ src/components/screens/EventDetails/Container.js
@@ -74,7 +74,7 @@ export default class EventDetails extends React.Component {
                   onPress={handleDelete}
                 />
                 {
-                  false && (
+                  !isValid && (
                     <Appbar.Action
                       icon="repeat"
                       color={colors.gray}
