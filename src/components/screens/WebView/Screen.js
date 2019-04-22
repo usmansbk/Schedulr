4816436a56f6444739ddb0f8c35ed4408c8a1320
Usman Suleiman <usmansbk@gmail.com>
2019-04-22 22:38:42 +0100

    Refactor webview screen

--- src/components/screens/WebView/Screen.js
+++ src/components/screens/WebView/Screen.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import { WebView } from 'react-native-webview';
+import Loading from 'components/common/Loading';
+import Error from 'components/common/Error';
 
-export default (props) => (
-  <WebView
-    source={{
-      uri: props.navigation.getParam('url')
-    }}
-  />
-);
+export default class MyWebView extends React.Component {
+  _webViewRef = ref => this.ref = ref;
+
+  _handleReload = () => {
+    this.ref && this.ref.reload();
+  };
+
+  render() {
+    return (
+      <WebView
+        ref={this._webViewRef}
+        source={{
+          uri: this.props.navigation.getParam('url')
+        }}
+        startInLoadingState
+        renderLoading={() => <Loading />}
+        renderError={() => <Error onRefresh={this._handleReload} />}
+      />
+    );
+  }
+} 
