8c8308cba95bc3eb753d8054d856ca0c4eb6ac1c
Usman Suleiman <usmansbk@gmail.com>
2019-05-17 12:09:25 +0100

    Fix theme

--- src/components/screens/Help/Help.js
+++ src/components/screens/Help/Help.js
@@ -1,45 +1,46 @@
 import React from 'react';
 import { ScrollView } from 'react-native';
 import { Appbar, List, Divider } from 'react-native-paper';
+import { inject, observer } from 'mobx-react/native';
 import env from 'config/env';
-import styles from 'config/styles';
-import colors from 'config/colors';
 
-export default (props) => (
-  <>
-    <Appbar.Header style={styles.header} collapsable>
-      <Appbar.BackAction color={colors.gray} onPress={props.goBack} />
-      <Appbar.Content
-        title="Help"
-        titleStyle={styles.headerColor}
-      />
-    </Appbar.Header>
-    <ScrollView style={styles.bg}>
-      <List.Item
-        title="FAQ"
-        onPress={() => props.onPressItem('faq')}
-      />
-      <Divider />
-      <List.Item
-        title="Contact us"
-        description="Questions? Need help"
-        onPress={() => props.onPressItem('contact')}
-      />
-      <Divider />
-      <List.Item
-        title="Copyright information"
-        onPress={() => props.onPressItem('copyright')}
-      />
-      <Divider />
-      <List.Item
-        title="Terms and Privacy Policy"
-        onPress={() => props.onPressItem('terms')}
-      />
-      <Divider />
-      <List.Item
-        title="App version"
-        description={env.APP_VERSION}
-      />
-    </ScrollView>
-  </>
-);
+export default inject('stores')(observer(
+  (props) => (
+    <>
+      <Appbar.Header style={props.stores.appStyles.styles.header} collapsable>
+        <Appbar.BackAction color={props.stores.themeStore.colors.gray} onPress={props.goBack} />
+        <Appbar.Content
+          title="Help"
+          titleStyle={props.stores.appStyles.styles.headerColor}
+        />
+      </Appbar.Header>
+      <ScrollView style={props.stores.appStyles.styles.bg}>
+        <List.Item
+          title="FAQ"
+          onPress={() => props.onPressItem('faq')}
+        />
+        <Divider />
+        <List.Item
+          title="Contact us"
+          description="Questions? Need help"
+          onPress={() => props.onPressItem('contact')}
+        />
+        <Divider />
+        <List.Item
+          title="Copyright information"
+          onPress={() => props.onPressItem('copyright')}
+        />
+        <Divider />
+        <List.Item
+          title="Terms and Privacy Policy"
+          onPress={() => props.onPressItem('terms')}
+        />
+        <Divider />
+        <List.Item
+          title="App version"
+          description={env.APP_VERSION}
+        />
+      </ScrollView>
+    </>
+  )
+));
