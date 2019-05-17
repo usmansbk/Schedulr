bc20d6aef7a4da7689be8653d5efa30873b9198b
Usman Suleiman <usmansbk@gmail.com>
2019-05-17 20:13:03 +0100

    Apply theming

--- src/components/common/Fab/Button.js
+++ src/components/common/Fab/Button.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import { FAB } from 'react-native-paper';
-import colors from 'config/colors';
+import { inject, observer } from 'mobx-react/native';
 
-const Fab = ({ onPress, label, icon, small }) => (
-  <FAB
-    label={label}
-    onPress={onPress}
-    theme={{
-      colors: {
-        accent: colors.primary
-      }
-    }}
-    style={{
-      position: 'absolute',
-      margin: 16,
-      right: 0,
-      bottom: 0,
-    }}
-    color="#fff"
-    icon={icon}
-    small={small}
-  />
-);
+const Fab = inject('stores')(observer(
+  ({ onPress, label, icon, small, stores }) => (
+    <FAB
+      label={label}
+      onPress={onPress}
+      theme={{
+        colors: {
+          accent: stores.themeStore.colors.primary
+        }
+      }}
+      style={{
+        position: 'absolute',
+        margin: 16,
+        right: 0,
+        bottom: 0,
+      }}
+      color="#fff"
+      icon={icon}
+      small={small}
+    />
+  )
+));
 
 export default Fab;
