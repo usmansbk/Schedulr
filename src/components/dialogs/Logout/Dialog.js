47f52455dbbe3dbdc4075883a279e13c4476b8d0
Usman Suleiman <usmansbk@gmail.com>
2019-05-17 12:05:42 +0100

    Fix dark theme

--- src/components/dialogs/Logout/Dialog.js
+++ src/components/dialogs/Logout/Dialog.js
@@ -5,29 +5,32 @@ import {
   Dialog,
   Portal,
 } from 'react-native-paper';
-import colors from 'config/colors';
+import { inject, observer } from 'mobx-react/native';
 
-export default ({
-  visible,
-  handleDismiss,
-  loading,
-  handleLogout,
-}) => (
-  <Portal>
-    <Dialog
-      style={{backgroundColor: colors.bg}}
-      dismissable={!loading}
-      visible={visible}
-      onDismiss={handleDismiss}
-    >
-      <Dialog.Title>Sign out?</Dialog.Title>
-      <Dialog.Content>
-        <Paragraph>Will clear data</Paragraph>
-      </Dialog.Content>
-      <Dialog.Actions>
-        <Button disabled={loading} onPress={handleDismiss}>Dismiss</Button>
-        <Button loading={loading} disabled={loading} onPress={handleLogout}>Continue</Button>
-      </Dialog.Actions>
-    </Dialog>
-  </Portal>
-);
+export default inject('stores')(observer(
+  ({
+    visible,
+    handleDismiss,
+    loading,
+    handleLogout,
+    stores
+  }) => (
+    <Portal>
+      <Dialog
+        style={{backgroundColor: stores.themeStore.colors.bg}}
+        dismissable={!loading}
+        visible={visible}
+        onDismiss={handleDismiss}
+      >
+        <Dialog.Title>Sign out?</Dialog.Title>
+        <Dialog.Content>
+          <Paragraph>Will clear data</Paragraph>
+        </Dialog.Content>
+        <Dialog.Actions>
+          <Button disabled={loading} onPress={handleDismiss}>Dismiss</Button>
+          <Button loading={loading} disabled={loading} onPress={handleLogout}>Continue</Button>
+        </Dialog.Actions>
+      </Dialog>
+    </Portal>
+  )
+));
