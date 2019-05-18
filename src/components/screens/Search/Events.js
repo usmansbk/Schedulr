021a857a6efbc7a09577c75329d48317014d23d7
Usman Suleiman <usmansbk@gmail.com>
2019-05-18 06:57:37 +0100

    Offline event search

--- src/components/screens/Search/Events.js
+++ src/components/screens/Search/Events.js
@@ -1,34 +1,41 @@
 import React from 'react';
 import { graphql, compose } from 'react-apollo';
 import gql from 'graphql-tag';
+import { inject, observer } from 'mobx-react/native';
 import List from 'components/lists/EventSearch';
 import { listAllEvents } from 'mygraphql/queries';
 
+@inject('stores')
+@observer
 export default class Events extends React.Component {
 
   render() {
-    const query = this.props.navigation.getParam('query', '');
+    const { stores } = this.props;
+
+    const { query, isConnected } = stores.appState;
+
     return (
-      <List
+      <ListHoc
         query={query}
+        isConnected={isConnected}
       />
     );
   }
 }
 
-// const ListHoc = compose(
-//   graphql(gql(listAllEvents), {
-//     alias: 'withSearchEventsOffline',
-//     skip: props => false && props.isConnected,
-//     options: {
-//       fetchPolicy: 'cache-only'
-//     },
-//     props: ({ data, ownProps }) => ({
-//       loading: data.loading,
-//       events: data && data.listAllEvents && data.listAllEvents.items.filter(
-//         item => item.title.toLowerCase().includes(ownProps.query.toLowerCase())
-//       ),
-//       ...ownProps
-//     })
-//   })
-// )(List);
+const ListHoc = compose(
+  graphql(gql(listAllEvents), {
+    alias: 'withSearchEventsOffline',
+    skip: props => props.isConnected,
+    options: {
+      fetchPolicy: 'cache-only'
+    },
+    props: ({ data, ownProps }) => ({
+      loading: data.loading,
+      events: data && data.listAllEvents && data.listAllEvents.items.filter(
+        item => item.title.toLowerCase().includes(ownProps.query.toLowerCase())
+      ),
+      ...ownProps
+    })
+  })
+)(List);
