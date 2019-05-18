41891ae17dd1c18c9b36f132e446f7c70fcb6e49
Usman Suleiman <usmansbk@gmail.com>
2019-05-19 00:06:13 +0100

    Fix bug

--- src/components/lists/Comments/List.js
+++ src/components/lists/Comments/List.js
@@ -50,12 +50,10 @@ export default class List extends React.Component {
   scrollDown = () => {
     this._listRef && this._listRef.scrollToEnd();
   }
-  shouldComponentUpdate = (nextProps) => { 
-    return (nextProps.navigation.isFocused &&
-      (
-        nextProps.comments.length !== this.props.comments.length ||
-        nextProps.loading !== this.props.loading
-      )
+  shouldComponentUpdate = (nextProps) => {
+    (
+      nextProps.comments.length !== this.props.comments.length ||
+      nextProps.loading !== this.props.loading
     );
   };
 
