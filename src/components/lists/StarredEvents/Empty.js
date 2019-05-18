eb801afbea6faaa68d889acee70348c32824eb55
Usman Suleiman <usmansbk@gmail.com>
2019-05-18 14:38:09 +0100

    Fix theme

--- src/components/lists/StarredEvents/Empty.js
+++ src/components/lists/StarredEvents/Empty.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import { View } from 'react-native';
 import { Headline, Paragraph } from 'react-native-paper';
-import styles from './styles';
+import { inject, observer } from 'mobx-react/native';
 
-export default ({ error, loading, search }) =>{
-  if (loading) return null;
-  return (
-    <View style={styles.empty}>
-      <Headline style={styles.emptyTitle}>
-      {
-        error ? 'Network error' : `${search ? "No event found" : "You haven't starred any event yet"}`
-      }
-      </Headline>
-      {
-        error && (
-          <Paragraph style={styles.paragraph}>
-            Check your internet connection. Pull to refresh.
-          </Paragraph>
-        )
-      }
-    </View>
-  );  
-}
+export default inject('stores')(observer(
+  ({ error, loading, search, stores }) =>{
+    if (loading) return null;
+    return (
+      <View style={stores.appStyles.starredEventsList.empty}>
+        <Headline style={stores.appStyles.starredEventsList.emptyTitle}>
+        {
+          error ? 'Network error' : `${search ? "No event found" : "You haven't starred any event yet"}`
+        }
+        </Headline>
+        {
+          error && (
+            <Paragraph style={stores.appStyles.starredEventsList.paragraph}>
+              Check your internet connection. Pull to refresh.
+            </Paragraph>
+          )
+        }
+      </View>
+    );  
+  }
+))
--- src/components/lists/StarredEvents/Footer.js
+++ src/components/lists/StarredEvents/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
 import { Text } from 'react-native-paper';
-import styles from './styles';
+import { inject, observer } from 'mobx-react/native';
 
-export default ({ visible }) => visible ? (
-  <View style={styles.footer}>
-    <Text style={styles.footerText}>No more events</Text>
-  </View>
-) : null;
+export default inject('stores')(observer(
+  ({ visible, stores }) => visible ? (
+    <View style={stores.appStyles.starredEventsList.footer}>
+      <Text style={stores.appStyles.starredEventsList.footerText}>No more events</Text>
+    </View>
+  ) : null
+));
