6df5ecfe6dc4d7fbc0baf9ac2820d85ba38f3186
Usman Suleiman <usmansbk@gmail.com>
2019-05-19 00:36:55 +0100

    Chore

--- src/components/lists/SkipEmptyDaysEvents/Header.js
+++ src/components/lists/SkipEmptyDaysEvents/Header.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { TouchableRipple, ActivityIndicator, Text } from 'react-native-paper';
+import { TouchableRipple, Text } from 'react-native-paper';
 import { inject, observer } from 'mobx-react/native';
 
 export default inject('stores')(observer(
-  ({ onPress, loading, stores }) => (
-    <TouchableRipple disabled={loading} onPress={onPress} style={stores.appStyles.eventsList.header}>
-      <Text style={stores.appStyles.eventsList.headerText}>Load past few days</Text>
+  ({ onPress, loading, stores, hasPrev }) => (
+    <TouchableRipple
+      disabled={loading || !hasPrev}
+      onPress={onPress}
+      style={stores.appStyles.eventsList.header}
+    >
+      <Text style={stores.appStyles.eventsList.headerText}>
+        {
+          hasPrev ? "Load past few days" : "No previous events"
+        }
+      </Text>
     </TouchableRipple>
   )
 ));
