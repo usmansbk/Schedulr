af5e0a4ad341946e5db19cf89fdefa733115a08d
Usman Suleiman <usmansbk@gmail.com>
2019-05-19 16:48:51 +0100

    Chore

--- src/components/lists/SkipEmptyDaysEvents/Footer.js
+++ src/components/lists/SkipEmptyDaysEvents/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {
-  ActivityIndicator,
   Text,
   TouchableRipple,
 } from 'react-native-paper';
@@ -8,28 +7,19 @@ import { View } from 'react-native';
 import { inject, observer } from 'mobx-react/native';
 
 export default inject('stores')(observer(
-  ({ loading, onPress, stores, hasMore }) => {
-    if (loading) return (
-      <ActivityIndicator
-        animating={loading}
-        hideWhenStopped
-        size="small"
-      />
-    );
-    return (
-      <TouchableRipple
-        disabled={loading || !hasMore}
-        onPress={onPress}
-        style={stores.appStyles.eventsList.footerContainer}
-      >
-        <View style={stores.appStyles.eventsList.footerContent}>
-          <Text style={stores.appStyles.eventsList.footerText}>
-            {
-              hasMore ? "Load next few days" : "No more events"
-            }
-          </Text>
-        </View>
-      </TouchableRipple>
-    )
-  }
-))
+  ({ loading, onPress, stores, hasMore }) => (
+    <TouchableRipple
+      disabled={loading || !hasMore}
+      onPress={onPress}
+      style={stores.appStyles.eventsList.footerContainer}
+    >
+      <View style={stores.appStyles.eventsList.footerContent}>
+        <Text style={stores.appStyles.eventsList.footerText}>
+          {
+            hasMore ? "Load next few days" : "No more events"
+          }
+        </Text>
+      </View>
+    </TouchableRipple>
+  )
+));
