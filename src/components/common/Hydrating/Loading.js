727e33b4196a328280cbe49be1c3aecf5a38e72c
Usman Suleiman <usmansbk@gmail.com>
2019-05-20 21:20:08 +0100

    Chore

--- src/components/common/Hydrating/Loading.js
+++ src/components/common/Hydrating/Loading.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import { View, StyleSheet, ActivityIndicator } from 'react-native';
+import { View, StyleSheet, StatusBar, ActivityIndicator } from 'react-native';
+import changeNavigationBarColor from 'react-native-navigation-bar-color';
 import colors from 'config/colors';
 
-export default () => (
-  <View style={styles.container}>
-    <ActivityIndicator size="large" color={colors.primary} />
-  </View>
-);
+export default class Loading extends React.Component {
+   
+  componentDidMount = async () => {
+    changeNavigationBarColor(colors.primary_light, true);
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar
+          backgroundColor={colors.primary_light}
+          barStyle="light-content"
+        />
+        <ActivityIndicator size="large" color={colors.white} />
+      </View>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: colors.bg
+    backgroundColor: colors.primary_light
   },
 });
