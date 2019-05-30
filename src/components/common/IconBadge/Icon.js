a4e48d21bf37f84bf0c71567bc36780dca493667
Usman Suleiman <usmansbk@gmail.com>
2019-05-30 11:56:03 +0100

    Chore

--- src/components/common/IconBadge/Icon.js
+++ src/components/common/IconBadge/Icon.js
@@ -4,25 +4,41 @@ import {
   Text,
   IconButton
 } from 'react-native-paper';
-import { View } from 'react-native';
+import { View, InteractionManager } from 'react-native';
 import { inject, observer } from 'mobx-react/native';
 
-export default inject('stores')(observer(
-  ({ color, icon, size, count, onPress, stores }) => (
-    <View style={stores.appStyles.styles.button}>
-      <IconButton
-        onPress={onPress}
-        icon={icon}
-        size={size}
-        color={color}
-        style={stores.appStyles.styles.iconButton}
-        animated
-      />
-      {
-        Boolean(count) && (
-          <Text style={stores.appStyles.styles.badge}>{count && numeral(count).format('0a')}</Text>
-        )
-      }
-    </View>
-  )
-));
+@inject('stores')
+@observer
+export default class IconBagdeButton extends React.Component {
+  _onPress = () => {
+    InteractionManager.runAfterInteractions(this.props.onPress);
+  };
+  
+  render() {
+    const {
+      color,
+      icon,
+      size,
+      count,
+      stores
+    } = this.props;
+
+    return  (
+      <View style={stores.appStyles.styles.button}>
+        <IconButton
+          onPress={this._onPress}
+          icon={icon}
+          size={size}
+          color={color}
+          style={stores.appStyles.styles.iconButton}
+          animated
+        />
+        {
+          Boolean(count) && (
+            <Text style={stores.appStyles.styles.badge}>{count && numeral(count).format('0a')}</Text>
+          )
+        }
+      </View>
+    )
+  }
+}
