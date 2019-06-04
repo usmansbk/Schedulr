895ba5f849ba27a6e4fb610695b31a66726bb8de
Usman Suleiman <usmansbk@gmail.com>
2019-06-04 19:07:28 +0100

    Chore

--- src/components/forms/Event/schema.js
+++ src/components/forms/Event/schema.js
@@ -10,5 +10,5 @@ export default Yup.object().shape({
     .trim()
     .max(160, 'Too Long'),
   boardId: Yup.string()
-    .required('Event must belong to a board')
+    .required('Event must be posted on a board')
 });
