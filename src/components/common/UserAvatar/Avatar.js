84d41b1dba9e3066be0bae5f37cc91cf3660c55f
Usman Suleiman <usmansbk@gmail.com>
2019-06-09 16:56:44 +0100

    Fix/re-renders

--- src/components/common/UserAvatar/Avatar.js
+++ src/components/common/UserAvatar/Avatar.js
@@ -4,30 +4,49 @@ import UserAvatar from 'react-native-user-avatar';
 import emojiRegex from 'emoji-regex';
 import CachedImage from './CachedImage';
 
-export default ({
-  name,
-  src,
-  onPress,
-  size,
-  style
-}) => {
-  const emojiMatch = emojiRegex().exec(name);
-  let avatarName;
-  if (emojiMatch) {
-    avatarName = emojiMatch[0];
-  } else {
-    const [ first, second ] = name.split(' ');
-    avatarName = `${first} ${second ? second : ''}`;
+export default class Avatar extends React.Component {
+  constructor(props) {
+    super(props);
+    const { name } = props;
+    const emojiMatch = emojiRegex().exec(name);
+    let avatarName;
+    if (emojiMatch) {
+      avatarName = emojiMatch[0];
+    } else {
+      const [ first, second ] = name.split(' ');
+      avatarName = `${first} ${second ? second : ''}`;
+    }
+    this.state = {
+      avatarName
+    }
   }
+  shouldComponentUpdate = (nextProps) => this.props.name !== nextProps.name;
 
-  return (
-    <TouchableRipple onPress={onPress} style={style}>
-      <UserAvatar
-        name={avatarName}
-        src={src}
-        size={size}
-        component={(props) => <CachedImage {...props} size={size} name={avatarName} />}
-      />
-    </TouchableRipple>
-  )
-};
+  _renderComponent = (props) => {
+    const { size } = this.props;
+    const { avatarName } = this.state;
+    return <CachedImage {...props} size={size} name={avatarName} />
+  };
+
+  render() {
+    const {
+      src,
+      onPress,
+      size,
+      style
+    } = this.props;
+
+    const { avatarName } = this.state;
+  
+    return (
+      <TouchableRipple onPress={onPress} style={style}>
+        <UserAvatar
+          name={avatarName}
+          src={src}
+          size={size}
+          component={this._renderComponent}
+        />
+      </TouchableRipple>
+    )
+  }
+}
