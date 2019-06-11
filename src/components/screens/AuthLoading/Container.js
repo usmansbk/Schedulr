776d71c899606ffbd01ed756642d7ac94459e62b
Usman Suleiman <usmansbk@gmail.com>
2019-06-11 23:41:54 +0100

    Chore

--- src/components/screens/AuthLoading/Container.js
+++ src/components/screens/AuthLoading/Container.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import { Auth } from 'aws-amplify';
+import { inject, observer } from 'mobx-react';
 import Loading from './Loading';
 
+@inject('stores')
+@observer
 export default class Container extends Component {
   constructor(props) {
     super(props);
@@ -11,8 +14,7 @@ export default class Container extends Component {
   _bootstrapAsync = async () => {
     try {
       const user = await Auth.currentAuthenticatedUser();
-      if (user) this.props.navigation.navigate('App');
-      // await Auth.currentAuthenticatedUser();
+      if (user && this.props.stores.me.id) this.props.navigation.navigate('App');
     } catch (error) {
       this.props.navigation.navigate('Auth');
     }
