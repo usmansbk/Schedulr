74094376751ef603b274bf489c7d2e860193b0d0
Usman Suleiman <usmansbk@gmail.com>
2019-06-16 13:53:23 +0100

    Chore

--- src/components/lists/BoardSearch/Empty.js
+++ src/components/lists/BoardSearch/Empty.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Headline, Caption } from 'react-native-paper';
+import { Headline } from 'react-native-paper';
 import { inject, observer } from 'mobx-react';
 
 export default inject('stores')(observer(
   ({ stores }) => {
     return (
       <View style={stores.appStyles.boardSearch.empty}>
-        <Headline style={stores.appStyles.boardSearch.emptyTitle}>What interests you?</Headline>
-        <Caption style={stores.appStyles.boardSearch.paragraph}>Find a board, follow, and get notified on event updates.</Caption>
+        <Headline style={stores.appStyles.boardSearch.emptyTitle}>Not found</Headline>
       </View>
     );
   }
