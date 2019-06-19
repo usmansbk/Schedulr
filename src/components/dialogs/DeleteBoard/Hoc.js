0551ae073228cd3563db4b7ad7ef5413835b4a38
Usman Suleiman <usmansbk@gmail.com>
2019-06-19 23:44:19 +0100

    Chore

--- src/components/dialogs/DeleteBoard/Hoc.js
+++ src/components/dialogs/DeleteBoard/Hoc.js
@@ -2,7 +2,7 @@ import { graphql, compose } from 'react-apollo';
 import gql from 'graphql-tag';
 import { withNavigation } from 'react-navigation';
 import Dialog from './Dialog';
-import { listAllBoards } from 'mygraphql/queries';
+import { listAllBoards, listAllEvents } from 'mygraphql/queries';
 import { deleteBoard } from 'mygraphql/mutations';
 
 export default compose(
@@ -26,7 +26,13 @@ export default compose(
             const query = gql(listAllBoards);
             const data = cache.readQuery({ query });
             data.listAllBoards.items = data.listAllBoards.items.filter(item => item.id !== deleteBoard.id);
+
+            const allEventsQuery = gql(listAllEvents);
+            const allEventsData = cache.readQuery({ query: allEventsQuery });
+            allEventsData.listAllEvents.items = data.listAllEvents.items.filter(item => item.board.id !== deleteBoard.id);
+
             cache.writeQuery({ query, data });
+            cache.writeQuery({ query: allEventsQuery, data: allEventsData });
           }
         }
       }),
