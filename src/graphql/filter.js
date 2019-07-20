4c3415c6083669950eb00f3f9bd8883c0608d52b
Usman Suleiman <usmansbk@gmail.com>
2019-07-20 23:54:03 +0100

    Fix filter

--- src/graphql/filter.js
+++ src/graphql/filter.js
@@ -3,19 +3,17 @@ import moment from 'moment';
 const intervals = ["DAILY", "WEEKDAYS", "WEEKLY", "MONTHLY", "MONTHLY_DAY", "YEARLY"];
 
 export const filterEvents = {
-  "expression":  "#endAt >= :startOfDay OR ( (#repeat IN (:intervals) AND #untilAt >= :startOfDay) OR #forever = :forever ) AND #isCancelled = :isCancelled",
+  "expression":  "#endAt >= :startOfDay OR ( #repeat IN (:intervals) AND (#untilAt >= :startOfDay OR #forever = :forever) AND #endAt >= :startOfDay )",
   "expressionNames": JSON.stringify({
     "#endAt"       : "endAt",
     "#repeat"      : "repeat",
     "#untilAt"     : "untilAt",
-    "#forever"     : "forever",
-    "#isCancelled" : "isCancelled"
+    "#forever"     : "forever"
   }),
   "expressionValues" :  JSON.stringify({
     ":startOfDay"    : moment().startOf('day').subtract(1, 'day').valueOf(),
     ":intervals"     : `${intervals}`,
-    ":forever"       : true,
-    ":isCancelled"   : false
+    ":forever"       : true
   })
 };
 
