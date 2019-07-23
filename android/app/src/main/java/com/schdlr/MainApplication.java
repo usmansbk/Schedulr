ef4d244f739f0c4f21e2c9600d3bed6991057575
Usman Suleiman <usmansbk@gmail.com>
2019-07-23 11:12:00 +0100

    Upgrade

--- android/app/src/main/java/com/schdlr/MainApplication.java
+++ android/app/src/main/java/com/schdlr/MainApplication.java
@@ -3,6 +3,7 @@ package com.schdlr;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.agontuk.RNFusedLocation.RNFusedLocationPackage;
@@ -33,6 +34,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FBSDKPackage(),
             new RNGestureHandlerPackage(),
             new RNGoogleSigninPackage(),
             new RNFusedLocationPackage(),
