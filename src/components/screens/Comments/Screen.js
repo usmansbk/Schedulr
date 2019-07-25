b0ac8a6f17b4337206fbc069961403fb01ac42d9
Usman Suleiman <usmansbk@gmail.com>
2019-07-25 04:50:36 +0100

    Chore

--- src/components/screens/Comments/Screen.js
+++ src/components/screens/Comments/Screen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { inject } from 'mobx-react';
+import { inject, observer } from 'mobx-react';
 import Comments from './Comments';
 import DeleteCommentDialog from 'components/dialogs/DeleteComment';
 
