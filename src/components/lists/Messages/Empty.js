e21c82d7b14cf39d4e0cf7d0cce3b403b4d1aa4e
Usman Suleiman <usmansbk@gmail.com>
2019-08-02 21:38:31 +0100

    Chore

--- src/components/lists/Messages/Empty.js
+++ src/components/lists/Messages/Empty.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Headline, Caption } from 'react-native-paper';
+import { Headline } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { inject, observer } from 'mobx-react';
 
@@ -8,8 +8,7 @@ export default inject('stores')(observer(
   ({ stores }) => (
     <View style={stores.appStyles.notifications.empty}>
       <Icon name="message" size={48} color={stores.themeStore.colors.gray} />
-      <Headline style={stores.appStyles.notifications.emptyTitle}>No new notifications</Headline>
-      <Caption style={stores.appStyles.notifications.paragraph}>All caught up!</Caption>
+      <Headline style={stores.appStyles.notifications.emptyTitle}>No new message</Headline>
     </View>
   )
 ));
