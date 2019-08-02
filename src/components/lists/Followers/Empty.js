9df45b571c5d8f72b51767f75846f63762e16133
Usman Suleiman <usmansbk@gmail.com>
2019-08-02 20:38:37 +0100

    Chore

--- src/components/lists/Followers/Empty.js
+++ src/components/lists/Followers/Empty.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Headline, Paragraph } from 'react-native-paper';
+import { Headline } from 'react-native-paper';
 import { inject, observer } from 'mobx-react';
+import ErrorScreen from 'components/common/Error';
 
 export default inject('stores')(observer(
-  ({ error, loading, stores }) => loading ? null : (
-    <View style={stores.appStyles.commentsList.empty}>
-      <Headline style={stores.appStyles.commentsList.emptyTitle}>
-        {
-          error ? 'Network error' : 'No followers'
-        }
-      </Headline>
-      {
-        error && (
-          <Paragraph style={stores.appStyles.commentsList.paragraph}>
-            Check your internet connection. Pull to refresh.
-          </Paragraph>
-        )
-      }
-    </View>
-  )
-));
+  ({ error, loading, stores, onRefresh }) =>{
+    if (loading) return null;
+    if (error) return <ErrorScreen
+      onRefresh={onRefresh}
+      loading={loading}
+    />;
+    return (
+      <View style={stores.appStyles.eventsList.empty}>
+        <Headline style={stores.appStyles.eventsList.emptyTitle}>
+          No followers
+        </Headline>
+      </View>
+    );  
+  }
+))
