0a4361e4323284dc3a419b63c2028dae0686cec0
Usman Suleiman <usmansbk@gmail.com>
2019-08-05 19:55:48 +0100

    Refactor

--- src/components/social_buttons/FBLoginButton/Container.js
+++ src/components/social_buttons/FBLoginButton/Container.js
@@ -1,73 +1,12 @@
-import React from 'react';
-import {
-  LoginManager,
-  GraphRequest,
-  GraphRequestManager,
-  AccessToken
-} from 'react-native-fbsdk';
-import SimpleToast from 'react-native-simple-toast';
+import React from 'react'
 import Button from './Button';
 
 export default class Container extends React.Component {
-  state = {
-    loading: false,
-  };
 
-  _signIn = async () => {
-    this.setState({ loading: true });
-    try {
-      const response = await LoginManager.logInWithPermissions(['email']);
-      if (response.isCancelled) {
-        SimpleToast.show('Login cancelled', SimpleToast.SHORT);
-      } else {
-        return await this._requestUserInfo();
-      }
-    } catch (error) {
-      SimpleToast.show('Connection error: ' + error.message, SimpleToast.SHORT);
-    }
-    this.setState({ loading: false });
-  };
-
-  _requestUserInfo = async () => {
-    const accessData = await AccessToken.getCurrentAccessToken();
-
-    const infoRequest = new GraphRequest(
-      '/me',
-      {
-        accessToken: accessData.accessToken,
-        parameters: {
-          fields: {
-            string: 'id, email, name, picture.type(large)',
-          }
-        }
-      },
-      this._responseInfoCallback,
-    );
-    new GraphRequestManager().addRequest(infoRequest).start();
-  }
-
-  _responseInfoCallback = async (error, result) => {
-    if (error) {
-      SimpleToast.show(error.message, SimpleToast.LONG);
-    } else if (result) {
-      const { email, name, picture } = result;
-      const accessData = await AccessToken.getCurrentAccessToken();
-      await this.props.onLogin({
-        name,
-        email,
-        pictureUrl: picture && picture.data && picture.data.url,
-        provider: 'facebook',
-        token: accessData.accessToken,
-        expires_at: accessData.expirationTime
-      });
-    } else {
-      SimpleToast.show('Something went wrong', SimpleToast.SHORT);
-    }
-    this.setState({ loading: false });
-  }
+  _signIn = () => this.props.onLogin('Facebook');
 
   render() {
-    const { loading } = this.state;
+    const { loading } = this.props;
     return (
       <Button
         loading={loading}
