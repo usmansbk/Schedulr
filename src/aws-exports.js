606c3f6a037399b3cef13ef66c49510876341fa0
Usman Suleiman <usmansbk@gmail.com>
2019-08-05 07:00:21 +0100

    Refactor

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,9 @@ const awsmobile = {
     "aws_user_pools_id": "us-east-1_AxP4tm7Sj",
     "aws_user_pools_web_client_id": "3fa3ecqh8buqmv74im6ff9le7d",
     "oauth": {},
-    "federationTarget": "COGNITO_IDENTITY_POOLS"
+    "federationTarget": "COGNITO_IDENTITY_POOLS",
+    "aws_mobile_analytics_app_id": "a08247a7fb2645e1a1fd72d2e2a6f5ea",
+    "aws_mobile_analytics_app_region": "us-east-1"
 };
 
 
