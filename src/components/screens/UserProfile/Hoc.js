70fe198281e1b481a2e4f9f325c845731b1cae11
Usman Suleiman <usmansbk@gmail.com>
2019-08-07 00:24:31 +0100

    Refactor

--- src/components/screens/UserProfile/Hoc.js
+++ src/components/screens/UserProfile/Hoc.js
@@ -1,30 +1,30 @@
-// import { graphql, compose } from 'react-apollo';
-// import gql from 'graphql-tag';
-// import { withNavigationFocus } from 'react-navigation';
+import { graphql, compose } from 'react-apollo';
+import gql from 'graphql-tag';
+import { withNavigationFocus } from 'react-navigation';
 import UserProfile from './UserProfile';
-export default UserProfile;
-// import { getUser } from 'mygraphql/queries';
 
-// const alias = 'withUserProfile';
+import { getUser } from 'mygraphql/queries';
 
-// export default compose(
-//   withNavigationFocus,
-//   graphql(gql(getUser), {
-//     alias,
-//     options: props => ({
-//       variables: {
-//         id: props.navigation.getParam('id'),
-//       },
-//       notifyOnNetworkStatusChange: true,
-//       fetchPolicy: 'cache-first',
-//     }),
-//     props: ({ data, ownProps }) => ({
-//       loading: data.loading,
-//       refreshing: data.networkStatus === 4,
-//       error: data.error,
-//       onRefresh: () => data.refetch(),
-//       user: data && data.getUser,
-//       ...ownProps,
-//     })
-//   })
-// )(UserProfile)
+const alias = 'withUserProfile';
+
+export default compose(
+  withNavigationFocus,
+  graphql(gql(getUser), {
+    alias,
+    options: props => ({
+      variables: {
+        id: props.navigation.getParam('id'),
+      },
+      notifyOnNetworkStatusChange: true,
+      fetchPolicy: 'cache-first',
+    }),
+    props: ({ data, ownProps }) => ({
+      loading: data.loading,
+      refreshing: data.networkStatus === 4,
+      error: data.error,
+      onRefresh: () => data.refetch(),
+      user: data && data.getUser,
+      ...ownProps,
+    })
+  })
+)(UserProfile)
