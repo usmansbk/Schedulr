c5e58a934117e49db3a787c07d8cbf6d4e1fd2d8
Usman Suleiman <usmansbk@gmail.com>
2019-08-06 01:23:58 +0100

    Refactor

--- src/components/screens/Login/Container.js
+++ src/components/screens/Login/Container.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Auth, Hub } from 'aws-amplify';
 import SimpleToast from 'react-native-simple-toast';
-import { inject, observer } from 'mobx-react';
 import Login from './Login';
 
-class Container extends React.Component {
+export default class Container extends React.Component {
   state = { loading: false };
 
   componentDidMount = async () => {
-    const { stores } = this.props;
     Hub.listen("auth", async ({ payload: { event, data } }) => {
       switch(event) {
         case "signIn":
@@ -39,8 +37,6 @@ class Container extends React.Component {
     />;
   }
 }
-
-export default inject('stores')(observer(Container));
 
 // export default graphql(gql(LoginUser), {
 //   alias: 'withLoginScreen',
