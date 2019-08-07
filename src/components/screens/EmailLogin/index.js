1f5932ba7000028826e3d4ee3b92a27af7f195d0
Usman Suleiman <usmansbk@gmail.com>
2019-08-07 17:15:49 +0100

    Customize form

--- src/components/screens/EmailLogin/index.js
+++ src/components/screens/EmailLogin/index.js
@@ -14,4 +14,31 @@ const theme ={
   }
 };
 
-export default withAuthenticator(Loading, null, null, null, theme);
+const signUpConfig = {
+  hideAllDefaults: true,
+  signUpFields: [
+    {
+      label: 'Name',
+      key: 'name',
+      required: true,
+      displayOrder: 1,
+      type: 'string'
+    },
+    {
+      label: 'Email',
+      key: 'email',
+      required: true,
+      displayOrder: 2,
+      type: 'string'
+    },
+    {
+      label: 'Password',
+      key: 'password',
+      required: true,
+      displayOrder: 3,
+      type: 'password'
+    }
+  ]
+};
+
+export default withAuthenticator(Loading, { signUpConfig }, null, null, theme);
