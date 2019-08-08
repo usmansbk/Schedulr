0fae479d1303138b30257d015de0f5aa767a2170
Usman Suleiman <usmansbk@gmail.com>
2019-08-08 09:06:34 +0100

    Add eng traslation

---
+++ src/i18n/en.js
@@ -0,0 +1,66 @@
+export default {
+  EVENTS_emptyList: "No upcoming events",
+  
+  BOARD_emptyList: "Your board is empty",
+  BOARD_emptyListCaption: "Follow or create a schedule",
+
+  BOOKMARKS_emptyList: "You haven't saved any events yet",
+
+  MORE_settings: "Settings",
+  MORE_inviteAFriend: "Invite a friend",
+  MORE_help: "Help",
+  MORE_signOut: "Sign out",
+
+  EXPLORE_emptyList: "Explore",
+  EXPLORE_emptyListCaption: "Nearby, sponsored and suggested events",
+
+  SEARCH_inputPlaceholder: "Search",
+  SEARCH_schedulesTabLabel: "Schedules",
+  SEARCH_eventsTabLabel: "Events",
+  SEARCH_schedulesEmptyList: "No results",
+  SEARCH_eventsEmptyList: "No nearby events",
+
+  NOTIFICATIONS_updatesTabLabel: "UPDATES",
+  NOTIFICATIONS_messagesTabLabel: "MESSAGES",
+  NOTIFICATIONS_emptyUpdatesList: "No new notifications",
+  NOTIFICATIONS_emptyUpdatesListCaption: "All caught up!",
+  NOTIFICATIONS_emptyMessagesList: "No messages",
+
+  SETTINGS_screenTitle: "Settings",
+  SETTINGS_generalSectionTitle: "General",
+  SETTINGS_sound: "Sound",
+  SETTINGS_vibrate: "Vibrate",
+  SETTINGS_darkTheme: "Dark theme",
+  SETTINGS_location: "Location",
+  SETTINGS_reminderSectionTitle: "Reminder",
+  SETTINGS_reminderDisable: "Disable",
+  SETTINGS_remindMe: "Remind me",
+  SETTINGS_pushSectionTitle: "Push notification",
+  SETTINGS_pushDisable: "Disable",
+
+  REMIND_ME_title: "Remind me",
+  REMIND_ME_five: "5 minutes before",
+  REMIND_ME_ten: "10 minutes before",
+  REMIND_ME_fifteen: "15 minutes before",
+  REMIND_ME_thirty: "30 minutes before",
+  REMIND_ME_oneHour: "1 hour before",
+  REMIND_ME_oneDay: "1 day before",
+
+  MODAL_done: "Done",
+  MODAL_continue: "CONTINUE",
+  MODAL_dismiss: "DISMISS",
+
+  SIGN_OUT_title: "Sign out?",
+  SIGN_OUT_message: "Will clear data",
+
+  HELP_title: "Help",
+  HELP_contactUs: "Contact us",
+  HELP_contactUsSubtitle: "Questions? Need help",
+  HELP_copyRight: "Copyright information",
+  HELP_terms: "Terms and Privacy Policy",
+  HELP_appVersion: "App version",
+
+  BUTTON_cancel: "CANCEL",
+  BUTTON_create: "CREATE",
+  BUTTON_save: "SAVE"
+};
