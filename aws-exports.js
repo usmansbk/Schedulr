eb060945ce216fa1002dcc7e8e92a8d64f1265f8
Usman Suleiman <usmansbk@gmail.com>
2019-08-11 02:38:36 +0100

    Refactor

--- aws-exports.js
+++ aws-exports.js
@@ -2,10 +2,10 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-1",
-    "aws_cognito_identity_pool_id": "us-east-1:a533e830-2952-4198-8564-7bd1d0183f1b",
+    "aws_cognito_identity_pool_id": "us-east-1:e86a8c71-fcd4-406f-876a-537a9c10c96f",
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_xsnGipK3A",
-    "aws_user_pools_web_client_id": "4j3egardp7r5ids9eqg6urc6gp",
+    "aws_user_pools_id": "us-east-1_vssBv0ZMF",
+    "aws_user_pools_web_client_id": "4gkotf3qfq1msb26tatk8upoea",
     "oauth": {
         "domain": "schdlr-dev.auth.us-east-1.amazoncognito.com",
         "scope": [
@@ -20,11 +20,11 @@ const awsmobile = {
         "responseType": "code"
     },
     "federationTarget": "COGNITO_USER_POOLS",
+    "aws_mobile_analytics_app_id": "9d52361b9e2b467abc8e65c42cf1b0b9",
+    "aws_mobile_analytics_app_region": "us-east-1",
     "aws_appsync_graphqlEndpoint": "https://ic4arvyxqjavzmax5vvu7gfrfa.appsync-api.us-east-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-1",
-    "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
-    "aws_mobile_analytics_app_id": "dac9bd75bbf142068fcbb918ddebb7a0",
-    "aws_mobile_analytics_app_region": "us-east-1"
+    "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS"
 };
 
 
