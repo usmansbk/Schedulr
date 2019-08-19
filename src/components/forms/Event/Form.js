4558ab69c26278d62f2680141088a2cfaf008640
Usman Suleiman <usmansbk@gmail.com>
2019-08-19 01:50:21 +0100

    chore/remove autofocus

--- src/components/forms/Event/Form.js
+++ src/components/forms/Event/Form.js
@@ -153,7 +153,6 @@ class Form extends React.Component {
                 onChangeText={handleChange('title')}
                 onBlur={handleBlur('title')}
                 mode="outlined"
-                autoFocus
               />
               <HelperText
                 type="error"
