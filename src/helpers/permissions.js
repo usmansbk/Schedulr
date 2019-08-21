28b27afedf69d72220ed2af8236748de0db76690
Usman Suleiman <usmansbk@gmail.com>
2019-08-21 23:06:28 +0100

    chore/i18n

--- src/helpers/permissions.js
+++ src/helpers/permissions.js
@@ -1,16 +1,17 @@
 import { PermissionsAndroid } from 'react-native';
 import SimpleToast from 'react-native-simple-toast';
+import { I18n } from 'aws-amplify';
 
 async function requestLocationPermission() {
   try {
     const granted = await PermissionsAndroid.request(
       PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
       {
-        title: 'Schdlr Location Permission',
-        message: "Schdlr App needs access to your location so events are easier to find.",
-        buttonPositive: "OK",
-        buttonNegative: "Cancel",
-        buttonNeutral: "Ask Me Later"
+        title: I18n.get('Schdlr Location Permission'),
+        message: I18n.get("Schdlr App needs access to your location so events are easier to find."),
+        buttonPositive: I18n.get("OK"),
+        buttonNegative: I18n.get("Cancel"),
+        buttonNeutral: I18n.get("Ask Me Later")
       }
     );
     return granted === PermissionsAndroid.RESULTS.GRANTED;
