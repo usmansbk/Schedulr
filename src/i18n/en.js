9b5a962ca5cfe2a6e16ec3abd57a968fdf60f77c
Usman Suleiman <usmansbk@gmail.com>
2019-08-21 17:47:43 +0100

    Chore/i18n

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -220,7 +220,7 @@ export default {
   ERROR_serverError: "Oop! My fault",
   ERROR_noConnection: "No connection",
   ERROR_404: "Not found",
-  ERROR_404_caption: "Item may have been removed",
+  ERROR_404_caption: "Item may have been deleted",
 
   PLACEHOLDER_aboutThisEvent: "About this event...",
   PLACEHOLDER_normal: "Normal",
