afb1bade69c22dd770cfeef886c8bdc097bafa66
Usman Suleiman <usmansbk@gmail.com>
2019-08-27 03:17:28 +0100

    chore

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -131,7 +131,7 @@ export default {
   ALERT_deleteType: "Delete type?",
   ALERT_deleteImage: "Delete image?",
   ALERT_unfollow: name => `Unfollow ${name}?`,
-  ALERT_unfollowMessage: 'Their events will no long show up in your calendar.',
+  ALERT_unfollowMessage: 'Their events will no long show up on your calendar.',
 
   SHARE_inviteAFriendTitle: "Invite via...",
   SHARE_SCHEDULE_inviteTitle: "Share invite link via...",
