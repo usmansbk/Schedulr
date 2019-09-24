aa0f070071ab0caca096db232b182135d4af80c2
Usman Suleiman <usmansbk@gmail.com>
2019-09-24 16:26:13 +0100

    chore

--- src/components/forms/Event/Form.js
+++ src/components/forms/Event/Form.js
@@ -143,7 +143,6 @@ class Form extends React.Component {
                 onChangeText={handleChange('title')}
                 onBlur={handleBlur('title')}
                 mode="outlined"
-                autoFocus={!edit}
               />
               <HelperText
                 type="error"
