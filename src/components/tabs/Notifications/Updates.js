c47bb6a88ecde691d32be9673a5398543f2769a7
Usman Suleiman <usmansbk@gmail.com>
2019-09-24 20:06:17 +0100

    chore

--- src/components/tabs/Notifications/Updates.js
+++ src/components/tabs/Notifications/Updates.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import Alert from 'components/dialogs/Alert';
 import { inject, observer } from 'mobx-react';
 import { I18n } from 'aws-amplify';
 import List from 'components/lists/Updates';
-import Fab from 'components/common/Fab';
 
 class Updates extends React.Component {
 
@@ -12,43 +10,17 @@ class Updates extends React.Component {
       tabBarLabel: I18n.get("NOTIFICATIONS_updatesTabLabel")
     };
   }
-
-  state = {
-    showClearWarning: false
-  };
-
-  _hideDialog = () => this.setState({ showClearWarning: false });
-
-  _clearNotifications = () => this.setState({ showClearWarning: true });
   
   render() {
     const { stores, navigation } = this.props;
     stores.notificationsStore.setNotificationIndicator(false);
     
     return (
-      <>
-        <List
-          updates={stores.notificationsStore.updates}
-          styles={stores.appStyles.notifications}
-          navigation={navigation}
-        />
-        {
-          !!stores.notificationsStore.updates.length && (
-            <Fab
-              onPress={this._clearNotifications}
-              icon="x"
-              small
-            />
-          )
-        }
-        <Alert
-          title={I18n.get("ALERT_clearNotifications")}
-          visible={this.state.showClearWarning}
-          handleDismiss={this._hideDialog}
-          confirmText={I18n.get("BUTTON_ok")}
-          onConfirm={stores.notificationsStore.clearNotifications}
-        />
-      </>
+      <List
+        updates={stores.notificationsStore.updates}
+        styles={stores.appStyles.notifications}
+        navigation={navigation}
+      />
     )
   }
 }
