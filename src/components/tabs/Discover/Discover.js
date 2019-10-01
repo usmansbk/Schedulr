1c29158a3cebc9ab4741ce2e1b0e047f908497d5
Usman Suleiman <usmansbk@gmail.com>
2019-10-01 13:04:09 +0100

    ignore

--- src/components/tabs/Discover/Discover.js
+++ src/components/tabs/Discover/Discover.js
@@ -7,7 +7,6 @@ class Discover extends React.Component {
   componentDidMount = async () => await this.props.stores.locationStore.fetchLocation(true);
 
   render() {
-    console.log(this.props.stores.locationStore.point);
     return (
       <List
         location={this.props.stores.locationStore.point}
