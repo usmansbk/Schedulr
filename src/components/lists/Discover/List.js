f1cdb2541c3adb8c711da763d387a5c7bc0e4e6d
Usman Suleiman <usmansbk@gmail.com>
2019-10-01 12:10:42 +0100

    chore/remove redundant dependencies

--- src/components/lists/Discover/List.js
+++ src/components/lists/Discover/List.js
@@ -1,23 +1,91 @@
 import React, {Component} from 'react';
-import { withCollapsible } from 'react-navigation-collapsible';
 import { FlatList } from 'react-navigation';
 import { inject, observer } from 'mobx-react';
 import Empty from './Empty';
 import Header from './Header';
-import Item from './Item';
+import Item from 'components/lists/Bookmarks/Item';
+import {
+  parseRepeat,
+  getStatus,
+  getCategory
+} from 'lib/parseItem';
+import {
+  getDuration,
+  getHumanTime
+} from 'lib/time';
+import { bookmarkedEvents } from 'lib/constants';
+import getImageUrl from 'helpers/getImageUrl';
 
-class DISCOVERTab extends Component{
+const { ITEM_HEIGHT, SEPARATOR_HEIGHT } = bookmarkedEvents;
+
+class List extends Component{
   static defaultProps = {
     data: [],
     loading: false,
     error: false
   };
 
+  _getItemLayout = (_, index) => (
+    {
+      length: ITEM_HEIGHT,
+      offset: ITEM_HEIGHT * index + SEPARATOR_HEIGHT,
+      index
+    }
+  );
+
   _renderEmptyList = () => <Empty />;
 
   _renderHeader = () => <Header navigation={this.props.navigation} />;
 
-  renderItem = ({item}) => <Item />;
+  _renderItem = ({ item }) => {
+    const {
+      id,
+      title,
+      category,
+      isCancelled,
+      allDay,
+      cancelledDates,
+      isPublic,
+      isOwner,
+      banner,
+      startAt,
+      endAt,
+      recurrence,
+      venue,
+      schedule,
+      isConcluded,
+      isBookmarked,
+      bookmarksCount,
+      commentsCount
+    } = item;
+
+    return (<Item
+      id={id}
+      title={title}
+      status={getStatus({
+        isCancelled,
+        cancelledDates,
+        startAt, endAt, isConcluded
+      })}
+      startAt={startAt}
+      endAt={endAt}
+      allDay={allDay}
+      pictureUrl={banner && getImageUrl(banner)}
+      isBookmarked={isBookmarked}
+      isAuth={isPublic || isOwner || (schedule && schedule.isFollowing)}
+      bookmarksCount={bookmarksCount}
+      commentsCount={commentsCount}
+      category={getCategory(category)}
+      recurrence={parseRepeat(recurrence)}
+      time={getHumanTime({ allDay, startAt, endAt })}
+      eventScheduleId={schedule && schedule.id}
+      duration={getDuration(startAt, endAt, allDay)}
+      address={venue}
+      onPressItem={this._onPressItem}
+      onPressComment={this._navigateToComments}
+      navigateToBanner={this._navigateToBanner}
+    />);
+  };
 
   componentDidMount = async () => await this.props.stores.locationStore.fetchLocation(true);
 
@@ -27,16 +95,20 @@ class DISCOVERTab extends Component{
     const styles = this.props.stores.appStyles.discover;
 
     return (
-      <FlatList 
+      <FlatList
         style={styles.list}
         data={this.props.data}
         renderItem={this.renderItem}
         keyExtractor={this._keyExtractor}
         ListEmptyComponent={this._renderEmptyList}
-        sticky
+        ListHeaderComponent={this._renderHeader}
+        initialNumToRender={5}
+        getItemLayout={this._getItemLayout}
+        stickyHeaderIndices={[0]}
+        contentContainerStyle={styles.contentContainer}
       />
     )
   }
 }
 
-export default inject("stores")(observer(DISCOVERTab));
+export default inject("stores")(observer(List));
