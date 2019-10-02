b6f874160173a80bff4f00d79f96ba8724d0e089
Usman Suleiman <usmansbk@gmail.com>
2019-10-02 13:55:39 +0100

    chore/add illustrations

--- src/components/common/ImageViewer/Screen.js
+++ src/components/common/ImageViewer/Screen.js
@@ -8,60 +8,62 @@ import Loading from '../Loading';
 
 export default inject("stores")(observer(
   ({s3Object, goBack, title, uploadPhoto, deletePhoto, uri, stores, me, loading }) => (
-    <View style={{ flex: 1, backgroundColor: stores.themeStore.colors.bg }}>
-      <Appbar.Header style={stores.appStyles.styles.header}>
-        <Appbar.Action
-          color={stores.themeStore.colors.gray}
-          icon={({color, size}) => <Icon
-            name="arrow-left"
-            onPress={goBack}
-            color={color}
-            size={size}
-          />}
-        />
-        <Appbar.Content
-          title={title}
-          titleStyle={stores.appStyles.styles.headerColor}
-        />
-        {
-          me && (
-            <>
-            {
-              Boolean(s3Object) && (
-                <Appbar.Action
-                  color={stores.themeStore.colors.gray}
-                  icon={({ color, size }) => <Icon
-                    name="trash-2"
-                    onPress={deletePhoto}
-                    size={size}
-                    color={color}
-                  />}
-                />
-              )
-            }
-            <Appbar.Action
-              color={stores.themeStore.colors.gray}
-              icon={({ color, size }) => <Icon
-                name="camera"
-                onPress={uploadPhoto}
-                size={size}
-                color={color}
-              />}
-            />
-            </>
-          )
-        }
-      </Appbar.Header>
+    <>
+    <Appbar.Header style={stores.appStyles.styles.header}>
+      <Appbar.Action
+        color={stores.themeStore.colors.gray}
+        icon={({color, size}) => <Icon
+          name="arrow-left"
+          onPress={goBack}
+          color={color}
+          size={size}
+        />}
+      />
+      <Appbar.Content
+        title={title}
+        titleStyle={stores.appStyles.styles.headerColor}
+      />
       {
-        loading ? <Loading /> : (
+        me && (
+          <>
+          {
+            Boolean(s3Object) && (
+              <Appbar.Action
+                color={stores.themeStore.colors.gray}
+                icon={({ color, size }) => <Icon
+                  name="trash-2"
+                  onPress={deletePhoto}
+                  size={size}
+                  color={color}
+                />}
+              />
+            )
+          }
+          <Appbar.Action
+            color={stores.themeStore.colors.gray}
+            icon={({ color, size }) => <Icon
+              name="camera"
+              onPress={uploadPhoto}
+              size={size}
+              color={color}
+            />}
+          />
+          </>
+        )
+      }
+    </Appbar.Header>
+    <View style={{ flex: 1, justifyContent: 'center', backgroundColor: stores.themeStore.colors.bg }}>
+      {
+        loading ? <Loading loading /> : (
           <PhotoView
-            source={uri ? {uri} : require('./img/placeholder.png')}
+            source={uri ? {uri} : require('../../../assets/photographer.png')}
             androidScaleType="fitCenter"
-            style={{flex: 1}}
-            loadingIndicatorSource={require('./img/placeholder.png')}
+            style={ uri ? {flex: 1} : { alignSelf: 'center', width: 400, height: 400 }}
+            loadingIndicatorSource={require('../../../assets/loading-indicator.png')}
           />
         )
       }
     </View>
+    </>
   )
 ));
