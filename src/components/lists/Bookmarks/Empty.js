53321ef85a23870f333e70517ce5b3ff97eda026
Usman Suleiman <usmansbk@gmail.com>
2019-10-02 09:37:00 +0100

    chore/add illustrations

--- src/components/lists/Bookmarks/Empty.js
+++ src/components/lists/Bookmarks/Empty.js
@@ -6,15 +6,18 @@ import { inject, observer } from 'mobx-react';
 
 export default inject('stores')(observer(
   ({ error, loading, stores }) =>{
-    if (loading) return null;
     return (
       <View style={stores.appStyles.bookmarkedEventsList.empty}>
-        <Image resizeMode="contain" style={{ width: 200, height: 200 }} source={require('../../../assets/calendar.png')} />
-        <Headline style={stores.appStyles.bookmarkedEventsList.emptyTitle}>
+        { loading ? <Image resizeMode="contain" style={{ width: 200, height: 200 }} source={require('../../../assets/food-delivery.png')} /> : (
+          <Image resizeMode="contain" style={{ width: 200, height: 200 }} source={require('../../../assets/support-woman.png')} />
+        )}
+        {
+          !loading && <Headline style={stores.appStyles.bookmarkedEventsList.emptyTitle}>
           {
             I18n.get("BOOKMARKS_emptyList")
           }
-        </Headline>
+          </Headline>
+        }
       </View>
     );  
   }
--- src/components/lists/Comments/Empty.js
+++ src/components/lists/Comments/Empty.js
@@ -5,14 +5,20 @@ import { inject, observer } from 'mobx-react';
 import { I18n } from 'aws-amplify';
 
 export default inject('stores')(observer(
-  ({ error, loading, stores }) => loading ? null : (
+  ({ error, loading, stores }) => (
     <View style={stores.appStyles.commentsList.empty}>
-      <Image resizeMode="contain" style={{ width: 200, height: 200 }} source={require('../../../assets/support-woman.png')} />
-      <Headline style={stores.appStyles.commentsList.emptyTitle}>
+      {
+        loading ? <Image resizeMode="contain" style={{ width: 200, height: 200 }} source={require('../../../assets/food-delivery.png')} /> : (
+          <Image resizeMode="contain" style={{ width: 200, height: 200 }} source={require('../../../assets/support-woman.png')} />
+        )
+      }
+      
+      { !loading && <Headline style={stores.appStyles.commentsList.emptyTitle}>
         {
           error ? I18n.get("ERROR_networkError") : I18n.get("COMMENTS_emptyList")
         }
-      </Headline>
+        </Headline>
+      }
       {
         error && (
           <Paragraph style={stores.appStyles.commentsList.paragraph}>
