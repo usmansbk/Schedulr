4e0a831e7d0321b9e40299dd6e649522119ee180
Usman Suleiman <usmansbk@gmail.com>
2019-10-07 18:54:08 +0100

    chore

--- src/components/screens/AuthLoading/Container.js
+++ src/components/screens/AuthLoading/Container.js
@@ -12,12 +12,9 @@ class Container extends Component {
     } else {
       try {
         await Auth.currentAuthenticatedUser();
-        if (this.props.stores.appState.userId) {
-          this.props.navigation.navigate('App');
-        } else {
-          await Auth.signOut();
-        }
+        this.props.navigation.navigate('App');
       } catch (error) {
+        console.log(error.message);
         this._navigateToAuth();
       }
     }
