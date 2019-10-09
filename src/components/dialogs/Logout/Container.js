4d23a9bd3b08a49f8f412ece032a2a53dba01e8d
Usman Suleiman <usmansbk@gmail.com>
2019-10-09 07:17:39 +0100

    chore

--- src/components/dialogs/Logout/Container.js
+++ src/components/dialogs/Logout/Container.js
@@ -13,9 +13,9 @@ class Container extends React.Component {
   _signOut = async () => {
     this.setState({ loading: true });
     try { await this.props.client.clearStore(); } catch(error){}
-    this.props.stores.reset();
     try { await Auth.signOut(); } catch(error) {}
     this._handleDimiss();
+    this.props.stores.reset();
     Cache.clear();
     this.props.navigation.navigate('Auth');
   };
