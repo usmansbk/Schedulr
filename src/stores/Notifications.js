f34c59d87c412d49658732db5517a85a752776d1
Usman Suleiman <usmansbk@gmail.com>
2019-10-09 18:55:10 +0100

    chore

--- src/stores/Notifications.js
+++ src/stores/Notifications.js
@@ -3,7 +3,6 @@ import { persist } from 'mobx-persist';
 import moment from 'moment';
 
 export default class Notifications {
-  @persist @observable newCommentsCount = 0;
   @persist @observable lastSyncTimestamp = moment().unix();
   @persist @observable hasNotifications = false;
 
@@ -12,7 +11,6 @@ export default class Notifications {
   @action updateLastSyncTimestamp = () => this.lastSyncTimestamp = moment().unix();
   @action appendNotifications = newNotifications => {
     if (newNotifications.length) {
-      this.newCommentsCount += newNotifications.filter(notif => notif.type === 'Comment').length;
       this.setNotificationIndicator(true);
       this.notifications = this.notifications.concat(newNotifications);
     }
@@ -33,5 +31,9 @@ export default class Notifications {
 
   @computed get comments() {
     return this.notifications.filter(notif => notif.type === 'Comment').sort((a, b) => -(a.timestamp - b.timestamp));
+  }
+
+  @computed get newCommentsCount() {
+    return this.notifications.filter(notif => notif.type === 'Comment').length;
   }
 }
