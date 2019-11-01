1aa9ac294af98b00bfeca93ece9e3c6c74113ca7
Babakolo Usman Suleiman <usmansbk@gmail.com>
2019-11-01 19:41:33 +0100

    chore/add image placeholder

--- src/components/common/UserAvatar/Image.js
+++ src/components/common/UserAvatar/Image.js
@@ -1,33 +1,25 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { Image } from 'react-native';
 import colors from 'config/colors';
 
 export default class CustomImage extends React.Component {
 
-  _imageStyle = {
+  _size = {
     width: this.props.size - 1,
-    height: this.props.size - 1,
+    height: this.props.size - 1,  
+  };
+
+  _imageStyle = {
     borderRadius: (this.props.size - 1) / 2,
     borderWidth: 1,
     borderColor: colors.light_gray_3,
+    ...this._size
   };
 
   _style = {
-    width: this.props.size - 1,
-    height: this.props.size - 1,
     borderRadius: (this.props.size - 1) / 2,
+    ...this._size
   };
-
-  _placeholder = {
-    width: this.props.size - 1,
-    height: this.props.size - 1,
-    borderRadius: (this.props.size - 1) / 2,
-    flex: 1,
-    alignSelf: 'center',
-    backgroundColor: colors.light_gray
-  };
-
-  _renderPlaceholder = () => <View style={this._placeholder} />;
 
   shouldComponentUpdate = (nextProps) => (this.props.uri !== nextProps.uri);
 
@@ -37,10 +29,9 @@ export default class CustomImage extends React.Component {
     return (
       <Image
         source={{ uri }}
-        indicator={this._renderPlaceholder}
         imageStyle={this._imageStyle}
         style={this._style}
-        indicatorProps={{indeterminate: false}}
+        loadingIndicatorSource={require('./img/placeholder.png')}
       />
     );
   }
