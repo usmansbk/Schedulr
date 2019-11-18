d7461ebeee29f2ffd074b61b8eca20cc37d69448
Babakolo Usman Suleiman <usmansbk@gmail.com>
2019-11-18 02:53:58 +0100

    refactor/move deltasync to store

--- src/components/tabs/Events/Hoc.js
+++ src/components/tabs/Events/Hoc.js
@@ -1,58 +1,35 @@
 import { graphql, compose } from 'react-apollo';
 import gql from 'graphql-tag';
-import { inject, observer } from 'mobx-react';
 import { withNavigationFocus } from 'react-navigation';
-import { getUserData, getDeltaUpdates } from 'api/queries';
+import { getUserData } from 'api/queries';
 import { baseEventsFilter } from 'api/filters';
-import updateBaseQuery from 'helpers/deltaSync';
 import Events from './Events';
 
 const alias = 'withEventsContainer';
 const BaseQuery = gql(getUserData);
-const DeltaQuery = gql(getDeltaUpdates);
 
-export default inject("stores")(observer(
-  compose(
-    withNavigationFocus,
-    graphql(BaseQuery, {
-      alias,
-      options: props => ({
-        fetchPolicy: 'cache-first',
-        notifyOnNetworkStatusChange: true,
-        variables: {
-          id: props.id,
-          filter: baseEventsFilter(),
-          limit: 50
-        }
-      }),
-      props: ({ data, ownProps}) => ({
-        loading: data && (data.loading || data.networkStatus === 4),
-        fetchingMore: data && data.networkStatus === 3,
-        data: data && data.getUserData,
-        userId: ownProps.stores.appState.userId,
-        onRefresh: () => data.refetch({
-          id: ownProps.id,
-          filter: baseEventsFilter(),
-          limit: 50
-        }),
-        fetchMore: () => {
-          const lastSyncTimestamp = ownProps.stores.appState.lastSyncTimestamp;
-          data.fetchMore({
-            query: DeltaQuery,
-            variables:{
-              lastSync: String(lastSyncTimestamp)
-            },
-            updateQuery: (prev, { fetchMoreResult }) => (
-              updateBaseQuery({
-                prev,
-                fetchMoreResult,
-                stores: ownProps.stores
-              })
-            )
-          })
-        },
-        ...ownProps
-      })
+export default compose(
+  withNavigationFocus,
+  graphql(BaseQuery, {
+    alias,
+    options: props => ({
+      fetchPolicy: 'cache-first',
+      notifyOnNetworkStatusChange: true,
+      variables: {
+        id: props.id,
+        filter: baseEventsFilter(),
+        limit: 50
+      }
     }),
-  )(Events)
-));
+    props: ({ data, ownProps}) => ({
+      loading: data && (data.loading || data.networkStatus === 4),
+      data: data && data.getUserData,
+      onRefresh: () => data.refetch({
+        id: ownProps.id,
+        filter: baseEventsFilter(),
+        limit: 50
+      }),
+      ...ownProps
+    })
+  }),
+)(Events);
