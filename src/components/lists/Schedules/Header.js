e41ba13a3fe7425727102f5d6bddd700cd451793
Babakolo Usman Suleiman <usmansbk@gmail.com>
2019-11-20 13:03:45 +0100

    chore/cleanup

--- src/components/lists/Schedules/Header.js
+++ src/components/lists/Schedules/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
 import { inject, observer } from 'mobx-react';
-import HeaderBanner from 'components/ads/HeaderBanner';
+import Banner from 'components/ads/Banner';
 
 export default inject("stores")(observer(({ stores }) => {
  return (
    <View style={stores.appStyles.schedulesList.header}>
-     <HeaderBanner />
+     <Banner />
    </View>
  ) 
 }));
