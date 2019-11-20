d0f2d40b66fbbc6f6c8ecacdf254297b5657b272
Babakolo Usman Suleiman <usmansbk@gmail.com>
2019-11-20 17:31:03 +0100

    refactor/support more sizes

--- src/components/ads/Banner.js
+++ src/components/ads/Banner.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { BannerAd, BannerAdSize, TestIds } from '@react-native-firebase/admob';
 import { inject, observer } from 'mobx-react';
-import env from 'config/env';
 
 class Banner extends React.Component {
   shouldComponentUpdate = () => false;
 
   render() {
-    const { large } = this.props;
+    const { large, medium_rect } = this.props;
+    let size = BannerAdSize.SMART_BANNER;
+    if (large) size = BannerAdSize.LARGE_BANNER;
+    else if (medium_rect) size = BannerAdSize.MEDIUM_RECTANGLE;
+
     return <BannerAd
       unitId={TestIds.BANNER}
-      size={large ? BannerAdSize.LARGE_BANNER : BannerAdSize.SMART_BANNER}
+      size={size}
       requestOptions={{
         requestNonPersonalizedAdsOnly: true,
         location: this.props.stores.locationStore.adLocation
