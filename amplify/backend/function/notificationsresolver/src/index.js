354bb6e480273494fc768cfa9197cea45c5ccf06
Babakolo Usman Suleiman <usmansbk@gmail.com>
2019-11-22 02:39:36 +0100

    chore/return dummy response

--- amplify/backend/function/notificationsresolver/src/index.js
+++ amplify/backend/function/notificationsresolver/src/index.js
@@ -5,9 +5,6 @@ var region = process.env.REGION
 
 Amplify Params - DO NOT EDIT */
 
-exports.handler = function (event, context) { //eslint-disable-line
-  console.log(`value1 = ${event.key1}`);
-  console.log(`value2 = ${event.key2}`);
-  console.log(`value3 = ${event.key3}`);
-  context.done(null, 'Hello World'); // SUCCESS with message
+exports.handler = async function (event, context) { //eslint-disable-line
+  return [];
 };
