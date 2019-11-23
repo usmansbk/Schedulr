e9a57152493e82fa2bf85386ae3bf16221e451bb
Babakolo Usman Suleiman <usmansbk@gmail.com>
2019-11-23 15:43:53 +0100

    chore/setup workspace for lambda

--- amplify/backend/function/deltasyncresolver/src/index.js
+++ amplify/backend/function/deltasyncresolver/src/index.js
@@ -1,6 +1,9 @@
 
 
 exports.handler = async function (event, context) { //eslint-disable-line
+  const lastSync = event.arguments.lastSync;
+  const userId = event.identity.claims.email;
+
   return {
     events: [],
     schedules: []
