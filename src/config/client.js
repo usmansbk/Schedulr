eae7dd7fbee84e7059af40dd12b7107e716b86fa
Babakolo Usman Suleiman <usmansbk@gmail.com>
2019-12-05 06:36:18 +0100

    refactor/modular

--- src/config/client.js
+++ src/config/client.js
@@ -5,6 +5,7 @@ import { onError } from 'apollo-link-error';
 import aws_config from 'aws_config';
 import logger from './logger';
 import stores from 'stores';
+import callback from 'helpers/offlineConfig';
 import {
   EVENT_TYPE,
   SCHEDULE_TYPE,
@@ -58,51 +59,7 @@ const client = new AWSAppSyncClient({
     }
   },
   offlineConfig: {
-    callback: (error, success) => {
-      if (error) {
-        const { mutation, variables: { input } } = error;
-        let message = '';
-        switch(mutation) {
-          case 'createEvent':
-          case 'updateEvent':
-            message = I18n.get(`ERROR_failedToCreateEvent`)(input.title);
-            break;
-          case 'createSchedule':
-          case 'updateSchedule':
-            message = I18n.get(`ERROR_failedToCreateSchedule`)(input.name);
-            break;
-          case 'createComment':
-            message = I18n.get(`ERROR_failedToCreateComment`)(input.content);
-            break;
-          case 'createBookmark':
-            message = I18n.get(`ERROR_failedToCreateBookmark`);
-            break;
-          case 'createFollow':
-            message = I18n.get(`ERROR_failedToCreateFollow`);
-            break;
-          case 'deleteEvent':
-            message = I18n.get(`ERROR_failedToDeleteEvent`);
-            break;
-          case 'deleteSchedule':
-            message = I18n.get(`ERROR_failedToDeleteSchedule`);
-            break;
-          case 'deleteFollow':
-            message = I18n.get(`ERROR_failedToDeleteFollow`);
-            break;
-          case 'deleteBookmark':
-            message = I18n.get(`ERROR_failedToDeleteBookmark`);
-            break;
-          case 'deleteComment':
-            message = I18n.get(`ERROR_failedToDeleteComment`);
-            break;
-          default:
-            message = I18n.get(`ERROR_fatal`);
-            break;
-        }
-        message = message + I18n.get('ERROR_configHint');
-        stores.snackbar.show(message, true);
-      }
-    }
+    callback 
   }
 }, { link });
 
---
+++ src/helpers/offlineConfig.js
@@ -0,0 +1,51 @@
+import { I18n } from 'aws-amplify';
+import stores from 'stores';
+
+export default(error, success) => {
+  if (error) {
+    const { mutation, variables: { input } } = error;
+    let message = '';
+    switch(mutation) {
+      case 'createEvent':
+      case 'updateEvent':
+        message = I18n.get(`ERROR_failedToCreateEvent`)(input.title);
+        break;
+      case 'createSchedule':
+      case 'updateSchedule':
+        message = I18n.get(`ERROR_failedToCreateSchedule`)(input.name);
+        break;
+      case 'createComment':
+        message = I18n.get(`ERROR_failedToCreateComment`)(input.content);
+        break;
+      case 'createBookmark':
+        message = I18n.get(`ERROR_failedToCreateBookmark`);
+        break;
+      case 'createFollow':
+        message = I18n.get(`ERROR_failedToCreateFollow`);
+        break;
+      case 'deleteEvent':
+        message = I18n.get(`ERROR_failedToDeleteEvent`);
+        break;
+      case 'deleteSchedule':
+        message = I18n.get(`ERROR_failedToDeleteSchedule`);
+        break;
+      case 'deleteFollow':
+        message = I18n.get(`ERROR_failedToDeleteFollow`);
+        break;
+      case 'deleteBookmark':
+        message = I18n.get(`ERROR_failedToDeleteBookmark`);
+        break;
+      case 'deleteComment':
+        message = I18n.get(`ERROR_failedToDeleteComment`);
+        break;
+      default:
+        message = I18n.get(`ERROR_fatal`);
+        break;
+    }
+    message = message + I18n.get('ERROR_configHint');
+    stores.snackbar.show(message, true);
+  } else {
+    console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ success')
+    console.log(JSON.stringify(success))
+  }
+}
