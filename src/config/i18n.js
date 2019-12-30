1a0cad384f3e8207bc84e440e7b8657fedc2d2e8
Babakolo Usman Suleiman <usmansbk@gmail.com>
2019-12-30 01:32:04 +0100

    chore

--- src/config/i18n.js
+++ src/config/i18n.js
@@ -1,7 +1,9 @@
 import { I18n } from 'aws-amplify';
+import moment from 'moment';
 import dict from 'i18n';
 
 export default (lang) => {
   I18n.setLanguage(lang || 'en');
+  moment.locale(lang);
   I18n.putVocabularies(dict);
 };
--- src/stores/AppState.js
+++ src/stores/AppState.js
@@ -38,7 +38,7 @@ export default class AppState {
     showPrivateScheduleAlert: true,
     showAppIntro: true
   };
-  @persist('list') @observable categories =  I18n.get('categories');
+  @persist('list') @observable categories =  Array.from(I18n.get('categories'));
 
   @action setUserId = id => this.userId = id;
   @action updateLastSyncTimestamp = () => this.lastSyncTimestamp = moment().unix();
@@ -79,7 +79,7 @@ export default class AppState {
       showPrivateScheduleAlert: true,
       showAppIntro: false
     };
-    this.categories = I18n.get('categories');
+    this.categories = Array.from(I18n.get('categories'));
     this.loggingIn = false;
     this.userId = null;
     this.lastSyncTimestamp = moment().unix();
