6ec96736b6cfec4a5d636f1d36a2b35db0a31067
Babakolo Usman Suleiman <usmansbk@gmail.com>
2019-12-31 00:11:13 +0100

    chore

---
+++ src/components/screens/Settings/Item.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  List,
+  Switch,
+} from 'react-native-paper';
+import Icon from 'react-native-vector-icons/Feather';
+import { inject, observer } from 'mobx-react';
+import { I18n } from 'aws-amplify';
+
+class Item extends React.Component {
+  _handleRemindMeDialog = () => this.props.handleRemindMeDialog();
+  _handleValueChange = () => this.props.handleValueChange(this.props.item.key);
+  shouldComponentUpdate = nextProps => Boolean(nextProps.value !== this.props.value);
+
+  render() {
+    const { color, item, value } = this.props;
+    
+    if (item.key === 'remindMe') {
+      return (
+        <List.Item
+          title={I18n.get("SETTINGS_remindMe")}
+          right={() => <List.Icon
+            icon={() => <Icon
+              name="chevron-right"
+              color={color}
+              size={24}
+            />}
+          />}
+          onPress={this._handleRemindMeDialog}
+        />
+      )
+    }
+    return (
+      <List.Item
+        title={I18n.get(`SETTINGS_${item.key}`)}
+        right={() => (
+          <Switch
+            value={value}
+            onValueChange={this._handleValueChange}
+          />
+        )}
+      />
+    );
+  }
+}
+
+export default inject("stores")(observer(Item));
---
+++ src/components/screens/Settings/items.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    title: 'general',
+    data: [
+      {
+        key: 'sound'
+      },
+      {
+        key: 'vibrate'
+      },
+      {
+        key: 'darkTheme'
+      }
+    ]
+  },
+  {
+    title: 'reminder',
+    data: [
+      {
+        key: 'disableReminders',
+        title: 'reminderDisable',
+        description: "WARNING_dontMissOut"
+      },
+      {
+        key: 'remindMe',
+      }
+    ]
+  },
+  {
+    title: 'push',
+    data: [
+      {
+        title: 'pushDisable',
+        key: 'disablePush'
+      },
+      {
+        title: 'disableComment',
+        key: 'disableComments'
+      }
+    ]
+  }
+];
