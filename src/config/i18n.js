0a05883720e1c3e9f4228764302798b77272b6df
Babakolo Usman Suleiman <usmansbk@gmail.com>
2019-12-31 15:21:56 +0100

    chore/get device locale

--- src/config/i18n.js
+++ src/config/i18n.js
@@ -1,9 +1,13 @@
 import { I18n } from 'aws-amplify';
+import * as RNLocalize from 'react-native-localize';
 import moment from 'moment';
 import dict from 'i18n';
 
-export default (lang) => {
-  I18n.setLanguage(lang || 'en');
-  moment.locale(lang);
+export default () => {
+  const locales = RNLocalize.getLocales();
+  const locale = locales[0];
+  const language = locale.languageCode;
+  I18n.setLanguage(language);
+  moment.locale(language);
   I18n.putVocabularies(dict);
 };
