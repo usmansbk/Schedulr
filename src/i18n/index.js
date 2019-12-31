f56ca898a09752c604c4b408c9772cec181157bd
Babakolo Usman Suleiman <usmansbk@gmail.com>
2019-12-31 23:56:52 +0100

    chore

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,7 +1,10 @@
-
+let momentLocale;
 export default (lang) => {
   switch(lang) {
     case 'en': case 'en-GB':
+      return require('./en').default;
+    case 'fr':
+      momentLocale = require('moment/locale/fr');
       return require('./en').default;
     default:
       return require('./en').default;
