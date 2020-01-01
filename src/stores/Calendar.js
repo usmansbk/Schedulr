a39fbe4e7e044a94e5e0bdde3d0ab692980d9c0b
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-01-01 01:18:43 +0100

    chore

--- src/stores/Calendar.js
+++ src/stores/Calendar.js
@@ -113,7 +113,7 @@ export default class Calendar {
   @action fetchEvents = async () => {
     try {
       if (this.calendars.length) {
-        const m = moment();
+        const m = moment().startOf('D');
         const startDate = m.toDate().toISOString();
         const endDate = m.clone().add(1, 'year').toDate().toISOString();
         this.events = await RNCalendarEvents.fetchAllEvents(startDate, endDate, this.calendars.map(cal => cal.id));
