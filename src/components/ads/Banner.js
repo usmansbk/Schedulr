1a2d7ee4cb842d8187bf1f771723f5dafd5ead8f
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-01-01 20:19:36 +0100

    chore

--- src/components/ads/Banner.js
+++ src/components/ads/Banner.js
@@ -1,92 +1,33 @@
 import React from 'react';
-import { ImageBackground } from 'react-native';
-import { BannerAd, BannerAdSize, TestIds } from '@react-native-firebase/admob';
-import { inject, observer } from 'mobx-react';
-import env from 'config/env';
+import { Image } from 'react-native';
 import { getSeason } from 'lib/utils';
 
-class Banner extends React.Component {
+export default class Banner extends React.Component {
   shouldComponentUpdate = () => false;
-
-  _onLoad = () => {
-
-  };
-  
-  _onError = (error) => {
-    console.log('Advert failed to load: ', error);
-  };
 
   render() {
     const season = getSeason();
-    const { large, medium_rect } = this.props;
-    let Unit = __DEV__ ? TestIds : env;
-    let size = BannerAdSize.BANNER;
-    let unitId = Unit.BANNER;
-    let style = {
-      height: 50,
+    const style = {
+      height: 100,
       width: '100%',
       alignItems: 'center',
-      justifyContent: 'center'
     };
-    
-    if (large) {
-      size = BannerAdSize.LARGE_BANNER;
-      unitId = Unit.BANNER;
-      style = {
-        height: 100,
-        width: '100%',
-        alignItems: 'center',
-      };
-    } else if (medium_rect) {
-      size = BannerAdSize.MEDIUM_RECTANGLE;
-      unitId = Unit.BANNER;
-      style = {
-        width: 300,
-        height: 250
-      };
-    }
+    let source;
     if (season === 'winter') {
-      source = require('../../assets/winter.jpeg');
+      source = require('../../assets/winter.png');
     } else if (season === 'autumn') {
-      source = require('../../assets/autumn.jpeg');
+      source = require('../../assets/autumn.png');
     } else if (season === 'spring') {
-      source = require('../../assets/spring.jpeg');
+      source = require('../../assets/spring.png');
     } else {
-      source = require('../../assets/sunset.png');
+      source = require('../../assets/summer.png');
     }
 
     return (
-      <ImageBackground
+      <Image
         source={source}
         style={style}
-       >
-         {
-           false && (
-            <BannerAd
-              unitId={unitId}
-              size={size}
-              requestOptions={{
-                requestNonPersonalizedAdsOnly: true,
-                location: this.props.stores.locationStore.adLocation,
-                keywords: [
-                  'scholarship',
-                  'fashion',
-                  'clothing',
-                  'student',
-                  'job',
-                  'news',
-                  'transport',
-                  'ride'
-                ]
-              }}
-              onAdLoaded={this._onLoad}
-              onAdFailedToLoad={this._onError}
-            />
-           )
-         }
-      </ImageBackground>
+      />
     )
   }
 }
-
-export default inject("stores")(observer(Banner));
