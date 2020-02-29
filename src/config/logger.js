2e1a186f5c7eb2dd6494dc1a254223923fca4585
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-02-29 19:39:02 +0100

    chore

--- src/config/logger.js
+++ src/config/logger.js
@@ -2,16 +2,16 @@ import crashlytics from '@react-native-firebase/crashlytics';
 
 function logError(error) {
   let e = error;
-  if (!(error instanceof Error)) {
+  if (!(error instanceof Error) && __DEV__) {
     if (typeof error === 'object') e = JSON.stringify(error);
     e = new Error(e);
+    console.log(e);
   }
-  console.log(e);
   crashlytics().recordError(e);
 }
 
 function log(message) {
-  if (message) {
+  if (message && __DEV__) {
     console.log(message);
     crashlytics().log(message);
   }
