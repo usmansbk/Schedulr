7d787c053669b7d0c07b121d3bebf0e22c18b2f2
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-04-26 14:39:04 +0100

    chore

--- src/components/common/UserAvatar/Image.js
+++ src/components/common/UserAvatar/Image.js
@@ -1,24 +1,12 @@
 import React from 'react';
 import { Image } from 'react-native';
-import colors from 'config/colors';
 
 export default class CustomImage extends React.Component {
 
-  _size = {
-    width: this.props.size - 1,
-    height: this.props.size - 1,  
-  };
-
-  _imageStyle = {
-    borderRadius: (this.props.size - 1) / 2,
-    borderWidth: 1,
-    borderColor: colors.light_gray_3,
-    ...this._size
-  };
-
   _style = {
     borderRadius: (this.props.size - 1) / 2,
-    ...this._size
+    width: this.props.size - 1,
+    height: this.props.size - 1,  
   };
 
   shouldComponentUpdate = (nextProps) => (this.props.uri !== nextProps.uri);
@@ -29,7 +17,6 @@ export default class CustomImage extends React.Component {
     return (
       <Image
         source={{ uri }}
-        imageStyle={this._imageStyle}
         style={this._style}
         defaultSource={require('../../../assets/placeholder.png')}
       />
