c377a9f84b09b4a98d7f693db29035721ea3f583
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-04-29 05:43:06 +0100

    refactor/use yup schema

--- src/components/forms/Event/schema.js
+++ src/components/forms/Event/schema.js
@@ -1,6 +1,7 @@
 import * as Yup from 'yup';
 import moment from 'moment';
 import { canRecur, getTimeUnit } from 'lib/time';
+import recurrence from './recurrence';
 
 const emptyToNull= val => !val ? null : val;
 
@@ -11,16 +12,19 @@ export default Yup.object().shape({
     .min(2, 'tooShort')
   ,
   description: Yup.string()
+    .nullable()
+    .default(null)
     .trim()
+    .nullable()
     .min(10, 'tooShort')
     .max(300, 'tooLong')
     .transform(emptyToNull)
-    .nullable()
   ,
   venue: Yup.string()
+    .nullable()
+    .default(null)
     .trim()
     .transform(emptyToNull)
-    .nullable()
   ,
   eventScheduleId: Yup.string()
     .required()
@@ -37,6 +41,7 @@ export default Yup.object().shape({
   ,
   until: Yup.date()
     .nullable()
+    .default(null)
     .when(['startAt', 'recurrence'],
     (startAt, recurrence, schema) => {
       const unit = getTimeUnit(recurrence);
@@ -46,12 +51,38 @@ export default Yup.object().shape({
   ,
   recurrence: Yup.string()
     .required()
+    .default(recurrence[0].id)
     .when(['startAt', 'endAt'],
     (startAt, endAt, schema) => {
-      const possibleRecurrence = canRecur({ startAt, endAt });
-      return schema.oneOf(possibleRecurrence, 'invalidDatesAndRecur');
+      const validRecurrence = canRecur({ startAt, endAt });
+      return schema.oneOf(validRecurrence, 'invalidDatesAndRecur');
     })
   ,
   forever: Yup.boolean()
+    .default(false)
     .nullable()
+  ,
+  allDay: Yup.boolean()
+    .default(false)
+    .nullable()
+  ,
+  isPublic: Yup.boolean()
+    .default(false)
+    .nullable()
+  ,
+  geo_point: Yup.object().shape({
+    lat: Yup.number().required(),
+    lon: Yup.number().required()
+  })
+    .default(null)
+    .nullable()
+  ,
+  location: Yup.string()
+    .default(null)
+    .nullable()
+  ,
+  category: Yup.string()
+    .nullable()
+    .default(null)
+    .transform(emptyToNull)
 });
