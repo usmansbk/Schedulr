59726ca4a8a4553cb122265ff253e817b15bb63e
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-05-01 21:41:26 +0100

    chore

--- src/components/tabs/Events/Hoc.js
+++ src/components/tabs/Events/Hoc.js
@@ -1,6 +1,5 @@
-import { graphql, compose } from 'react-apollo';
+import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
-import { withNavigationFocus } from 'react-navigation';
 import { getUserData } from 'api/queries';
 import { baseEventsFilter } from 'api/filters';
 import Events from './Events';
@@ -8,26 +7,23 @@ import Events from './Events';
 const alias = 'withEventsContainer';
 const BaseQuery = gql(getUserData);
 
-export default compose(
-  withNavigationFocus,
-  graphql(BaseQuery, {
-    alias,
-    options: () => ({
-      fetchPolicy: 'cache-first',
-      notifyOnNetworkStatusChange: true,
-      variables: {
-        filter: baseEventsFilter(),
-        limit: 50
-      }
-    }),
-    props: ({ data, ownProps}) => ({
-      loading: data && (data.loading || data.networkStatus === 4),
-      data: data && data.getUserData,
-      onRefresh: () => data.refetch({
-        filter: baseEventsFilter(),
-        limit: 50
-      }),
-      ...ownProps
-    })
+export default graphql(BaseQuery, {
+  alias,
+  options: () => ({
+    fetchPolicy: 'cache-first',
+    notifyOnNetworkStatusChange: true,
+    variables: {
+      filter: baseEventsFilter(),
+      limit: 50
+    }
   }),
-)(Events);
+  props: ({ data, ownProps}) => ({
+    loading: data && (data.loading || data.networkStatus === 4),
+    data: data && data.getUserData,
+    onRefresh: () => data.refetch({
+      filter: baseEventsFilter(),
+      limit: 50
+    }),
+    ...ownProps
+  })
+})(Events);
