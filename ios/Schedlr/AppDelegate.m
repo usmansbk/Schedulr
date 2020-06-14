521b927dc5a38e8eede2939a8054b197fdafa7d4
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-14 11:13:29 +0100

    rename app

--- ios/Schedlr/AppDelegate.m
+++ ios/Schedlr/AppDelegate.m
@@ -20,7 +20,7 @@
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
-                                                      moduleName:@"Schdlr"
+                                                      moduleName:@"Schedlr"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [UIColor blackColor];
