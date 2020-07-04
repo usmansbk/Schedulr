b418e93122bd16ecdb8feffb6381136114e535c2
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-07-04 18:54:01 +0100

    chore

--- src/components/lists/ScheduleSearch/Item.js
+++ src/components/lists/ScheduleSearch/Item.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { View, TouchableOpacity } from 'react-native';
-import {  Text, Caption } from 'react-native-paper';
-import { inject, observer } from 'mobx-react';
+import {View, TouchableOpacity} from 'react-native';
+import {Text, Caption} from 'react-native-paper';
+import {inject, observer} from 'mobx-react';
 import UserAvatar from 'components/common/UserAvatar';
 import FollowButton from 'components/common/FollowButton';
 import Icon from 'components/common/Icon';
-import { schedule_search } from 'lib/constants';
+import {schedule_search} from 'lib/constants';
 
-const { AVATAR_SIZE } = schedule_search;
+const {AVATAR_SIZE} = schedule_search;
 
 class Item extends React.Component {
   _onPress = () => {
@@ -30,15 +30,15 @@ class Item extends React.Component {
       isOwner,
       isClosed,
       isFollowing,
-      stores
+      stores,
     } = this.props;
 
     const styles = stores.appStyles.scheduleSearch;
     const isAuth = (isPublic || isFollowing) && !isOwner;
-    
+
     return (
-      <TouchableOpacity style={styles.itemContainer} onPress={this._onPress}>
-        <View style={styles.itemContent}>
+      <View style={styles.itemContainer}>
+        <TouchableOpacity onPress={this._onPress} style={styles.itemContent}>
           <UserAvatar
             size={AVATAR_SIZE}
             name={name}
@@ -46,34 +46,39 @@ class Item extends React.Component {
             src={pictureUrl}
             onPress={this._onPress}
           />
-          {
-            (isClosed) && <Icon
+          {isClosed && (
+            <Icon
               style={styles.rightIcon}
-              name={isClosed ? "archive" : "cloud-off"}
+              name={isClosed ? 'archive' : 'cloud-off'}
               size={16}
               color={stores.themeStore.colors.light_gray_3}
             />
-          }
+          )}
           <View style={styles.itemBody}>
             <View style={styles.nameRow}>
-              <Text numberOfLines={1} ellipsizeMode="tail" style={styles.itemName}>{name}</Text>
+              <Text
+                numberOfLines={1}
+                ellipsizeMode="tail"
+                style={styles.itemName}>
+                {name}
+              </Text>
             </View>
-            { Boolean(description || topic) && <Caption numberOfLines={1} ellipsizeMode="tail" style={styles.itemDescription}>{topic || description}</Caption> }
+            {Boolean(description || topic) && (
+              <Caption
+                numberOfLines={1}
+                ellipsizeMode="tail"
+                style={styles.itemDescription}>
+                {topic || description}
+              </Caption>
+            )}
           </View>
-          {
-            isAuth && (
-              <FollowButton
-                id={id}
-                name={name}
-                isFollowing={isFollowing}
-                small
-              />
-            )
-          }
-        </View>
-      </TouchableOpacity>
+        </TouchableOpacity>
+        {isAuth && (
+          <FollowButton id={id} name={name} isFollowing={isFollowing} small />
+        )}
+      </View>
     );
   }
 }
 
-export default inject("stores")(observer(Item));
+export default inject('stores')(observer(Item));
