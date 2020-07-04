d671dd775f520262c4485bdd719e36d0243cef7d
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-07-04 14:19:00 +0100

    chore

--- src/components/common/UserAvatar/UserAvatar.js
+++ src/components/common/UserAvatar/UserAvatar.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { View, Image } from 'react-native';
-import { Text } from 'react-native-paper';
-import { getInitials, getAvatarBackgroundColor } from './helpers';
-import { avatarColorsWithOpacity } from 'config/colors';
+import {View, Image} from 'react-native';
+import {Text} from 'react-native-paper';
+import {getInitials, getAvatarBackgroundColor} from './helpers';
+import {avatarColorsWithOpacity} from 'config/colors';
 
-export default ({
-  src,
-  size=64,
-  name="John Doe"
-}) => {
+export default ({src, size = 64, name = 'John Doe'}) => {
   const source = {uri: src};
 
   const initials = getInitials(name);
@@ -17,29 +13,30 @@ export default ({
     width: size,
     height: size,
     borderRadius: size / 2,
-    backgroundColor: src ? 'transparent' : avatarColorsWithOpacity[bgColor],
+    backgroundColor: avatarColorsWithOpacity[bgColor],
     justifyContent: 'center',
     alignItems: 'center',
   };
 
   return (
     <View style={style}>
-      {
-        src ? (
-          <Image
-            style={style}
-            source={source}
-            defaultSource={require('../../../assets/placeholder.png')}
-          />
-        ) : (
-          <Text
-            style={{
-              color: bgColor,
-              fontSize: size / 2,
-              fontFamily: 'sans-serif-light',
-            }} adjustsFontSizeToFit>{initials}</Text>
-        )
-      }
+      {src ? (
+        <Image
+          style={style}
+          source={source}
+          defaultSource={require('../../../assets/placeholder.png')}
+        />
+      ) : (
+        <Text
+          style={{
+            color: bgColor,
+            fontSize: size / 2,
+            fontFamily: 'sans-serif-light',
+          }}
+          adjustsFontSizeToFit>
+          {initials}
+        </Text>
+      )}
     </View>
-  )
+  );
 };
