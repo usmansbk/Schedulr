44133dac35a343e6041979a47b8b2af514489cbb
usmansbk <usmansbk@gmail.com>
2020-09-03 03:01:30 +0100

    rename app

--- android/app/src/main/java/com/schdlr/MainActivity.java
+++ android/app/src/main/java/com/schdlr/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "Schdlr";
+        return "Schedulr";
     }
   
     @Override
