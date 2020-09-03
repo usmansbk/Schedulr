97def3bce4eddb3dbcf1b931ed07e504900586ee
usmansbk <usmansbk@gmail.com>
2020-09-03 03:06:03 +0100

    add keys

--- src/components/common/Attachment/Attachment.js
+++ src/components/common/Attachment/Attachment.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import { View } from 'react-native';
+import {View} from 'react-native';
 import Media from './Media';
 
 export default class Attachment extends React.Component {
   render() {
-    const { attachment, navigateToViewEmbed} = this.props;
+    const {attachment, navigateToViewEmbed} = this.props;
 
     return (
       <View>
-        {
-          Boolean(attachment) &&
-            attachment.map(file => (
-            <Media navigateToViewEmbed={navigateToViewEmbed} file={file} />))
-        }
+        {Boolean(attachment) &&
+          attachment.map((file, index) => (
+            <Media
+              key={index}
+              navigateToViewEmbed={navigateToViewEmbed}
+              file={file}
+            />
+          ))}
       </View>
-    )
+    );
   }
 }
