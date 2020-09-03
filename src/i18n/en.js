4fdb6686c1417c58a1ed9bdeeec86f59051e0141
usmansbk <usmansbk@gmail.com>
2020-09-03 03:37:40 +0100

    update walkthrough

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -418,49 +418,24 @@ export default {
   walkthrough: [
     {
       key: 'p1',
-      title: 'Schdlr',
-      text: 'Schdlr is a social event scheduler',
-      image: require('../assets/student.png'),
-      backgroundColor: '#22bcb5',
+      title: 'Manage',
+      text: 'Manage events, get updates and reminders',
+      image: require('../assets/calendar.png'),
+      backgroundColor: '#fff',
     },
     {
       key: 'p2',
-      title: 'Create schedules easily!',
-      text:
-        'Enter a schedule name and your schedule is ready to be used and shared.',
-      image: require('../assets/list-app.png'),
-      backgroundColor: '#63a4ff',
+      title: 'Share',
+      text: 'Join a group of interest and stay up to date with their events',
+      image: require('../assets/handshake.png'),
+      backgroundColor: '#fff',
     },
     {
       key: 'p3',
-      title: 'Add events to your schedules!',
-      text: 'Organize your events by grouping them in specific schedules.',
-      image: require('../assets/calendar.png'),
-      backgroundColor: '#00897b',
-    },
-    {
-      key: 'p4',
-      title: 'Keep everyone Up-to-date!',
-      text:
-        'Create schedules for events in your life and invite people to follow and share them.',
-      image: require('../assets/handshake.png'),
-      backgroundColor: '#ab47bc',
-    },
-    {
-      key: 'p5',
-      title: 'Follow schedules of interest!',
-      text:
-        'Follow schedules and get their events offline reminders and real-time updates.',
-      image: require('../assets/app-user.png'),
-      backgroundColor: '#673ab7',
-    },
-    {
-      key: 'p6',
-      title: 'Welcome to Schdlr!',
-      text:
-        'Schdlr helps you to organize your events, by creating schedules to keep everyone up-to-date.',
-      image: require('../assets/schoolbooks.png'),
-      backgroundColor: '#22bcb5',
+      title: 'Discover',
+      text: 'See events happening around you',
+      image: require('../assets/discover.png'),
+      backgroundColor: '#fff',
     },
   ],
 };
