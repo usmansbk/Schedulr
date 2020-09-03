be5145e9281260ecbfafb6d6a7b79cab76557d91
usmansbk <usmansbk@gmail.com>
2020-09-03 04:58:41 +0100

    fix login

--- src/components/screens/Login/Container.js
+++ src/components/screens/Login/Container.js
@@ -114,7 +114,7 @@ class Container extends React.Component {
           this.props.stores.appState.setState(user.state);
           this.props.stores.appState.setUserId(email);
           this.props.stores.settings.setUserPreference(user.preference);
-          crashlytics().setUserEmail(email);
+          crashlytics().setAttributes({email});
           logger.log('sign-in');
           this.props.navigation.navigate('App');
         } catch (error) {
