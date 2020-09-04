d370f366989b7e0db84ec1421fa7491d7171d55e
usmansbk <usmansbk@gmail.com>
2020-09-04 08:03:33 +0100

    chore

--- src/components/screens/EmailAuth/ForgotPassword.js
+++ src/components/screens/EmailAuth/ForgotPassword.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
-import {Text} from 'react-native-paper';
+import {observer, inject} from 'mobx-react';
+import {View, ScrollView, StyleSheet} from 'react-native';
+import {Text, Appbar} from 'react-native-paper';
+import Icon from 'components/common/Icon';
 
-export default function Login() {
+function ForgotPassword(props) {
+  const styles = StyleSheet.create({
+    container: {
+      flexGrow: 1,
+      backgroundColor: props.stores.theme.colors.bg,
+      padding: 16,
+    },
+  });
+
   return (
-    <View>
-      <Text>Email Login</Text>
-    </View>
+    <>
+      <Appbar.Header style={props.stores.styles.appStyles.header}>
+        <Appbar.Action
+          onPress={() => props.navigation.goBack()}
+          animated={false}
+          icon={({size, color}) => (
+            <Icon size={size} color={color} name="arrow-left" />
+          )}
+        />
+      </Appbar.Header>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Text>Email Login</Text>
+      </ScrollView>
+    </>
   );
 }
+
+export default inject('stores')(observer(ForgotPassword));
