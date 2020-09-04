f2debdbd74b5058583178ab445cf4db42ecaa552
usmansbk <usmansbk@gmail.com>
2020-09-04 08:23:20 +0100

    style screen

--- src/components/screens/EmailAuth/SignIn.js
+++ src/components/screens/EmailAuth/SignIn.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {observer, inject} from 'mobx-react';
+import {I18n} from 'aws-amplify';
 import {View, ScrollView, StyleSheet} from 'react-native';
-import {Text, Appbar} from 'react-native-paper';
+import {Appbar, Headline, TextInput, Button} from 'react-native-paper';
 import Icon from 'components/common/Icon';
 
 function SignIn(props) {
@@ -10,6 +11,20 @@ function SignIn(props) {
       flexGrow: 1,
       backgroundColor: props.stores.theme.colors.bg,
       padding: 16,
+    },
+    field: {
+      marginVertical: 8,
+    },
+    button: {
+      height: 48,
+    },
+    label: {
+      color: 'white',
+      fontFamily: 'SemiBold',
+    },
+    row: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
     },
   });
 
@@ -25,7 +40,52 @@ function SignIn(props) {
         />
       </Appbar.Header>
       <ScrollView contentContainerStyle={styles.container}>
-        <Text>Email Login</Text>
+        <Headline>{I18n.get('Login')}</Headline>
+        <TextInput
+          label={I18n.get('Email')}
+          placeholder={I18n.get('Type your email')}
+          theme={{roundness: 0}}
+          style={styles.field}
+        />
+        <TextInput
+          label={I18n.get('Password')}
+          secureTextEntry
+          placeholder={I18n.get('Type your password')}
+          theme={{roundness: 0}}
+          style={styles.field}
+        />
+        <View style={styles.row}>
+          <Button
+            uppercase={false}
+            onPress={() => props.navigation.navigate('Confirm')}
+            style={styles.field}
+            contentStyle={styles.button}>
+            {I18n.get('Verify')}
+          </Button>
+          <Button
+            uppercase={false}
+            onPress={() => props.navigation.navigate('ForgotPassword')}
+            style={styles.field}
+            contentStyle={styles.button}>
+            {I18n.get('Forgot password?')}
+          </Button>
+        </View>
+        <Button
+          onPress={() => null}
+          uppercase={false}
+          style={styles.field}
+          mode="contained"
+          labelStyle={styles.label}
+          contentStyle={styles.button}>
+          {I18n.get('Login')}
+        </Button>
+        <Button
+          onPress={() => props.navigation.navigate('SignUp')}
+          uppercase={false}
+          style={styles.field}
+          contentStyle={styles.button}>
+          {I18n.get('Create account')}
+        </Button>
       </ScrollView>
     </>
   );
