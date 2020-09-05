5a293a891cef526376da925b67ae40a3281e5256
usmansbk <usmansbk@gmail.com>
2020-09-05 22:37:23 +0100

    add function

---
+++ amplify/backend/function/querynearbyresolver/src/index.js
@@ -0,0 +1,13 @@
+/* Amplify Params - DO NOT EDIT
+	STORAGE_GEOTABLE_ARN
+	STORAGE_GEOTABLE_NAME
+Amplify Params - DO NOT EDIT */
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
