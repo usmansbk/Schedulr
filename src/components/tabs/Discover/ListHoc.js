0f32b8c521b3351dd1dfe6e8b848d471ec1339a2
usmansbk <usmansbk@gmail.com>
2020-09-05 21:13:54 +0100

    chore

--- src/components/tabs/Discover/ListHoc.js
+++ src/components/tabs/Discover/ListHoc.js
@@ -1,7 +1,7 @@
-import { graphql, compose } from 'react-apollo';
-import { withNavigationFocus } from 'react-navigation';
+import {graphql, compose} from 'react-apollo';
+import {withNavigationFocus} from 'react-navigation';
 import gql from 'graphql-tag';
-import { nearbyEvents } from 'api/queries';
+import {nearbyEvents} from 'api/queries';
 import List from 'components/lists/Discover';
 import updateQuery from 'helpers/updateQuery';
 
@@ -9,41 +9,47 @@ const alias = 'withDiscoverContainer';
 
 export default compose(
   withNavigationFocus,
-  graphql(gql(nearbyEvents),
-  {
+  graphql(gql(nearbyEvents), {
     alias,
-    options: props => ({
+    options: (props) => ({
       fetchPolicy: 'cache-and-network',
-      variables : {
-        city: props.city,
-        location: props.location,
-        category: props.category,
-        km: 250,
-        limit: 50
-      },
-      notifyOnNetworkStatusChange: true
-    }),
-    skip: props => !props.location,
-    props: ({ data, ownProps}) => ({
-      data: data && data.nearbyEvents && data.nearbyEvents.items || [],
-      nextToken: data && data.nearbyEvents && data.nearbyEvents.nextToken,
-      loading: data && (data.loading || data.networkStatus === 4 || data.networkStatus === 3),
-      onRefresh: () => data && data.refetch({
-        nextToken: null
-      }),
-      fetchMore: (nextToken) => data.fetchMore({
-        variables: {
-          nextToken,
-          limit: 50
+      variables: {
+        filter: {
+          city: props.city,
+          location: props.location,
+          category: props.category,
+          km: 250,
         },
-        updateQuery: (prev, { fetchMoreResult }) => (
-          updateQuery({
-            prev,
-            fetchMoreResult,
-            rootField: 'nearbyEvents',
-          })
-        )
-      }),
-      ...ownProps
-    })
-}))(List);
+        limit: 50,
+      },
+      notifyOnNetworkStatusChange: true,
+    }),
+    skip: (props) => !props.location,
+    props: ({data, ownProps}) => ({
+      data: (data && data.nearbyEvents && data.nearbyEvents.items) || [],
+      nextToken: data && data.nearbyEvents && data.nearbyEvents.nextToken,
+      loading:
+        data &&
+        (data.loading || data.networkStatus === 4 || data.networkStatus === 3),
+      onRefresh: () =>
+        data &&
+        data.refetch({
+          nextToken: null,
+        }),
+      fetchMore: (nextToken) =>
+        data.fetchMore({
+          variables: {
+            nextToken,
+            limit: 50,
+          },
+          updateQuery: (prev, {fetchMoreResult}) =>
+            updateQuery({
+              prev,
+              fetchMoreResult,
+              rootField: 'nearbyEvents',
+            }),
+        }),
+      ...ownProps,
+    }),
+  }),
+)(List);
