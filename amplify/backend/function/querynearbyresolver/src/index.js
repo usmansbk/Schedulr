b9bb3d54e82c637be49aad4d574e26e8da6adef5
usmansbk <usmansbk@gmail.com>
2020-09-06 14:15:30 +0100

    chore

--- amplify/backend/function/querynearbyresolver/src/index.js
+++ amplify/backend/function/querynearbyresolver/src/index.js
@@ -16,7 +16,12 @@ config.longitudeFirst = true;
 const manager = new ddbGeo.GeoDataManager(config);
 
 exports.handler = async (event) => {
-  const {arguments} = event;
+  const {
+    arguments,
+    identity: {
+      claims: {email},
+    },
+  } = event;
   const {filter = {}, limit = 50, nextToken = 0} = arguments;
   const {
     location: {lat: latitude, lon: longitude},
@@ -24,20 +29,23 @@ exports.handler = async (event) => {
   } = filter;
 
   try {
-    const result = await manager.queryRadius({
-      RadiusInMeter: radius * 1000,
-      CenterPoint: {
-        latitude,
-        longitude,
-      },
-    });
+    const result = await manager
+      .queryRadius({
+        RadiusInMeter: radius * 1000,
+        CenterPoint: {
+          latitude,
+          longitude,
+        },
+      })
+      .map(AWS.DynamoDB.Converter.unmarshall)
+      .filter((item) => item.eventAuthorId !== email);
     const start = nextToken ? Number(nextToken) : 0;
     const end = start + Number(limit);
 
-    const paginated = result.slice(start, end);
+    const items = result.slice(start, end);
     return {
-      items: paginated.map(AWS.DynamoDB.Converter.unmarshall),
-      nextToken: start + paginated.length + 1,
+      items,
+      nextToken: start + items.length + 1,
       total: result.length,
     };
   } catch (error) {
