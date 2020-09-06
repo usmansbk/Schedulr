1931dfe910c56a0604256e542a0d6bfe75d62738
usmansbk <usmansbk@gmail.com>
2020-09-06 08:22:50 +0100

    chore

--- amplify/backend/function/querynearbyresolver/src/index.js
+++ amplify/backend/function/querynearbyresolver/src/index.js
@@ -2,9 +2,33 @@
 	STORAGE_GEOTABLE_ARN
 	STORAGE_GEOTABLE_NAME
 Amplify Params - DO NOT EDIT */
+const AWS = require('aws-sdk');
+const ddbGeo = require('dynamodb-geo');
+
+const ddb = new AWS.DynamoDB();
+const config = new ddbGeo.GeoDataManagerConfiguration(
+  ddb,
+  process.env.STORAGE_GEOTABLE_NAME,
+);
+
+config.longitudeFirst = true;
+
+const manager = new ddbGeo.GeoDataManager(config);
 
 exports.handler = async (event) => {
-  // TODO implement
+  console.log(JSON.stringify(event, null, 2));
+  try {
+    const result = await manager.queryRadius({
+      RadiusInMeter: 1000,
+      CenterPoint: {
+        latitude: 52.22573,
+        longitude: 0.149593,
+      },
+    });
+    console.log(result);
+  } catch (error) {
+    console.log('Error %>', error.message);
+  }
   const response = {
     items: [],
     nextToken: null,
