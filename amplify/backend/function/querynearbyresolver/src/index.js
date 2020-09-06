0fecbd8a5642f35d880c3c7ffb120c0655ad9eef
usmansbk <usmansbk@gmail.com>
2020-09-06 14:28:23 +0100

    fix bug

--- amplify/backend/function/querynearbyresolver/src/index.js
+++ amplify/backend/function/querynearbyresolver/src/index.js
@@ -25,20 +25,28 @@ exports.handler = async (event) => {
   const {filter = {}, limit = 50, nextToken = 0} = arguments;
   const {
     location: {lat: latitude, lon: longitude},
+    category = inputCategory,
     km: radius = 100,
   } = filter;
 
+  const category = inputCategory || '';
+
   try {
-    const result = await manager
-      .queryRadius({
+    const result = (
+      await manager.queryRadius({
         RadiusInMeter: radius * 1000,
         CenterPoint: {
           latitude,
           longitude,
         },
       })
+    )
       .map(AWS.DynamoDB.Converter.unmarshall)
-      .filter((item) => item.eventAuthorId !== email);
+      .filter(
+        (item) =>
+          item.eventAuthorId !== email &&
+          item.category.toLowerCase().contains(category.toLowerCase()),
+      );
     const start = nextToken ? Number(nextToken) : 0;
     const end = start + Number(limit);
 
