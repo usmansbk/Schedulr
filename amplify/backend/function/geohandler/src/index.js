9033495af764e24dc37f49cd92f769342ea2461d
usmansbk <usmansbk@gmail.com>
2020-09-06 06:03:43 +0100

    test

--- amplify/backend/function/geohandler/src/index.js
+++ amplify/backend/function/geohandler/src/index.js
@@ -11,35 +11,39 @@ config.longitudeFirst = true;
 
 const manager = new ddbGeo.GeoDataManager(config);
 
+const BATCH_SIZE = 25;
+
 exports.handler = async (event) => {
   //eslint-disable-line
   console.log(JSON.stringify(event, null, 2));
-  event.Records.forEach((record) => {
-    console.log(record.eventID);
-    console.log(record.eventName);
-    console.log('DynamoDB Record: %j', record.dynamodb);
-  });
-  try {
-    await manager
-      .putPoint({
-        RangeKeyValue: {S: '1234'},
-        GeoPoint: {
-          latitude: 51.51,
-          longitude: -0.13,
-        },
-        PutItemInput: {
-          Item: {
-            id: {S: 'working'},
-            country: {S: 'UK'},
-            capital: {S: 'London'},
-          },
-        },
-      })
-      .promise();
-    console.log('*** WORKING');
-  } catch (error) {
-    console.log('***', error.message);
+  const {Records} = event;
+  const data = Records.map(transform);
+  for (let i = 0; i < data.length; i += BATCH_SIZE) {
+    const batch = data.slice(i, i + BATCH_SIZE);
+    try {
+      await manager.batchWritePoints(batch).promise();
+      console.log('BATCH WRITE SUCCESSFUL');
+    } catch (error) {
+      console.log('Error %>', error.message);
+    }
   }
 
   return 'Successfully processed DynamoDB record';
 };
+
+function transform(record) {
+  const {
+    dynamodb: {NewImage: Item},
+  } = record;
+  console.log('Transform', JSON.stringify(Item));
+  return {
+    RangeKeyValue: {S: '1234'},
+    GeoPoint: {
+      latitude: 51.51,
+      longitude: -0.13,
+    },
+    PutItemInput: {
+      Item,
+    },
+  };
+}
