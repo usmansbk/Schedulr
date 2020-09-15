5a5f0d7d774cc2f9278ea00ec4ae7ac096b8344e
usmansbk <usmansbk@gmail.com>
2020-09-15 20:50:51 +0100

    chore

--- src/components/common/Banner/Ad.js
+++ src/components/common/Banner/Ad.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, ImageBackground} from 'react-native';
+import {StyleSheet, ImageBackground, View} from 'react-native';
 import {BannerAd, BannerAdSize, TestIds} from '@react-native-firebase/admob';
 import env from 'config/env';
 
@@ -9,28 +9,35 @@ const styles = StyleSheet.create({
   container: {
     justifyContent: 'center',
     alignItems: 'center',
+    width: '100%',
+  },
+  image: {
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
 export default function AdBanner({large, mediumRect}) {
   const height = large ? (mediumRect ? 250 : 100) : 50;
-  const width = 320;
+  const width = mediumRect ? 300 : 320;
   const size = large
     ? mediumRect
       ? BannerAdSize.MEDIUM_RECTANGLE
       : BannerAdSize.LARGE_BANNER
     : BannerAdSize.BANNER;
   return (
-    <ImageBackground
-      source={require('assets/nature-rays.jpg')}
-      style={[
-        styles.container,
-        {
-          height,
-          width: '100%',
-        },
-      ]}>
-      <BannerAd unitId={adUnitId} size={size} />
-    </ImageBackground>
+    <View style={styles.container}>
+      <ImageBackground
+        source={require('assets/nature-rays.jpg')}
+        style={[
+          styles.image,
+          {
+            height,
+            width,
+          },
+        ]}>
+        <BannerAd unitId={adUnitId} size={size} />
+      </ImageBackground>
+    </View>
   );
 }
