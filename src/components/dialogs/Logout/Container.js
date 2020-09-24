dd8eba69bc923ded8c059537e7055e87bf6ab980
usmansbk <usmansbk@gmail.com>
2020-09-24 09:46:19 +0100

    chore

--- src/components/dialogs/Logout/Container.js
+++ src/components/dialogs/Logout/Container.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Auth } from 'aws-amplify';
-import { withNavigation } from 'react-navigation';
-import { inject, observer } from 'mobx-react';
-import { withApollo } from 'react-apollo';
+import {Auth} from 'aws-amplify';
+import {withNavigation} from 'react-navigation';
+import {inject, observer} from 'mobx-react';
+import {withApollo} from 'react-apollo';
 import Dialog from './Dialog';
 import logger from 'config/logger';
 
@@ -11,31 +11,28 @@ class Container extends React.Component {
     loading: false,
   };
 
-  _confirmRef = ref => this.confirmRef = ref;
+  _confirmRef = (ref) => (this.confirmRef = ref);
   open = () => this.confirmRef.open();
 
   _signOut = async () => {
-    this.setState({ loading: true });
+    this.setState({loading: true});
     try {
       await this.props.client.clearStore();
       await Auth.signOut();
       this.props.stores.reset();
-    } catch(error) {
+    } catch (error) {
       logger.logError(error);
     }
     this.props.navigation.navigate('Auth');
   };
 
   render() {
-    return (
-      <Dialog
-        handleLogout={this._signOut}
-        ref={this._confirmRef}
-      />
-    );
+    return <Dialog handleLogout={this._signOut} ref={this._confirmRef} />;
   }
 }
 
-const withStores = inject("stores")(observer(withApollo(Container, { withRef: true})));
+const withStores = inject('stores')(
+  observer(withApollo(Container, {withRef: true})),
+);
 
 export default withNavigation(withStores);
