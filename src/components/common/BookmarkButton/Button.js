5bc5556ee25411e97b688a4bb3f411b52791e452
usmansbk <usmansbk@gmail.com>
2020-11-03 21:01:30 +0100

    chore

--- src/components/common/BookmarkButton/Button.js
+++ src/components/common/BookmarkButton/Button.js
@@ -3,23 +3,8 @@ import IconBadge from '../IconBadge';
 import logger from 'config/logger';
 
 export default class Button extends React.Component {
-  state = {
-    loading: false,
-    isBookmarked: false,
-  };
-
-  static getDerivedStateFromProps(props, state) {
-    if (props.isBookmarked !== state.isBookmarked && !state.loading) {
-      return {
-        isBookmarked: props.isBookmarked,
-        loading: state.loading,
-      };
-    }
-    return null;
-  }
-
-  shouldComponentUpdate = (_, nextState) =>
-    nextState.isBookmarked !== this.state.isBookmarked;
+  shouldComponentUpdate = (nextProp) =>
+    nextProp.isBookmarked !== this.props.isBookmarked;
 
   _onPress = async () => {
     const {
@@ -30,28 +15,21 @@ export default class Button extends React.Component {
       bookmarkEvent,
       bookmarkScheduleId,
     } = this.props;
-    this.setState((prevState) => ({
-      loading: true,
-      isBookmarked: !prevState.isBookmarked,
-    }));
-    const {loading} = this.state;
+
     this.timer = setTimeout(async () => {
-      if (!loading) {
-        const input = {
-          id: `${stores.appState.userId}-${id}`,
-        };
-        try {
-          if (isBookmarked) {
-            await removeBookmark(input, id);
-          } else {
-            (input.bookmarkEventId = id),
-              (input.bookmarkScheduleId = bookmarkScheduleId);
-            await bookmarkEvent(input);
-          }
-        } catch (error) {
-          logger.logError(error);
+      const input = {
+        id: `${stores.appState.userId}-${id}`,
+      };
+      try {
+        if (isBookmarked) {
+          await removeBookmark(input, id);
+        } else {
+          input.bookmarkEventId = id;
+          input.bookmarkScheduleId = bookmarkScheduleId;
+          await bookmarkEvent(input);
         }
-        this.setState({loading: false});
+      } catch (error) {
+        logger.logError(error);
       }
     }, 0);
   };
@@ -63,12 +41,8 @@ export default class Button extends React.Component {
   };
 
   render() {
-    const {size, bookmarksCount, stores} = this.props;
-    const {isBookmarked} = this.state;
-
-    const count =
-      this.props.isBookmarked && bookmarksCount === 1 ? 0 : bookmarksCount;
-
+    const {size, bookmarksCount, isBookmarked, stores} = this.props;
+    const count = isBookmarked && bookmarksCount === 1 ? 0 : bookmarksCount;
     const activeColor = stores.theme.colors.like;
     const color = stores.theme.colors.gray;
 
